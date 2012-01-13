9f7fe775e8eafa40975d76d029d1eba54b1f8026
l_maliakal_d <l_maliakal_d@951219d9-93cf-4727-9268-0efd64621fa3>
2012-01-13 09:38:29 +0000

    works with gigabit
    git-svn-id: file:///afs/psi.ch/project/sls_det_software/svn/slsDetectorSoftware@93 951219d9-93cf-4727-9268-0efd64621fa3

--- slsDetectorSoftware/gotthardDetectorServer/stop_server.c
+++ slsDetectorSoftware/gotthardDetectorServer/stop_server.c
@@ -1,7 +1,7 @@
 /* A simple server in the internet domain using TCP
    The port number is passed as an argument */
 #include "communication_funcs.h"
-//#include "firmware_funcs.h"
+#include "firmware_funcs.h"
 
 
 int sockfd;

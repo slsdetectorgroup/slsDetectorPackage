c822b064ab9a4dfc3515f8793abdc7348b432c42
l_maliakal_d <l_maliakal_d@951219d9-93cf-4727-9268-0efd64621fa3>
2012-03-16 15:47:25 +0000

    added the testfunction
    git-svn-id: file:///afs/psi.ch/project/sls_det_software/svn/slsDetectorSoftware@151 951219d9-93cf-4727-9268-0efd64621fa3

--- slsDetectorSoftware/multiSlsDetector/multiSlsDetector.cpp
+++ slsDetectorSoftware/multiSlsDetector/multiSlsDetector.cpp
@@ -3220,7 +3220,18 @@ int multiSlsDetector::retrieveDetectorSetup(string const fname1, int level){
 
 
 int multiSlsDetector::testFunction(int times) {
-  return OK;
+
+	int ret=-100, ret1;
+	for (int idet=0; idet<thisMultiDetector->numberOfDetectors; idet++) {
+		if (detectors[idet]) {
+			ret1=detectors[idet]->testFunction(times);
+			if (ret==-100)
+				ret=ret1;
+			else if (ret!=ret1)
+				ret=-1;
+		}
+	}
+	return ret;
 }
   
 

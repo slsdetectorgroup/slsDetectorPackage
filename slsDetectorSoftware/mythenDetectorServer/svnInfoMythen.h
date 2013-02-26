0e9b32fb337511d55b88f056f3d744ef0171faaf
l_maliakal_d <l_maliakal_d@951219d9-93cf-4727-9268-0efd64621fa3>
2013-02-26 08:35:12 +0000

    made advanced tab detector dependent. each set mac adress etc is done via slsdetector and not multi
    git-svn-id: file:///afs/psi.ch/project/sls_det_software/svn/slsDetectorSoftware@472 951219d9-93cf-4727-9268-0efd64621fa3

--- slsDetectorSoftware/mythenDetectorServer/svnInfoMythen.h
+++ slsDetectorSoftware/mythenDetectorServer/svnInfoMythen.h
@@ -2,10 +2,10 @@
 #define SVNURL "file:///afs/psi.ch/project/sls_det_software/svn/slsDetectorSoftware/mythenDetectorServer"
 //#define SVNREPPATH ""
 #define SVNREPUUID "951219d9-93cf-4727-9268-0efd64621fa3"
-//#define SVNREV 0x469
+//#define SVNREV 0x471
 //#define SVNKIND ""
 //#define SVNSCHED ""
 #define SVNAUTH "l_maliakal_d"
-#define SVNREV 0x469
-#define SVNDATE 0x20130222
+#define SVNREV 0x471
+#define SVNDATE 0x20130226
 //

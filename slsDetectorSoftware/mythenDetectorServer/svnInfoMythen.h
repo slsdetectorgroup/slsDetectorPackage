96d9e5d0788696a742bc0ca7d12b1767f1cbe9cc
l_maliakal_d <l_maliakal_d@951219d9-93cf-4727-9268-0efd64621fa3>
2013-03-06 15:26:29 +0000

    some revision variable problem resolved in slsDetector
    git-svn-id: file:///afs/psi.ch/project/sls_det_software/svn/slsDetectorSoftware@484 951219d9-93cf-4727-9268-0efd64621fa3

---
+++ slsDetectorSoftware/mythenDetectorServer/svnInfoMythen.h
@@ -0,0 +1,11 @@
+//#define SVNPATH ""
+#define SVNURL "file:///afs/psi.ch/project/sls_det_software/svn/slsDetectorSoftware/mythenDetectorServer"
+//#define SVNREPPATH ""
+#define SVNREPUUID "951219d9-93cf-4727-9268-0efd64621fa3"
+//#define SVNREV 0x482
+//#define SVNKIND ""
+//#define SVNSCHED ""
+#define SVNAUTH "l_maliakal_d"
+#define SVNREV 0x482
+#define SVNDATE 0x20130306
+//
---
+++ slsDetectorSoftware/slsDetector/svnInfoLib.h
@@ -0,0 +1,11 @@
+//#define SVNPATH ""
+#define SVNURLLIB "file:///afs/psi.ch/project/sls_det_software/svn/slsDetectorSoftware"
+//#define SVNREPPATH ""
+#define SVNREPUUIDLIB "951219d9-93cf-4727-9268-0efd64621fa3"
+//#define SVNREV 0x482
+//#define SVNKIND ""
+//#define SVNSCHED ""
+#define SVNAUTHLIB "l_maliakal_d"
+#define SVNREVLIB 0x482
+#define SVNDATELIB 0x20130306
+//

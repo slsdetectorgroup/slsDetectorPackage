b0d873943981b7bea20e02a1d833cd411285be0f
l_maliakal_d <l_maliakal_d@af1100a4-978c-4157-bff7-07162d2ba061>
2013-05-17 10:20:45 +0000

    update svn
    git-svn-id: file:///afs/psi.ch/project/sls_det_software/svn/slsDetectorGui@187 af1100a4-978c-4157-bff7-07162d2ba061

--- slsDetectorGui/include/svnInfoGui.h
+++ slsDetectorGui/include/svnInfoGui.h
@@ -2,10 +2,10 @@
 #define SVNURL "file:///afs/psi.ch/project/sls_det_software/svn/slsDetectorGui"
 //#define SVNREPPATH ""
 #define SVNREPUUID "af1100a4-978c-4157-bff7-07162d2ba061"
-//#define SVNREV 0x184
+//#define SVNREV 0x186
 //#define SVNKIND ""
 //#define SVNSCHED ""
 #define SVNAUTH "l_maliakal_d"
-#define SVNREV 0x184
-#define SVNDATE 0x20130515
+#define SVNREV 0x186
+#define SVNDATE 0x20130517
 //

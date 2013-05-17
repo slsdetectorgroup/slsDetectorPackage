19179e1eb73db55e495fcac717196407c8b2326e
l_maliakal_d <l_maliakal_d@951219d9-93cf-4727-9268-0efd64621fa3>
2013-05-17 10:20:40 +0000

    update svn
    git-svn-id: file:///afs/psi.ch/project/sls_det_software/svn/slsDetectorSoftware@565 951219d9-93cf-4727-9268-0efd64621fa3

--- slsDetectorSoftware/mythenDetectorServer/svnInfoMythen.h
+++ slsDetectorSoftware/mythenDetectorServer/svnInfoMythen.h
@@ -2,10 +2,10 @@
 #define SVNURL "file:///afs/psi.ch/project/sls_det_software/svn/slsDetectorSoftware/mythenDetectorServer"
 //#define SVNREPPATH ""
 #define SVNREPUUID "951219d9-93cf-4727-9268-0efd64621fa3"
-//#define SVNREV 0x555
+//#define SVNREV 0x557
 //#define SVNKIND ""
 //#define SVNSCHED ""
 #define SVNAUTH "l_maliakal_d"
-#define SVNREV 0x555
-#define SVNDATE 0x20130515
+#define SVNREV 0x557
+#define SVNDATE 0x20130516
 //
--- slsDetectorSoftware/slsDetector/svnInfoLib.h
+++ slsDetectorSoftware/slsDetector/svnInfoLib.h
@@ -2,10 +2,10 @@
 #define SVNURLLIB "file:///afs/psi.ch/project/sls_det_software/svn/slsDetectorSoftware"
 //#define SVNREPPATH ""
 #define SVNREPUUIDLIB "951219d9-93cf-4727-9268-0efd64621fa3"
-//#define SVNREV 0x556
+//#define SVNREV 0x564
 //#define SVNKIND ""
 //#define SVNSCHED ""
 #define SVNAUTHLIB "l_maliakal_d"
-#define SVNREVLIB 0x556
-#define SVNDATELIB 0x20130516
+#define SVNREVLIB 0x564
+#define SVNDATELIB 0x20130517
 //

94d5b307071325d3f456f63f60d12a6bb9e7db2c
l_maliakal_d <l_maliakal_d@951219d9-93cf-4727-9268-0efd64621fa3>
2013-06-13 13:32:54 +0000

    revision stuff
    git-svn-id: file:///afs/psi.ch/project/sls_det_software/svn/slsDetectorSoftware@603 951219d9-93cf-4727-9268-0efd64621fa3

--- slsDetectorSoftware/slsDetector/svnInfoLib.h
+++ slsDetectorSoftware/slsDetector/svnInfoLib.h
@@ -2,10 +2,10 @@
 #define SVNURLLIB "file:///afs/psi.ch/project/sls_det_software/svn/slsDetectorSoftware"
 //#define SVNREPPATH ""
 #define SVNREPUUIDLIB "951219d9-93cf-4727-9268-0efd64621fa3"
-//#define SVNREV 0x594
+//#define SVNREV 0x602
 //#define SVNKIND ""
 //#define SVNSCHED ""
 #define SVNAUTHLIB "l_maliakal_d"
-#define SVNREVLIB 0x594
-#define SVNDATELIB 0x20130612
+#define SVNREVLIB 0x602
+#define SVNDATELIB 0x20130613
 //

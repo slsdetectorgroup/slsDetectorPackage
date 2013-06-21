802095ebc6c38b79458ce1640fad2ad87e10e9f3
l_maliakal_d <l_maliakal_d@951219d9-93cf-4727-9268-0efd64621fa3>
2013-06-21 14:28:33 +0000

    updated svn
    git-svn-id: file:///afs/psi.ch/project/sls_det_software/svn/slsDetectorSoftware@627 951219d9-93cf-4727-9268-0efd64621fa3

--- slsDetectorSoftware/gotthardDetectorServer/svnInfoGotthard.h
+++ slsDetectorSoftware/gotthardDetectorServer/svnInfoGotthard.h
@@ -2,10 +2,10 @@
 #define SVNURL "file:///afs/psi.ch/project/sls_det_software/svn/slsDetectorSoftware/gotthardDetectorServer"
 //#define SVNREPPATH ""
 #define SVNREPUUID "951219d9-93cf-4727-9268-0efd64621fa3"
-//#define SVNREV 0x603
+//#define SVNREV 0x617
 //#define SVNKIND ""
 //#define SVNSCHED ""
 #define SVNAUTH "l_maliakal_d"
-#define SVNREV 0x603
-#define SVNDATE 0x20130613
+#define SVNREV 0x617
+#define SVNDATE 0x20130619
 //

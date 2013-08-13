557f2f90ec931a777eb8a71f12e3a59ab3d67339
l_maliakal_d <l_maliakal_d@951219d9-93cf-4727-9268-0efd64621fa3>
2013-08-13 10:30:27 +0000

    update svn rev
    git-svn-id: file:///afs/psi.ch/project/sls_det_software/svn/slsDetectorSoftware@652 951219d9-93cf-4727-9268-0efd64621fa3

--- slsDetectorSoftware/moenchDetectorServer/svnInfoMoench.h
+++ slsDetectorSoftware/moenchDetectorServer/svnInfoMoench.h
@@ -2,10 +2,10 @@
 #define SVNURL "file:///afs/psi.ch/project/sls_det_software/svn/slsDetectorSoftware/moenchDetectorServer"
 //#define SVNREPPATH ""
 #define SVNREPUUID "951219d9-93cf-4727-9268-0efd64621fa3"
-//#define SVNREV 0x635
+//#define SVNREV 0x649
 //#define SVNKIND ""
 //#define SVNSCHED ""
 #define SVNAUTH "l_maliakal_d"
-#define SVNREV 0x635
-#define SVNDATE 0x20130716
+#define SVNREV 0x649
+#define SVNDATE 0x20130808
 //

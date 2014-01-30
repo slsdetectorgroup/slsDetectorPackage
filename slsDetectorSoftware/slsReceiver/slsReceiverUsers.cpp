382f5652ea2db3678840ea02c3c0690af9d21eba
l_maliakal_d <l_maliakal_d@951219d9-93cf-4727-9268-0efd64621fa3>
2014-01-30 16:41:22 +0000

    make it work for multi threaded compression receiver for moench only
    git-svn-id: file:///afs/psi.ch/project/sls_det_software/svn/slsDetectorSoftware@727 951219d9-93cf-4727-9268-0efd64621fa3

--- slsDetectorSoftware/slsReceiver/slsReceiverUsers.cpp
+++ slsDetectorSoftware/slsReceiver/slsReceiverUsers.cpp
@@ -1,37 +1,39 @@
 #include "slsReceiverUsers.h"
 #include "slsReceiver_funcs.h"
 
+slsReceiverFuncs* slsReceiverUsers::receiver(NULL);
+
 slsReceiverUsers::slsReceiverUsers(int argc, char *argv[], int &success) {
-  receiver=new slsReceiverFuncs(argc, argv, success);
+	slsReceiverUsers::receiver=new slsReceiverFuncs(argc, argv, success);
 }
 
 slsReceiverUsers::~slsReceiverUsers() {
-  delete receiver;
+  delete slsReceiverUsers::receiver;
 }
 
 void slsReceiverUsers::start() {
-  receiver->start();
+	slsReceiverUsers::receiver->start();
 }
 
 
 void slsReceiverUsers::closeFile(int p) {
-  slsReceiverFuncs::closeFile(0);
+	slsReceiverUsers::receiver->closeFile(p);
 }
 
 
 void slsReceiverUsers::registerCallBackStartAcquisition(int (*func)(char*, char*,int, int, void*),void *arg){
-  receiver->registerCallBackStartAcquisition(func,arg);
+	slsReceiverUsers::receiver->registerCallBackStartAcquisition(func,arg);
 }
 
   
 
 void slsReceiverUsers::registerCallBackAcquisitionFinished(void (*func)(int, void*),void *arg){
-  receiver->registerCallBackAcquisitionFinished(func,arg);
+	slsReceiverUsers::receiver->registerCallBackAcquisitionFinished(func,arg);
 }
 	
 
 void slsReceiverUsers::registerCallBackRawDataReady(void (*func)(int, char*, int, FILE*, char*, void*),void *arg){
-  receiver->registerCallBackRawDataReady(func,arg);
+	slsReceiverUsers::receiver->registerCallBackRawDataReady(func,arg);
 }
 
 

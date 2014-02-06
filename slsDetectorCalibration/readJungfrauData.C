6b7d0d8c55f226d6a5ff74e3de1b50dc5b8d4530
bergamaschi <bergamaschi@113b152e-814d-439b-b186-022a431db7b5>
2014-02-06 10:46:11 +0000

    readJungfrauData works
    git-svn-id: file:///afs/psi.ch/project/sls_det_software/svn/slsDetectorCalibration@39 113b152e-814d-439b-b186-022a431db7b5

--- slsDetectorCalibration/readJungfrauData.C
+++ slsDetectorCalibration/readJungfrauData.C
@@ -12,7 +12,7 @@
 
   fout=new TFile("/mnt/slitnas/datadir_jungfrau02/20140131_analysis/test/outfile.root","RECREATE");
 
-  hs2N=jungfrauReadData("/mnt/slitnas/datadir_jungfrau02/20140131_BeamSLS/datadir_310114/18keV_10us_HIGHG0_CDS550_%d.bin","tit",30,40,1500,-500,2500,1,0.,1,159,1,159, 0,1); 
+  hs2N=jungfrauReadData("/mnt/slitnas/datadir_jungfrau02/20140131_BeamSLS/datadir_310114/18keV_10us_HIGHG0_CDS550_%d.bin","tit",30,40,1500,-500,2500,1,0.,1,47,1,47, 0,1); 
   hs2N->SetName("cds_g4");
   hs2N->SetTitle("cds_g4");
   (TH2F*)(hs2N->GetHists()->At(0))->Write();
@@ -23,7 +23,7 @@
  (TH2F*)(hs2N->GetHists()->At(4))->Write();
 
 
-  fout->Close();
+ // fout->Close();
 
 
 

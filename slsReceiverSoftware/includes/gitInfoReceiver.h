21a3b68d812f8a11881c2aeaccd8304f21a5f4a3
Maliakal Dhanya <dhanya.maliakal@psi.ch>
2014-06-03 12:06:57 +0200

    git version history

---
+++ slsReceiverSoftware/includes/gitInfoReceiver.h
@@ -0,0 +1,11 @@
+//#define SVNPATH ""
+#define SVNURL "git@gitorious.psi.ch:sls_det_software/sls_receiver_software.git"
+//#define SVNREPPATH ""
+#define SVNREPUUID "77af4677a040fe113b5115475c18fd27f2453def"
+//#define SVNREV 0x5
+//#define SVNKIND ""
+//#define SVNSCHED ""
+#define SVNAUTH "Maliakal_Dhanya"
+#define SVNREV 0x5
+#define SVNDATE 0x20140528
+//

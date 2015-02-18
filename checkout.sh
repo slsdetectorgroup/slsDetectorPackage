59c3d26c96ea2a8331583aac6ace42f9823266e6
Dhanya Maliakal <dhanya.maliakal@psi.ch>
2015-02-18 13:48:08 +0100

    added script ot checkout

---
+++ checkout.sh
@@ -0,0 +1,11 @@
+#git clone $1@gitorious.psi.ch:sls_det_software/sls_detectors_package.git slsDetectorsPackage
+
+#cd slsDetectorsPackage
+
+git clone $1@gitorious.psi.ch:sls_det_software/sls_detector_software.git slsDetectorSoftware
+
+git clone $1@gitorious.psi.ch:sls_det_software/sls_detector_gui.git slsDetectorGui
+
+git clone $1@gitorious.psi.ch:sls_det_software/sls_receiver_software.git slsReceiverSoftware
+
+git clone $1@gitorious.psi.ch:sls_det_software/sls_detector_calibration.git slsDetectorCalibration

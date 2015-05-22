ee8b0b2ee78e81a4ea81d509b4505f58740a59f8
Dhanya Maliakal <dhanya.maliakal@psi.ch>
2015-05-22 10:22:53 +0200

    knowing which repo

--- statusall.sh
+++ statusall.sh
@@ -1,11 +1,17 @@
+echo "***slsDetectorsPackage";
 git status;
+echo "***slsDetectorSoftware";
 cd slsDetectorSoftware; 
 git status;
+echo "***slsReceiverSoftware";
 cd ../slsReceiverSoftware;
 git status;
+echo "***slsDetectorGui";
 cd ../slsDetectorGui;
 git status;
+echo "***slsDetectorCalibration";
 cd ../slsDetectorCalibration;
 git status;
+echo "***slsImageReconstruction";
 cd ../slsImageReconstruction;
 git status;

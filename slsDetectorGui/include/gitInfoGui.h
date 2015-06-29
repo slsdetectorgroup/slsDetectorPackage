a5e18833de1cda40e0c0dd3bd467c1e1eacb8955
Dhanya Maliakal <dhanya.maliakal@psi.ch>
2015-06-29 16:55:51 +0200

    git rev in

--- slsDetectorGui/include/gitInfoGui.h
+++ slsDetectorGui/include/gitInfoGui.h
@@ -1,11 +1,11 @@
 //#define SVNPATH ""
 #define SVNURL "maliakal_d@gitorious.psi.ch:sls_det_software/sls_detector_gui.git"
 //#define SVNREPPATH ""
-#define SVNREPUUID "d9b88696bfd75c95d13cada581f1901c235e25a7"
-//#define SVNREV 0x299
+#define SVNREPUUID "bc733edfc422efa18796b8b1dd47ad51c852884a"
+//#define SVNREV 0x300
 //#define SVNKIND ""
 //#define SVNSCHED ""
 #define SVNAUTH "Dhanya_Maliakal"
-#define SVNREV 0x299
-#define SVNDATE 0x20150612
+#define SVNREV 0x300
+#define SVNDATE 0x20150616
 //

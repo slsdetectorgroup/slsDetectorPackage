8ca9157882b3a14aa059104eb225f4a7d977ec1c
Dhanya Maliakal <dhanya.maliakal@psi.ch>
2016-02-02 11:37:40 +0100

    corrected checkout script

--- checkout.sh
+++ checkout.sh
@@ -2,13 +2,13 @@
 
 #cd slsDetectorsPackage
 
-git clone $1@git.psi.ch:sls_det_software/sls_detector_software.git slsDetectorSoftware
+git clone $1@git.psi.ch:sls_detectors_software/sls_detector_software.git slsDetectorSoftware
 
-git clone $1@git.psi.ch:sls_det_software/sls_detector_gui.git slsDetectorGui
+git clone $1@git.psi.ch:sls_detectors_software/sls_detector_gui.git slsDetectorGui
 
-git clone $1@git.psi.ch:sls_det_software/sls_receiver_software.git slsReceiverSoftware
+git clone $1@git.psi.ch:sls_detectors_software/sls_receiver_software.git slsReceiverSoftware
 
-git clone $1@git.psi.ch:sls_det_software/sls_detector_calibration.git slsDetectorCalibration
+git clone $1@git.psi.ch:sls_detectors_software/sls_detector_calibration.git slsDetectorCalibration
 
-git clone $1@git.psi.ch:sls_det_software/sls_image_reconstruction.git slsImageReconstruction
+git clone $1@git.psi.ch:sls_detectors_software/sls_image_reconstruction.git slsImageReconstruction
 

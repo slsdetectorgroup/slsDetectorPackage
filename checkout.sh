0ce03226ee6ea549ea891f10fec3fda1d2bfdeca
Dhanya Maliakal <dhanya.maliakal@psi.ch>
2016-02-02 10:42:59 +0100

    changed the checkout script to point to new remote repo

--- checkout.sh
+++ checkout.sh
@@ -1,14 +1,14 @@
-#git clone $1@gitorious.psi.ch:sls_det_software/sls_detectors_package.git slsDetectorsPackage
+#git clone $1@git.psi.ch:sls_det_software/sls_detectors_package.git slsDetectorsPackage
 
 #cd slsDetectorsPackage
 
-git clone $1@gitorious.psi.ch:sls_det_software/sls_detector_software.git slsDetectorSoftware
+git clone $1@git.psi.ch:sls_det_software/sls_detector_software.git slsDetectorSoftware
 
-git clone $1@gitorious.psi.ch:sls_det_software/sls_detector_gui.git slsDetectorGui
+git clone $1@git.psi.ch:sls_det_software/sls_detector_gui.git slsDetectorGui
 
-git clone $1@gitorious.psi.ch:sls_det_software/sls_receiver_software.git slsReceiverSoftware
+git clone $1@git.psi.ch:sls_det_software/sls_receiver_software.git slsReceiverSoftware
 
-git clone $1@gitorious.psi.ch:sls_det_software/sls_detector_calibration.git slsDetectorCalibration
+git clone $1@git.psi.ch:sls_det_software/sls_detector_calibration.git slsDetectorCalibration
 
-git clone $1@gitorious.psi.ch:sls_det_software/sls_image_reconstruction.git slsImageReconstruction
+git clone $1@git.psi.ch:sls_det_software/sls_image_reconstruction.git slsImageReconstruction
 

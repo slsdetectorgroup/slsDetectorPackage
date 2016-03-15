26933bd8bde7a738faea92f974b34e9940d794ad
Dhanya Maliakal <dhanya.maliakal@psi.ch>
2016-03-15 16:50:44 +0100

    fixed undefined gain enum to a large number

--- slsDetectorSoftware/commonFiles/sls_detector_defs.h
+++ slsDetectorSoftware/commonFiles/sls_detector_defs.h
@@ -375,7 +375,7 @@ enum detectorSettings{
   FIXGAIN2,			/**< fix gain 2 */
   FORCESWITCHG1,	/**< force switch gain 1 */
   FORCESWITCHG2,	/**< force switch gain 2 */
-  UNDEFINED,        /**< undefined or custom  settings */
+  UNDEFINED=200,    /**< undefined or custom  settings */
   UNINITIALIZED     /**< uninitialiazed (status at startup) */
 };
 /**

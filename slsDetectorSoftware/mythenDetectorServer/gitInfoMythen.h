9c8137d892458b69ae0b2946c5014f4e738853d6
Dhanya Maliakal <dhanya.maliakal@psi.ch>
2016-07-06 10:32:57 +0200

    updating versions

--- slsDetectorSoftware/mythenDetectorServer/gitInfoMythen.h
+++ slsDetectorSoftware/mythenDetectorServer/gitInfoMythen.h
@@ -1,11 +1,11 @@
 //#define SVNPATH ""
 #define SVNURL "git@git.psi.ch:sls_detectors_software/sls_detector_software.git/mythenDetectorServer"
 //#define SVNREPPATH ""
-#define SVNREPUUID "c23782d2e6e8280eaef10714c8bf00bb7c9c498f"
-//#define SVNREV 0x325
+#define SVNREPUUID "769ba51f2796ac8088f29a6cdb87955befbbdae2"
+//#define SVNREV 0x326
 //#define SVNKIND ""
 //#define SVNSCHED ""
 #define SVNAUTH "Dhanya_Maliakal"
-#define SVNREV 0x325
+#define SVNREV 0x326
 #define SVNDATE 0x20160705
 //
--- slsDetectorSoftware/slsDetector/gitInfoLib.h
+++ slsDetectorSoftware/slsDetector/gitInfoLib.h
@@ -1,11 +1,11 @@
 //#define SVNPATH ""
 #define SVNURLLIB "git@git.psi.ch:sls_detectors_software/sls_detector_software.git"
 //#define SVNREPPATH ""
-#define SVNREPUUIDLIB "c23782d2e6e8280eaef10714c8bf00bb7c9c498f"
-//#define SVNREV 0x1102
+#define SVNREPUUIDLIB "769ba51f2796ac8088f29a6cdb87955befbbdae2"
+//#define SVNREV 0x1103
 //#define SVNKIND ""
 //#define SVNSCHED ""
 #define SVNAUTHLIB "Dhanya_Maliakal"
-#define SVNREVLIB 0x1102
+#define SVNREVLIB 0x1103
 #define SVNDATELIB 0x20160705
 //

20918f2d52f7502ae49f011e5500bacc5220a823
Dhanya Maliakal <dhanya.maliakal@psi.ch>
2016-11-03 13:22:19 +0100

    versioning done

--- slsDetectorSoftware/jungfrauDetectorServer/gitInfoJungfrau.h
+++ slsDetectorSoftware/jungfrauDetectorServer/gitInfoJungfrau.h
@@ -1,11 +1,11 @@
 //#define SVNPATH ""
-#define SVNURL "git@gitorious.psi.ch:sls_det_software/sls_detector_software.git/moenchDetectorServer"
+#define SVNURL "git@git.psi.ch:sls_detectors_software/sls_detector_software.git/jungfrauDetectorServer"
 //#define SVNREPPATH ""
-#define SVNREPUUID "046a469b1e6582c4c55bd6eaeb4818b618d0a9a9"
-//#define SVNREV 0x14
+#define SVNREPUUID "1f1b31ddf2516a8b2fe51c02dc3602b5fe24faa4"
+//#define SVNREV 0x1283
 //#define SVNKIND ""
 //#define SVNSCHED ""
-#define SVNAUTH "Maliakal_Dhanya"
-#define SVNREV 0x14
-#define SVNDATE 0x20140603
+#define SVNAUTH "Dhanya_Maliakal"
+#define SVNREV 0x1283
+#define SVNDATE 0x20161103
 //

c2c7365566bdd29fa2f1def025a4c88b8c4351f9
Dhanya Maliakal <dhanya.maliakal@psi.ch>
2016-11-08 15:23:05 +0100

    works for jungfrau

--- slsDetectorGui/include/gitInfoGui.h
+++ slsDetectorGui/include/gitInfoGui.h
@@ -1,11 +1,11 @@
 //#define SVNPATH ""
 #define SVNURL "git@git.psi.ch:sls_detectors_software/sls_detector_gui.git"
 //#define SVNREPPATH ""
-#define SVNREPUUID "1f62dc79bfedd1210c3eaabe35e2d0f230c841ac"
-//#define SVNREV 0x358
+#define SVNREPUUID "1e52b924e2133e0c453f05d2060e739ff22cf40e"
+//#define SVNREV 0x363
 //#define SVNKIND ""
 //#define SVNSCHED ""
 #define SVNAUTH "Dhanya_Maliakal"
-#define SVNREV 0x358
-#define SVNDATE 0x20161025
+#define SVNREV 0x363
+#define SVNDATE 0x20161108
 //

111f8beab380fc2a88210b0846cd168fa36ad0f9
Dhanya Maliakal <dhanya.maliakal@psi.ch>
2017-02-10 11:51:29 +0100

    updaterev

--- slsDetectorSoftware/eigerDetectorServer/gitInfoEiger.h
+++ slsDetectorSoftware/eigerDetectorServer/gitInfoEiger.h
@@ -1,11 +1,11 @@
 //#define SVNPATH ""
 #define SVNURL "git@git.psi.ch:sls_detectors_software/sls_detector_software.git/eigerDetectorServer"
 //#define SVNREPPATH ""
-#define SVNREPUUID "3d4e56cfec309c4d7a90df164dbfa6132e6757dd"
-//#define SVNREV 0x1324
+#define SVNREPUUID "44df2b221f9f06f7d4361ed0291e026ecbefcab2"
+//#define SVNREV 0x1326
 //#define SVNKIND ""
 //#define SVNSCHED ""
 #define SVNAUTH "Dhanya_Maliakal"
-#define SVNREV 0x1324
-#define SVNDATE 0x20170203
+#define SVNREV 0x1326
+#define SVNDATE 0x20170210
 //
--- slsDetectorSoftware/slsDetector/gitInfoLib.h
+++ slsDetectorSoftware/slsDetector/gitInfoLib.h
@@ -1,11 +1,11 @@
 //#define SVNPATH ""
 #define SVNURLLIB "git@git.psi.ch:sls_detectors_software/sls_detector_software.git"
 //#define SVNREPPATH ""
-#define SVNREPUUIDLIB "3d4e56cfec309c4d7a90df164dbfa6132e6757dd"
-//#define SVNREV 0x1324
+#define SVNREPUUIDLIB "44df2b221f9f06f7d4361ed0291e026ecbefcab2"
+//#define SVNREV 0x1326
 //#define SVNKIND ""
 //#define SVNSCHED ""
 #define SVNAUTHLIB "Dhanya_Maliakal"
-#define SVNREVLIB 0x1324
-#define SVNDATELIB 0x20170203
+#define SVNREVLIB 0x1326
+#define SVNDATELIB 0x20170210
 //

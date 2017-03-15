dc77b07c92c7f72eac56a0165df221887a7b8f00
Dhanya Maliakal <dhanya.maliakal@psi.ch>
2017-03-15 15:02:22 +0100

    update rev

--- slsDetectorSoftware/eigerDetectorServer/gitInfoEiger.h
+++ slsDetectorSoftware/eigerDetectorServer/gitInfoEiger.h
@@ -1,11 +1,11 @@
 //#define SVNPATH ""
 #define SVNURL "git@git.psi.ch:sls_detectors_software/sls_detector_software.git/eigerDetectorServer"
 //#define SVNREPPATH ""
-#define SVNREPUUID "44df2b221f9f06f7d4361ed0291e026ecbefcab2"
-//#define SVNREV 0x1326
+#define SVNREPUUID "d377e39dfb25b3d8a11652bf6ede0f185c86bd21"
+//#define SVNREV 0x1333
 //#define SVNKIND ""
 //#define SVNSCHED ""
 #define SVNAUTH "Dhanya_Maliakal"
-#define SVNREV 0x1326
-#define SVNDATE 0x20170210
+#define SVNREV 0x1333
+#define SVNDATE 0x20170315
 //

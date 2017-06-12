7f01ccc83f3f0faa3e0ebb33ce22b8cb6c591f4e
Dhanya Maliakal <dhanya.maliakal@psi.ch>
2017-06-12 18:53:42 +0200

    updaterev

--- slsReceiverSoftware/include/gitInfoReceiver.h
+++ slsReceiverSoftware/include/gitInfoReceiver.h
@@ -1,11 +1,11 @@
 //#define SVNPATH ""
 #define SVNURL "git@git.psi.ch:sls_detectors_software/sls_receiver_software.git"
 //#define SVNREPPATH ""
-#define SVNREPUUID "a453c1d55d73fecc18af9df6ada21621a989ee4b"
-//#define SVNREV 0x514
+#define SVNREPUUID "9aa99d67a76f6c42c68ed12dbfa0a5b2acbc2595"
+//#define SVNREV 0x586
 //#define SVNKIND ""
 //#define SVNSCHED ""
 #define SVNAUTH "Dhanya_Maliakal"
-#define SVNREV 0x514
-#define SVNDATE 0x20170324
+#define SVNREV 0x586
+#define SVNDATE 0x20170612
 //

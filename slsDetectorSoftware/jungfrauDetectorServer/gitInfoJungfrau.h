a46a8cc68622774e13b213a9c432d809ca2badb4
Dhanya Maliakal <dhanya.maliakal@psi.ch>
2017-06-14 11:41:11 +0200

    updaterev

--- slsDetectorSoftware/jungfrauDetectorServer/gitInfoJungfrau.h
+++ slsDetectorSoftware/jungfrauDetectorServer/gitInfoJungfrau.h
@@ -1,11 +1,11 @@
 //#define SVNPATH ""
 #define SVNURL "git@git.psi.ch:sls_detectors_software/sls_detector_software.git/jungfrauDetectorServer"
 //#define SVNREPPATH ""
-#define SVNREPUUID "b20194f3117746fd4e5eaba268bc477213fa3090"
-//#define SVNREV 0x1409
+#define SVNREPUUID "49f4f7295411e3dbcf00fd93ca821eb95e13771c"
+//#define SVNREV 0x1411
 //#define SVNKIND ""
 //#define SVNSCHED ""
 #define SVNAUTH "Dhanya_Maliakal"
-#define SVNREV 0x1409
+#define SVNREV 0x1411
 #define SVNDATE 0x20170614
 //
--- slsDetectorSoftware/slsDetector/gitInfoLib.h
+++ slsDetectorSoftware/slsDetector/gitInfoLib.h
@@ -1,11 +1,11 @@
 //#define SVNPATH ""
 #define SVNURLLIB "git@git.psi.ch:sls_detectors_software/sls_detector_software.git"
 //#define SVNREPPATH ""
-#define SVNREPUUIDLIB "b20194f3117746fd4e5eaba268bc477213fa3090"
-//#define SVNREV 0x1409
+#define SVNREPUUIDLIB "49f4f7295411e3dbcf00fd93ca821eb95e13771c"
+//#define SVNREV 0x1411
 //#define SVNKIND ""
 //#define SVNSCHED ""
 #define SVNAUTHLIB "Dhanya_Maliakal"
-#define SVNREVLIB 0x1409
+#define SVNREVLIB 0x1411
 #define SVNDATELIB 0x20170614
 //

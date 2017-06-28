6bfbb075c85c80d66bf5b623372fe16ddea47bff
Dhanya Maliakal <dhanya.maliakal@psi.ch>
2017-06-28 18:18:30 +0200

    ensuring fiel permissions

--- slsReceiverSoftware/include/gitInfoReceiver.h
+++ slsReceiverSoftware/include/gitInfoReceiver.h
@@ -1,11 +1,11 @@
 //#define SVNPATH ""
 #define SVNURL "git@git.psi.ch:sls_detectors_software/sls_receiver_software.git"
 //#define SVNREPPATH ""
-#define SVNREPUUID "7a69378574427de0c5cd9a1b86231c47732829a8"
-//#define SVNREV 0x599
+#define SVNREPUUID "36fc5833daba78a6e4daf7c3fca3199a4eb75e1c"
+//#define SVNREV 0x600
 //#define SVNKIND ""
 //#define SVNSCHED ""
 #define SVNAUTH "Dhanya_Maliakal"
-#define SVNREV 0x599
+#define SVNREV 0x600
 #define SVNDATE 0x20170628
 //

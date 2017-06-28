1a74ceca3e0b79874588386929601f6c18c4f088
Dhanya Maliakal <dhanya.maliakal@psi.ch>
2017-06-28 18:16:55 +0200

    ensuring file permissions

--- slsDetectorSoftware/slsDetector/gitInfoLib.h
+++ slsDetectorSoftware/slsDetector/gitInfoLib.h
@@ -1,11 +1,11 @@
 //#define SVNPATH ""
 #define SVNURLLIB "git@git.psi.ch:sls_detectors_software/sls_detector_software.git"
 //#define SVNREPPATH ""
-#define SVNREPUUIDLIB "3e403d7a4da76b6edc9db16656580bcc7b5302e1"
-//#define SVNREV 0x1433
+#define SVNREPUUIDLIB "f6589f7e6e60aa6f02e3adcf526b2a1a73153364"
+//#define SVNREV 0x1434
 //#define SVNKIND ""
 //#define SVNSCHED ""
 #define SVNAUTHLIB "Dhanya_Maliakal"
-#define SVNREVLIB 0x1433
+#define SVNREVLIB 0x1434
 #define SVNDATELIB 0x20170628
 //

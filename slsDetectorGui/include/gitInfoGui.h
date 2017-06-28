cfff7969e35ce466baddc54103f5dad587269e04
Dhanya Maliakal <dhanya.maliakal@psi.ch>
2017-06-28 18:52:43 +0200

    changes for Makefile dependencies

--- slsDetectorGui/include/gitInfoGui.h
+++ slsDetectorGui/include/gitInfoGui.h
@@ -1,11 +1,11 @@
 //#define SVNPATH ""
 #define SVNURL "git@git.psi.ch:sls_detectors_software/sls_detector_gui.git"
 //#define SVNREPPATH ""
-#define SVNREPUUID "7910e5772fc18bee7e706d6a57ccdc8b9c353d18"
-//#define SVNREV 0x393
+#define SVNREPUUID "6ceaa18c5d55e94339999977a91fca1d8712578d"
+//#define SVNREV 0x395
 //#define SVNKIND ""
 //#define SVNSCHED ""
 #define SVNAUTH "Dhanya_Maliakal"
-#define SVNREV 0x393
-#define SVNDATE 0x20170623
+#define SVNREV 0x395
+#define SVNDATE 0x20170628
 //

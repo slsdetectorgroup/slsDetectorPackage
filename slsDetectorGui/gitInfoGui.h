bd9f19f6c7c33a31eafc4404e3b3f8465371d063
Dhanya Maliakal <dhanya.maliakal@psi.ch>
2017-06-30 12:20:42 +0200

    changed the makefile a bit to find libraries

--- slsDetectorGui/gitInfoGui.h
+++ slsDetectorGui/gitInfoGui.h
@@ -1,11 +1,11 @@
 //#define SVNPATH ""
 #define SVNURL "git@git.psi.ch:sls_detectors_software/sls_detector_gui.git"
 //#define SVNREPPATH ""
-#define SVNREPUUID "9794596be92a600d19395eb6bf7818ae5e0a68f7"
-//#define SVNREV 0x398
+#define SVNREPUUID "0da39fb3680d6eb9d8a9871cb9e6a152ee638046"
+//#define SVNREV 0x399
 //#define SVNKIND ""
 //#define SVNSCHED ""
 #define SVNAUTH "Dhanya_Maliakal"
-#define SVNREV 0x398
+#define SVNREV 0x399
 #define SVNDATE 0x20170629
 //

1e50bc4cc09708d9f8bde94245ef4ac6120c2941
Dhanya Maliakal <dhanya.maliakal@psi.ch>
2017-07-03 17:44:52 +0200

    conflict git

--- slsDetectorSoftware/slsDetector/gitInfoLib.h
+++ slsDetectorSoftware/slsDetector/gitInfoLib.h
@@ -1,20 +1,11 @@
 //#define SVNPATH ""
 #define SVNURLLIB "git@git.psi.ch:sls_detectors_software/sls_detector_software.git"
 //#define SVNREPPATH ""
-<<<<<<< HEAD
-#define SVNREPUUIDLIB "f296ac9eb68e7681675cad9943f5054eb7f8bdce"
-//#define SVNREV 0x1438
+#define SVNREPUUIDLIB "79271ea7e3f5ac7113582e694b30b71a64d18844"
+//#define SVNREV 0x1441
 //#define SVNKIND ""
 //#define SVNSCHED ""
 #define SVNAUTHLIB "Dhanya_Maliakal"
-#define SVNREVLIB 0x1438
-=======
-#define SVNREPUUIDLIB "788060fd5a954e42fea270b25b8a398293013e96"
-//#define SVNREV 0x1437
-//#define SVNKIND ""
-//#define SVNSCHED ""
-#define SVNAUTHLIB "Dhanya_Maliakal"
-#define SVNREVLIB 0x1437
->>>>>>> developer
+#define SVNREVLIB 0x1441
 #define SVNDATELIB 0x20170703
 //

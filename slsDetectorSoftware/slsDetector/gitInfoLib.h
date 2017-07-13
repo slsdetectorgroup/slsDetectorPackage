04165a9da8ffb194f3c94e55d0ab1dc1f9572f2e
Dhanya Maliakal <dhanya.maliakal@psi.ch>
2017-07-13 14:44:34 +0200

    rest implementation, index issues

--- slsDetectorSoftware/slsDetector/gitInfoLib.h
+++ slsDetectorSoftware/slsDetector/gitInfoLib.h
@@ -1,11 +1,11 @@
 //#define SVNPATH ""
 #define SVNURLLIB "git@git.psi.ch:sls_detectors_software/sls_detector_software.git"
 //#define SVNREPPATH ""
-#define SVNREPUUIDLIB "5719fa8e7143d444cc9ca6ba81444541b179e317"
-//#define SVNREV 0x1445
+#define SVNREPUUIDLIB "5380c93aa2acce275be11dd519cb49ad2a5a15d5"
+//#define SVNREV 0x1447
 //#define SVNKIND ""
 //#define SVNSCHED ""
 #define SVNAUTHLIB "Dhanya_Maliakal"
-#define SVNREVLIB 0x1445
-#define SVNDATELIB 0x20170711
+#define SVNREVLIB 0x1447
+#define SVNDATELIB 0x20170713
 //

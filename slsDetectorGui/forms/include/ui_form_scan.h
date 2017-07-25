5771a3c7225a0d01c6c55cbe93479be333ceaadf
Dhanya Maliakal <dhanya.maliakal@psi.ch>
2017-07-25 13:56:27 +0200

    includes in gui

--- slsDetectorGui/forms/include/ui_form_scan.h
+++ slsDetectorGui/forms/include/ui_form_scan.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading UI file 'form_scan.ui'
 **
-** Created: Thu Jul 13 14:40:29 2017
+** Created: Tue Jul 25 12:31:25 2017
 **      by: Qt User Interface Compiler version 4.8.2
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
--- slsDetectorGui/include/gitInfoGui.h
+++ slsDetectorGui/include/gitInfoGui.h
@@ -1,11 +1,11 @@
 //#define SVNPATH ""
 #define SVNURL "git@git.psi.ch:sls_detectors_software/sls_detector_gui.git"
 //#define SVNREPPATH ""
-#define SVNREPUUID "fb43f6d8d9f933472bc04dd482911a54cb7e021c"
-//#define SVNREV 0x406
+#define SVNREPUUID "9811b0e5522d40b4d6bafa91b5f5b24ed558f807"
+//#define SVNREV 0x408
 //#define SVNKIND ""
 //#define SVNSCHED ""
 #define SVNAUTH "Dhanya_Maliakal"
-#define SVNREV 0x406
-#define SVNDATE 0x20170713
+#define SVNREV 0x408
+#define SVNDATE 0x20170725
 //

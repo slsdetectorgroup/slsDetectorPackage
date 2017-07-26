91f8987b6c3f09ecfb5dc65285390d49fd0eb2c1
Dhanya Maliakal <dhanya.maliakal@psi.ch>
2017-07-26 11:12:56 +0200

    removed ui include files as they are generated anyway for every make

--- slsDetectorGui/include/gitInfoGui.h
+++ slsDetectorGui/include/gitInfoGui.h
@@ -1,11 +1,11 @@
 //#define SVNPATH ""
 #define SVNURL "git@git.psi.ch:sls_detectors_software/sls_detector_gui.git"
 //#define SVNREPPATH ""
-#define SVNREPUUID "9811b0e5522d40b4d6bafa91b5f5b24ed558f807"
-//#define SVNREV 0x408
+#define SVNREPUUID "a391613c47abcacccd7690300cc70c4b4cd5aad4"
+//#define SVNREV 0x409
 //#define SVNKIND ""
 //#define SVNSCHED ""
 #define SVNAUTH "Dhanya_Maliakal"
-#define SVNREV 0x408
+#define SVNREV 0x409
 #define SVNDATE 0x20170725
 //

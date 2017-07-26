915fd41bf6fb72c7530ef8d5d9030e732abbd88c
Dhanya Maliakal <dhanya.maliakal@psi.ch>
2017-07-26 15:09:24 +0200

    should be able to compile in pcmoench as l_msdetect

--- slsDetectorGui/include/gitInfoGui.h
+++ slsDetectorGui/include/gitInfoGui.h
@@ -1,11 +1,11 @@
 //#define SVNPATH ""
 #define SVNURL "git@git.psi.ch:sls_detectors_software/sls_detector_gui.git"
 //#define SVNREPPATH ""
-#define SVNREPUUID "a391613c47abcacccd7690300cc70c4b4cd5aad4"
-//#define SVNREV 0x409
+#define SVNREPUUID "0feb7f734e1ef261dcc82d13df4a5caaf90917f4"
+//#define SVNREV 0x411
 //#define SVNKIND ""
 //#define SVNSCHED ""
 #define SVNAUTH "Dhanya_Maliakal"
-#define SVNREV 0x409
-#define SVNDATE 0x20170725
+#define SVNREV 0x411
+#define SVNDATE 0x20170726
 //

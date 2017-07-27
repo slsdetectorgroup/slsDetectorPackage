c7d4582efa275f77c7203ad81f79d30f6f0e067f
Dhanya Maliakal <dhanya.maliakal@psi.ch>
2017-07-27 12:08:28 +0200

    versioning

--- slsDetectorSoftware/slsDetector/gitInfoLib.h
+++ slsDetectorSoftware/slsDetector/gitInfoLib.h
@@ -1,11 +1,11 @@
 //#define SVNPATH ""
 #define SVNURLLIB "git@git.psi.ch:sls_detectors_software/sls_detector_software.git"
 //#define SVNREPPATH ""
-#define SVNREPUUIDLIB "a80690be016d1f51db5e50c3db27b998af9ba6c8"
-//#define SVNREV 0x1449
+#define SVNREPUUIDLIB "2874d09e1ddd257e87cbcf7f4d8bb6e7ef69d99f"
+//#define SVNREV 0x1450
 //#define SVNKIND ""
 //#define SVNSCHED ""
 #define SVNAUTHLIB "Dhanya_Maliakal"
-#define SVNREVLIB 0x1449
-#define SVNDATELIB 0x20170726
+#define SVNREVLIB 0x1450
+#define SVNDATELIB 0x20170727
 //

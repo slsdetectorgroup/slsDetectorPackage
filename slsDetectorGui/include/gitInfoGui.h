c31ee2d76fe272702ac216c90e73cf19eba1a6b8
Dhanya Maliakal <dhanya.maliakal@psi.ch>
2017-08-04 14:20:51 +0200

    allowing -1 in gui for spinbox

--- slsDetectorGui/include/gitInfoGui.h
+++ slsDetectorGui/include/gitInfoGui.h
@@ -1,11 +1,11 @@
 //#define SVNPATH ""
 #define SVNURL "git@git.psi.ch:sls_detectors_software/sls_detector_gui.git"
 //#define SVNREPPATH ""
-#define SVNREPUUID "3d4a8bf5fe9083caedea4cac98ce487cd7608edd"
-//#define SVNREV 0x412
+#define SVNREPUUID "b7e546443bc168440632d8aa96e683bc4deada49"
+//#define SVNREV 0x413
 //#define SVNKIND ""
 //#define SVNSCHED ""
 #define SVNAUTH "Dhanya_Maliakal"
-#define SVNREV 0x412
-#define SVNDATE 0x20170727
+#define SVNREV 0x413
+#define SVNDATE 0x20170803
 //

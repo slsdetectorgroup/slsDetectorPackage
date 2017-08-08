ccc60dacf4fbd324c01120ebbfd876e871d20458
Dhanya Maliakal <dhanya.maliakal@psi.ch>
2017-08-08 16:24:33 +0200

    updated release.txt and the cmk.sh help script

--- cmk.sh
+++ cmk.sh
@@ -17,6 +17,29 @@ Usage: $0 [-c] [-b] [-h] [-d <HDF5 directory>] [-j]
  -h: Builds/Rebuilds Cmake files with HDF5 package
  -d: HDF5 Custom Directory
  -j: Number of threads to compile through
+ 
+For only make:
+./cmk.sh
+
+For make clean;make:
+./cmk.sh -c
+
+For using hdf5 without default dir /opt/hdf5v1.10.0:
+./cmk.sh -h
+
+For using hdf5 without custom dir /blabla:
+./cmk.sh -h -d /blabla
+
+For rebuilding cmake without hdf5 (Use this if you had previously run with hdf5 and now you dont want it)
+./cmk.sh -b
+
+For using multiple cores to compile faster:
+(all these options work)
+./cmk.sh -j9
+./cmk.sh -cj9 #with clean
+./cmk.sh -hj9 #with hdf5
+./cmk.sh -j9 -h #with hdf
+ 
  " ; exit 1; }
 
 while getopts ":bchd:j:" opt ; do

ec4be4f2c095cfc9f6532a981ced9de877696793
Dhanya Maliakal <dhanya.maliakal@psi.ch>
2017-08-18 16:11:49 +0200

    bugfix: framesl was not conected, exptime default time is 10 us

--- slsDetectorSoftware/jungfrauDetectorServer/gitInfoJungfrau.h
+++ slsDetectorSoftware/jungfrauDetectorServer/gitInfoJungfrau.h
@@ -1,11 +1,11 @@
 //#define SVNPATH ""
 #define SVNURL "git@git.psi.ch:sls_detectors_software/sls_detector_software.git"
 //#define SVNREPPATH ""
-#define SVNREPUUID "9f1b82c18ab0893d65bfadeb646b8ea244614632"
-//#define SVNREV 0x1443
+#define SVNREPUUID "c58dfe801770ceb63a9baab0be5b4cd16794b57e"
+//#define SVNREV 0x1495
 //#define SVNKIND ""
 //#define SVNSCHED ""
 #define SVNAUTH "Dhanya_Maliakal"
-#define SVNREV 0x1443
-#define SVNDATE 0x20170710
+#define SVNREV 0x1495
+#define SVNDATE 0x20170815
 //

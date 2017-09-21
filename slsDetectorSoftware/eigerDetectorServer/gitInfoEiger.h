c2f6e5b20cefe1eadd6fef921b75f0b1b378f873
Dhanya Maliakal <dhanya.maliakal@psi.ch>
2017-09-21 17:58:10 +0200

    modifying to adjust to common basic tests debug mode for jungfrau

--- slsDetectorSoftware/eigerDetectorServer/gitInfoEiger.h
+++ slsDetectorSoftware/eigerDetectorServer/gitInfoEiger.h
@@ -1,11 +1,11 @@
 //#define SVNPATH ""
 #define SVNURL "git@git.psi.ch:sls_detectors_software/sls_detector_software.git"
 //#define SVNREPPATH ""
-#define SVNREPUUID "3c1eb747d1930d6d38030a5a607f72d3b58a7a21"
-//#define SVNREV 0x1549
+#define SVNREPUUID "8b025fcf83af16926960d0d03c6b0c9562fdc74c"
+//#define SVNREV 0x1584
 //#define SVNKIND ""
 //#define SVNSCHED ""
 #define SVNAUTH "Dhanya_Maliakal"
-#define SVNREV 0x1549
-#define SVNDATE 0x20170830
+#define SVNREV 0x1584
+#define SVNDATE 0x20170921
 //

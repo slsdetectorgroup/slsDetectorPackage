8585a536ec06a4c80f5bcaed6e632e034973828e
Dhanya Thattil <dhanya.thattil@psi.ch>
2017-10-12 11:42:37 +0200

    fixed eiger server status problem, updating binary in next commit

--- slsDetectorSoftware/eigerDetectorServer/gitInfoEiger.h
+++ slsDetectorSoftware/eigerDetectorServer/gitInfoEiger.h
@@ -1,11 +1,11 @@
 //#define SVNPATH ""
 #define SVNURL "git@git.psi.ch:sls_detectors_software/sls_detector_software.git"
 //#define SVNREPPATH ""
-#define SVNREPUUID "4c02a99589af6ae56ba5819da50f1e567057393a"
-//#define SVNREV 0x1572
+#define SVNREPUUID "a161a9aad8ac38a79ba43340344a3c923cea85f7"
+//#define SVNREV 0x1578
 //#define SVNKIND ""
 //#define SVNSCHED ""
-#define SVNAUTH "Dhanya_Maliakal"
-#define SVNREV 0x1572
-#define SVNDATE 0x20171006
+#define SVNAUTH "Dhanya_Thattil"
+#define SVNREV 0x1578
+#define SVNDATE 0x20171011
 //

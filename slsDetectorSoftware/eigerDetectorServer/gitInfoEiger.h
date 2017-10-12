d2767afa7ca34b4bf373520d6b803df601728145
Dhanya Maliakal <dhanya.maliakal@psi.ch>
2017-10-12 14:57:12 +0200

    added the eiger server binary with merges from 3.0.1

--- slsDetectorSoftware/eigerDetectorServer/gitInfoEiger.h
+++ slsDetectorSoftware/eigerDetectorServer/gitInfoEiger.h
@@ -1,11 +1,11 @@
 //#define SVNPATH ""
 #define SVNURL "git@git.psi.ch:sls_detectors_software/sls_detector_software.git"
 //#define SVNREPPATH ""
-#define SVNREPUUID "a161a9aad8ac38a79ba43340344a3c923cea85f7"
-//#define SVNREV 0x1578
+#define SVNREPUUID "77d9fda91fa82c2b4259d64ca9d4d6b4fa4b647c"
+//#define SVNREV 0x1607
 //#define SVNKIND ""
 //#define SVNSCHED ""
 #define SVNAUTH "Dhanya_Thattil"
-#define SVNREV 0x1578
-#define SVNDATE 0x20171011
+#define SVNREV 0x1607
+#define SVNDATE 0x20171012
 //

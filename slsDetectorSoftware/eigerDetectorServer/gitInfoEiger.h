eb78ae0fe11919e76a7ac00dd9a62b67f165c5c2
Dhanya Maliakal <dhanya.maliakal@psi.ch>
2017-10-12 11:44:46 +0200

    new eiger server binary with status fixed

--- slsDetectorSoftware/eigerDetectorServer/gitInfoEiger.h
+++ slsDetectorSoftware/eigerDetectorServer/gitInfoEiger.h
@@ -1,11 +1,11 @@
 //#define SVNPATH ""
 #define SVNURL "git@git.psi.ch:sls_detectors_software/sls_detector_software.git"
 //#define SVNREPPATH ""
-#define SVNREPUUID "4c02a99589af6ae56ba5819da50f1e567057393a"
-//#define SVNREV 0x1572
+#define SVNREPUUID "2d3496137e3bbb8953fe380838e0abe278f37770"
+//#define SVNREV 0x1575
 //#define SVNKIND ""
 //#define SVNSCHED ""
-#define SVNAUTH "Dhanya_Maliakal"
-#define SVNREV 0x1572
-#define SVNDATE 0x20171006
+#define SVNAUTH "Dhanya_Thattil"
+#define SVNREV 0x1575
+#define SVNDATE 0x20171011
 //

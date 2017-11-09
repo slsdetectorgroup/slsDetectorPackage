fb0afc282d5e1bb497e14db90c3bbb5954faa6d9
Dhanya Maliakal <dhanya.maliakal@psi.ch>
2017-11-09 14:01:26 +0100

    gotthard server, too fast now sets stopped bit high and stops acquisition, so polling status also made aware of stopped bit

--- slsDetectorSoftware/gotthardDetectorServer/gitInfoGotthard.h
+++ slsDetectorSoftware/gotthardDetectorServer/gitInfoGotthard.h
@@ -1,11 +1,11 @@
 //#define SVNPATH ""
 #define SVNURL "git@git.psi.ch:sls_detectors_software/sls_detector_software.git"
 //#define SVNREPPATH ""
-#define SVNREPUUID "eb72e8f69f5c87f9330741e0d07fd48b9d937e09"
-//#define SVNREV 0x1584
+#define SVNREPUUID "c5777e517c451226e3275e6948895baee21a1f94"
+//#define SVNREV 0x1585
 //#define SVNKIND ""
 //#define SVNSCHED ""
 #define SVNAUTH "Dhanya_Maliakal"
-#define SVNREV 0x1584
-#define SVNDATE 0x20171108
+#define SVNREV 0x1585
+#define SVNDATE 0x20171109
 //

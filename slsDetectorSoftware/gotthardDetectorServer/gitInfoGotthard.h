d24ab31d872e876e9be6aa38270f5011c8dc27f1
Dhanya Maliakal <dhanya.maliakal@psi.ch>
2017-11-09 13:44:38 +0100

    gotthard fix

--- slsDetectorSoftware/gotthardDetectorServer/gitInfoGotthard.h
+++ slsDetectorSoftware/gotthardDetectorServer/gitInfoGotthard.h
@@ -1,11 +1,11 @@
 //#define SVNPATH ""
 #define SVNURL "git@git.psi.ch:sls_detectors_software/sls_detector_software.git"
 //#define SVNREPPATH ""
-#define SVNREPUUID "e4fa807b5d987714a047b9d2cac271c89f6c2fef"
-//#define SVNREV 0x1487
+#define SVNREPUUID "eb72e8f69f5c87f9330741e0d07fd48b9d937e09"
+//#define SVNREV 0x1584
 //#define SVNKIND ""
 //#define SVNSCHED ""
 #define SVNAUTH "Dhanya_Maliakal"
-#define SVNREV 0x1487
-#define SVNDATE 0x20170816
+#define SVNREV 0x1584
+#define SVNDATE 0x20171108
 //

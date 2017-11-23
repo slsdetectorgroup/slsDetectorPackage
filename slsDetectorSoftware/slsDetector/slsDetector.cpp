16e4fbf8dd98d80c3549904f870eea2029d8ce0e
Dhanya Maliakal <dhanya.maliakal@psi.ch>
2017-11-23 18:21:54 +0100

    receiver read freq should save value in shm even if no receiver present

--- slsDetectorSoftware/slsDetector/slsDetector.cpp
+++ slsDetectorSoftware/slsDetector/slsDetector.cpp
@@ -780,6 +780,7 @@ int slsDetector::initializeDetectorSize(detectorType type) {
     thisDetector->zmqport = 0;
     thisDetector->receiver_zmqport = 0;
     thisDetector->receiver_datastream = false;
+    thisDetector->receiver_read_freq = 0;
 
     for (int ia=0; ia<MAX_ACTIONS; ++ia) {
       strcpy(thisDetector->actionScript[ia],"none");
@@ -6077,6 +6078,7 @@ string slsDetector::setReceiver(string receiverIP){
 		std::cout << "dynamic range:" << thisDetector->dynamicRange << endl << endl;
 		std::cout << "flippeddatax:" << thisDetector->flippedData[d] << endl;
 		std::cout << "10GbE:" << thisDetector->tenGigaEnable << endl << endl;
+		std::cout << "r_readfreq:" << thisDetector->receiver_read_freq << endl << endl;
 		std::cout << "rx streaming port:" << thisDetector->receiver_zmqport << endl;
 
 		//std::cout << "dataStreaming:" << enableDataStreamingFromReceiver(-1) << endl << endl;
@@ -6122,6 +6124,7 @@ string slsDetector::setReceiver(string receiverIP){
 				enableTenGigabitEthernet(thisDetector->tenGigaEnable);
 
 			// data streaming
+			setReadReceiverFrequency(thisDetector->receiver_read_freq);
 			setReceiverStreamingPort(getReceiverStreamingPort());
 			enableDataStreamingFromReceiver(enableDataStreamingFromReceiver(-1));
 		}
@@ -8270,6 +8273,10 @@ int slsDetector::updateReceiverNoWait() {
   parentDet->enableOverwriteMask(ind);
   pthread_mutex_unlock(&ms);
 
+  // receiver read frequency
+  n += 	dataSocket->ReceiveDataOnly(&ind,sizeof(ind));
+  thisDetector->receiver_read_freq = ind;
+
   // receiver streaming port
   n += 	dataSocket->ReceiveDataOnly(&ind,sizeof(ind));
   thisDetector->receiver_zmqport = ind;
@@ -8510,34 +8517,35 @@ int64_t slsDetector::clearAllErrorMask(){
 
 
 
-int slsDetector::setReadReceiverFrequency(int getFromReceiver, int freq){
-	int fnum=F_READ_RECEIVER_FREQUENCY;
-	int ret = FAIL;
-	int retval=-1;
-	int arg = freq;
+int slsDetector::setReadReceiverFrequency(int freq){
 
-	if(!getFromReceiver)
-		return retval;
+	if (freq >= 0) {
+		thisDetector->receiver_read_freq = freq;
 
-	if(thisDetector->receiverOnlineFlag==ONLINE_FLAG){
+		int fnum=F_READ_RECEIVER_FREQUENCY;
+		int ret = FAIL;
+		int retval=-1;
+		int arg = freq;
+
+		if(thisDetector->receiverOnlineFlag==ONLINE_FLAG){
 #ifdef VERBOSE
-		std::cout << "Sending read frequency to receiver " << arg  << std::endl;
+			std::cout << "Sending read frequency to receiver " << arg  << std::endl;
 #endif
-		if (connectData() == OK){
-			ret=thisReceiver->sendInt(fnum,retval,arg);
-			disconnectData();
+			if (connectData() == OK){
+				ret=thisReceiver->sendInt(fnum,retval,arg);
+				disconnectData();
+			}
+			if((ret == FAIL) || (retval != freq)) {
+				cout << "could not set receiver read frequency to " << freq <<" Returned:" << retval << endl;
+				setErrorMask((getErrorMask())|(RECEIVER_READ_FREQUENCY));
+			}
+
+			if(ret==FORCE_UPDATE)
+				updateReceiver();
 		}
-		if(ret==FAIL)
-			retval = -1;
-		if(ret==FORCE_UPDATE)
-			updateReceiver();
 	}
 
-	if ((freq > 0) && (retval != freq)){
-		cout << "could not set receiver read frequency to " << freq <<" Returned:" << retval << endl;
-		setErrorMask((getErrorMask())|(RECEIVER_READ_FREQUENCY));
-	}
-	return retval;
+	return thisDetector->receiver_read_freq;
 }
 
 
--- slsDetectorSoftware/slsDetector/slsDetector.h
+++ slsDetectorSoftware/slsDetector/slsDetector.h
@@ -275,6 +275,8 @@ class slsDetector : public slsDetectorUtils, public energyConversion {
     int receiver_zmqport;
     /** data streaming (up stream) enable in receiver */
     bool receiver_datastream;
+    /* Receiver read frequency */
+    int receiver_read_freq;
 
   } sharedSlsDetector;
 
@@ -1766,12 +1768,10 @@ class slsDetector : public slsDetectorUtils, public energyConversion {
   /** Sets the read receiver frequency
    	  if data required from receiver randomly readRxrFrequency=0,
    	   else every nth frame to be sent to gui
-   	   @param getFromReceiver is 1 if it should ask the receiver,
-   	   	   0 if it can get it from multi structure
    	   @param freq is the receiver read frequency
    	   /returns read receiver frequency
    */
-  int setReadReceiverFrequency(int getFromReceiver, int freq=-1);
+  int setReadReceiverFrequency(int freq=-1);
 
   /** Sets the read receiver timer
    	  if data required from receiver randomly readRxrFrequency=0,

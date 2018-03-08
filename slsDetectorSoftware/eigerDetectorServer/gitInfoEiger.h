fee6465213ba06768438b08f4940fd6b83da1ab1
Dhanya Thattil <dhanya.thattil@psi.ch>
2018-03-08 16:46:15 +0100

    eiger and jungfrau servers int to uint32_t for read and write registers

--- slsDetectorSoftware/eigerDetectorServer/gitInfoEiger.h
+++ slsDetectorSoftware/eigerDetectorServer/gitInfoEiger.h
@@ -1,6 +1,6 @@
 #define GITURL "git@github.com:slsdetectorgroup/slsDetectorPackage.git"
-#define GITREPUUID "12e4530702b836985408bc1e85ddb0d996a0153b"
-#define GITAUTH "Dhanya_Thattil"
-#define GITREV 0x3445
-#define GITDATE 0x20180223
+#define GITREPUUID "8b02aa9e91bbee0470492dcf1ff67415851e14fa"
+#define GITAUTH "Erik_Frojdh"
+#define GITREV 0x3455
+#define GITDATE 0x20180308
 #define GITBRANCH "3.1.0-rc"

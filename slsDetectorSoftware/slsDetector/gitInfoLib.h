688dd15fe9ee45ae3048b3b52918da5f31283a22
Dhanya Thattil <dhanya.thattil@psi.ch>
2018-03-20 12:12:38 +0100

    Jungfrau bug fix: cannot give same adcphase value to pll, updated recommended default of adcphase for half speed and quarter speed, updated client manuals for both adcphase and clkdivider

--- slsDetectorSoftware/slsDetector/gitInfoLib.h
+++ slsDetectorSoftware/slsDetector/gitInfoLib.h
@@ -1,6 +1,6 @@
 #define GITURL "git@github.com:slsdetectorgroup/slsDetectorPackage.git"
-#define GITREPUUID "b389402d647e939342dbb2c9317c688c0eb6b105"
-#define GITAUTH "Erik_Frojdh"
-#define GITREV 0x3458
-#define GITDATE 0x20180312
-#define GITBRANCH "3.1.0-rc"
+#define GITREPUUID "85aa47b8353bb84f52c435a967a03ddf46af5dd1"
+#define GITAUTH "Dhanya_Thattil"
+#define GITREV 0x3465
+#define GITDATE 0x20180319
+#define GITBRANCH "3.1.1-rc"

b8bdbf4da61f95b88893b02ddabc2491b16fa10f
Dhanya Thattil <dhanya.thattil@psi.ch>
2018-03-26 18:10:14 +0200

    updaterev

--- slsDetectorSoftware/slsDetector/gitInfoLib.h
+++ slsDetectorSoftware/slsDetector/gitInfoLib.h
@@ -1,6 +1,6 @@
 #define GITURL "git@github.com:slsdetectorgroup/slsDetectorPackage.git"
-#define GITREPUUID "98752e1cdbd78167ad20c6a01a17852b1a2263a7"
-#define GITAUTH "Gemma_Tinti"
-#define GITREV 0x3733
-#define GITDATE 0x20180321
+#define GITREPUUID "5340b8ef24e00cbc04e9827adc55e3adc9046a24"
+#define GITAUTH "Dhanya_Thattil"
+#define GITREV 0x3745
+#define GITDATE 0x20180326
 #define GITBRANCH "developer"

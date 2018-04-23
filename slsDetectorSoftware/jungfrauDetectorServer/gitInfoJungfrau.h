c9675d111a61075ebe4031ec6bdf285a76be0fce
Dhanya Thattil <dhanya.thattil@psi.ch>
2018-04-23 16:11:04 +0200

    jungfrau server: fixed the settings for gain2 stages

--- slsDetectorSoftware/jungfrauDetectorServer/gitInfoJungfrau.h
+++ slsDetectorSoftware/jungfrauDetectorServer/gitInfoJungfrau.h
@@ -1,6 +1,6 @@
 #define GITURL "git@github.com:slsdetectorgroup/slsDetectorPackage.git"
-#define GITREPUUID "5ff2e8606011ef6e37b5cfe6254acf80eaa50e5b"
+#define GITREPUUID "03e781a0ac53cb73af5d00003a131383125ab40f"
 #define GITAUTH "Dhanya_Thattil"
-#define GITREV 0x3771
-#define GITDATE 0x20180413
+#define GITREV 0x3776
+#define GITDATE 0x20180423
 #define GITBRANCH "jungfraunew"

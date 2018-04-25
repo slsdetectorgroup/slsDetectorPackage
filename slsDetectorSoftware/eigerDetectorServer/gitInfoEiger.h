11475316fd8721b49854e7b887fd3ec069a2a181
Dhanya Thattil <dhanya.thattil@psi.ch>
2018-04-25 17:11:52 +0200

    eiger binary updated

--- slsDetectorSoftware/eigerDetectorServer/gitInfoEiger.h
+++ slsDetectorSoftware/eigerDetectorServer/gitInfoEiger.h
@@ -1,6 +1,6 @@
 #define GITURL "git@github.com:slsdetectorgroup/slsDetectorPackage.git"
-#define GITREPUUID "753290b2289ec7a3dbb153493896f778a7182aae"
+#define GITREPUUID "a09c2e38a3c790574c89ed675a94c08cbb09f2ee"
 #define GITAUTH "Dhanya_Thattil"
-#define GITREV 0x3755
-#define GITDATE 0x20180406
-#define GITBRANCH "zmqhandler"
+#define GITREV 0x3787
+#define GITDATE 0x20180413
+#define GITBRANCH "esrfchanges"

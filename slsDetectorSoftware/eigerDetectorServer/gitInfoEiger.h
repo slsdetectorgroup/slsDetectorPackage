a37bbc50fe25c30582eb33f84c2547878a6b2053
Dhanya Thattil <dhanya.thattil@psi.ch>
2018-05-02 17:48:46 +0200

    updated server binaries

--- slsDetectorSoftware/eigerDetectorServer/gitInfoEiger.h
+++ slsDetectorSoftware/eigerDetectorServer/gitInfoEiger.h
@@ -1,6 +1,6 @@
 #define GITURL "git@github.com:slsdetectorgroup/slsDetectorPackage.git"
-#define GITREPUUID "a09c2e38a3c790574c89ed675a94c08cbb09f2ee"
+#define GITREPUUID "5b01b4cbd808a3c43f1ec97032a1020c8f2ce37a"
 #define GITAUTH "Dhanya_Thattil"
-#define GITREV 0x3787
-#define GITDATE 0x20180413
-#define GITBRANCH "esrfchanges"
+#define GITREV 0x3807
+#define GITDATE 0x20180502
+#define GITBRANCH "developer"
--- slsDetectorSoftware/jungfrauDetectorServer/gitInfoJungfrau.h
+++ slsDetectorSoftware/jungfrauDetectorServer/gitInfoJungfrau.h
@@ -1,6 +1,6 @@
 #define GITURL "git@github.com:slsdetectorgroup/slsDetectorPackage.git"
-#define GITREPUUID "c9675d111a61075ebe4031ec6bdf285a76be0fce"
+#define GITREPUUID "5b01b4cbd808a3c43f1ec97032a1020c8f2ce37a"
 #define GITAUTH "Dhanya_Thattil"
-#define GITREV 0x3777
-#define GITDATE 0x20180426
-#define GITBRANCH "jungfraunew"
+#define GITREV 0x3807
+#define GITDATE 0x20180502
+#define GITBRANCH "developer"

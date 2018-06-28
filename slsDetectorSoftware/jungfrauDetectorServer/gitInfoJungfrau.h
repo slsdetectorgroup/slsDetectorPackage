d76568d6d93c3f89ac875beabf373a4020047281
Dhanya Thattil <dhanya.thattil@psi.ch>
2018-06-28 08:55:14 +0200

    jungfrau server, merge fix binaries

--- slsDetectorSoftware/jungfrauDetectorServer/gitInfoJungfrau.h
+++ slsDetectorSoftware/jungfrauDetectorServer/gitInfoJungfrau.h
@@ -1,6 +1,6 @@
 #define GITURL "git@github.com:slsdetectorgroup/slsDetectorPackage.git"
-#define GITREPUUID "bab7d8e3fba7119b3da122cd4769f2447d6da28a"
+#define GITREPUUID "72f47b2375be2f3b095e5bd35cebda523d2210ae"
 #define GITAUTH "Dhanya_Thattil"
-#define GITREV 0x3890
-#define GITDATE 0x20180601
+#define GITREV 0x3917
+#define GITDATE 0x20180628
 #define GITBRANCH "developer"

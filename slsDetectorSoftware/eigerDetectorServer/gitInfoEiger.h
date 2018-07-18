2fc98c7a5475a7d259e8e3d08707eb7c75c788f6
Dhanya Thattil <dhanya.thattil@psi.ch>
2018-07-18 15:04:47 +0200

    updated binaries, eiger jungfrau

--- slsDetectorSoftware/eigerDetectorServer/gitInfoEiger.h
+++ slsDetectorSoftware/eigerDetectorServer/gitInfoEiger.h
@@ -1,6 +1,6 @@
 #define GITURL "git@github.com:slsdetectorgroup/slsDetectorPackage.git"
-#define GITREPUUID "c2b1eef75cb3c8a83adc091c656d9d23ad331139"
+#define GITREPUUID "3b6ead77836f2b6d2a795a9a994259d1dc8c131d"
 #define GITAUTH "Dhanya_Thattil"
-#define GITREV 0x3939
-#define GITDATE 0x20180710
-#define GITBRANCH "3.3.0-rc"
+#define GITREV 0x3943
+#define GITDATE 0x20180717
+#define GITBRANCH "developer"

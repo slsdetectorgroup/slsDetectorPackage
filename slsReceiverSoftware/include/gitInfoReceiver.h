588037fd18b805cb7f135f2f5094b8d36279ab53
Dhanya Thattil <dhanya.thattil@psi.ch>
2018-08-23 16:23:24 +0200

    updated release version

--- slsReceiverSoftware/include/gitInfoReceiver.h
+++ slsReceiverSoftware/include/gitInfoReceiver.h
@@ -1,6 +1,6 @@
 #define GITURL "git@github.com:slsdetectorgroup/slsDetectorPackage.git"
-#define GITREPUUID "6bb7195a2c7dc9526088882e0244a7455d3c15b2"
+#define GITREPUUID "e0b8fb36097aaafca033ca755bad4243bca655bc"
 #define GITAUTH "Dhanya_Thattil"
-#define GITREV 0x3941
-#define GITDATE 0x20180718
-#define GITBRANCH "3.3.0-rc"
+#define GITREV 0x3991
+#define GITDATE 0x20180823
+#define GITBRANCH "4.0.0"

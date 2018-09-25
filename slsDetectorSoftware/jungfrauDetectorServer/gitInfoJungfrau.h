33d1b11279dcd86aa22ec435dd0d17fcf67bdcaa
Dhanya Thattil <dhanya.thattil@psi.ch>
2018-09-25 14:04:54 +0200

    jungfrau server updated for API version change

--- slsDetectorSoftware/jungfrauDetectorServer/gitInfoJungfrau.h
+++ slsDetectorSoftware/jungfrauDetectorServer/gitInfoJungfrau.h
@@ -1,6 +1,6 @@
 #define GITURL "git@github.com:slsdetectorgroup/slsDetectorPackage.git"
-#define GITREPUUID "b7f62fcd47f8c0a5c23b2073812adb9bba806ce9"
+#define GITREPUUID "b163180d6f2505ff8a84f10e586cc2e0bbdaac86"
 #define GITAUTH "Dhanya_Thattil"
-#define GITREV 0x3990
-#define GITDATE 0x20180823
-#define GITBRANCH "4.0.0-rc"
+#define GITREV 0x4009
+#define GITDATE 0x20180925
+#define GITBRANCH "4.0.0"

2b0208bd982e4ca5c3ded7ba090a9483f6104459
Dhanya Thattil <dhanya.thattil@psi.ch>
2018-12-03 10:48:52 +0100

    gotthard server bug fix: adc channel shifted by one fixed changin token timing rev for board 2

--- slsDetectorSoftware/gotthardDetectorServer/gitInfoGotthard.h
+++ slsDetectorSoftware/gotthardDetectorServer/gitInfoGotthard.h
@@ -1,6 +1,6 @@
 #define GITURL "git@github.com:slsdetectorgroup/slsDetectorPackage.git"
-#define GITREPUUID "c2db073d5e936d64733ba9bdfdce311a556ae212"
+#define GITREPUUID "05d8ef6277b9d367aea69cc5b57e322d40079e27"
 #define GITAUTH "Dhanya_Thattil"
-#define GITREV 0x4018
-#define GITDATE 0x20180927
-#define GITBRANCH "4.0.0"
+#define GITREV 0x4029
+#define GITDATE 0x20181203
+#define GITBRANCH "4.0.1-rc"

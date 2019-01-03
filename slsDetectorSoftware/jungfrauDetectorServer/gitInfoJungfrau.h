8738c6b9a2cdbdf1e718508bc12ad8fdeeb18f6f
Dhanya Thattil <dhanya.thattil@psi.ch>
2019-01-03 11:43:21 +0100

    jungfrau server: updated severdefs to correspond to registerdefs update, need to update firmware version required for jungffrau server exe

--- slsDetectorSoftware/jungfrauDetectorServer/gitInfoJungfrau.h
+++ slsDetectorSoftware/jungfrauDetectorServer/gitInfoJungfrau.h
@@ -1,6 +1,6 @@
 #define GITURL "git@github.com:slsdetectorgroup/slsDetectorPackage.git"
-#define GITREPUUID "b163180d6f2505ff8a84f10e586cc2e0bbdaac86"
+#define GITREPUUID "f701c731fb72f258ae8cc6fb440761616e4f2801"
 #define GITAUTH "Dhanya_Thattil"
-#define GITREV 0x4009
-#define GITDATE 0x20180925
-#define GITBRANCH "4.0.0"
+#define GITREV 0x4043
+#define GITDATE 0x20190103
+#define GITBRANCH "4.0.1-rc"

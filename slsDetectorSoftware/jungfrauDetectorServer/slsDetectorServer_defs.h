de34d85761cd85e490fbd54b9241119cce073b99
Dhanya Thattil <dhanya.thattil@psi.ch>
2019-01-07 11:51:25 +0100

    jungfrau server for firmware 0.7, updated required FW version to run current code

--- slsDetectorSoftware/jungfrauDetectorServer/slsDetectorServer_defs.h
+++ slsDetectorSoftware/jungfrauDetectorServer/slsDetectorServer_defs.h
@@ -10,7 +10,7 @@
 #define GOODBYE 					(-200)
 #define PROGRAMMING_MODE			(-200)
 #define MIN_REQRD_VRSN_T_RD_API     0x171220
-#define REQRD_FRMWR_VRSN            0x180615
+#define REQRD_FRMWR_VRSN            0x181206 // temp bug fix from last version, timing mode is backwards compatible
 
 
 /* Struct Definitions */

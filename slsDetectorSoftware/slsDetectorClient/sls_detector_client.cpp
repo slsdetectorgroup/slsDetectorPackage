68545f2b26415c3cb5c5642850a762616f9a8a42
Erik Frojdh <erik.frojdh@gmail.com>
2019-01-16 10:56:51 +0100

    removed pointer in multiSlsDetectorClient

--- slsDetectorSoftware/slsDetectorClient/sls_detector_client.cpp
+++ slsDetectorSoftware/slsDetectorClient/sls_detector_client.cpp
@@ -1,40 +1,33 @@
-#include "multiSlsDetectorClient.h"
 #include "gitInfoLib.h"
-
+#include "multiSlsDetectorClient.h"
 #include <cstdlib>
 
-int main(int argc, char *argv[])
-  
-{
-	for (int i = 1; i < argc; ++i ) {
-		if (!(strcmp (argv[i],"--version")) || !(strcmp (argv[i],"-v"))) {
-			int64_t tempval = GITDATE;
-			std::cout << argv[0] << " " << GITBRANCH << " (0x" << std::hex << tempval << ")" << std::endl;
-			return 0;
-		}
-	}
+int main(int argc, char *argv[]) {
+    for (int i = 1; i < argc; ++i) {
+        if (!(strcmp(argv[i], "--version")) || !(strcmp(argv[i], "-v"))) {
+            int64_t tempval = GITDATE;
+            std::cout << argv[0] << " " << GITBRANCH << " (0x" << std::hex << tempval << ")" << std::endl;
+            return 0;
+        }
+    }
 
 #ifdef PUT
-  int action=slsDetectorDefs::PUT_ACTION;
+    int action = slsDetectorDefs::PUT_ACTION;
 #endif
-    
+
 #ifdef GET
-  int action=slsDetectorDefs::GET_ACTION;
+    int action = slsDetectorDefs::GET_ACTION;
 #endif
-    
+
 #ifdef READOUT
-  int action=slsDetectorDefs::READOUT_ACTION;
+    int action = slsDetectorDefs::READOUT_ACTION;
 #endif
-    
+
 #ifdef HELP
-  int action=slsDetectorDefs::HELP_ACTION;
+    int action = slsDetectorDefs::HELP_ACTION;
 #endif
 
-  if (argc>1)
-    multiSlsDetectorClient(argc-1, argv+1, action);
-  else
-    multiSlsDetectorClient(argc-1, argv, action);
-
+    if (argc > 1)
+        argv++;
+    multiSlsDetectorClient(argc - 1, argv, action);
 }
-
-  

202d88d6be6e2c7bf00f6e76eed7770a55126b31
Erik Frojdh <erik.frojdh@gmail.com>
2019-04-18 15:31:07 +0200

    experimental python

--- python/sls_detector/experimental.py
+++ python/sls_detector/experimental.py
@@ -6,19 +6,4 @@ class ExperimentalDetector(multiDetectorApi):
         super().__init__(0)
         self.online = True
 
-    @property
-    def online(self):
-        return self._setOnline() == 1
-
-    @online.setter
-    def online(self, value):
-        self._setOnline(value)
-
-    @property
-    def rx_udpip(self):
-        return self._getReceiverUDPIP(-1)
-
-    @rx_udpip.setter
-    def rx_udpip(self, ip):
-        self._setReceiverUDPIP(ip, -1)
     

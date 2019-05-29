b3d6d91f26e987446f61901c8e3366bedc47006a
Erik Frojdh <erik.frojdh@gmail.com>
2019-05-29 08:30:54 +0200

    test frames

--- slsDetectorSoftware/tests/test-multiSlsDetectorClient.cpp
+++ slsDetectorSoftware/tests/test-multiSlsDetectorClient.cpp
@@ -7,15 +7,30 @@ auto GET = slsDetectorDefs::GET_ACTION;
 auto PUT = slsDetectorDefs::PUT_ACTION;
 
 TEST_CASE("rx_fifodepth", "[.cmd]") {
-    std::ostringstream oss1;
-    multiSlsDetectorClient("rx_fifodepth 10", PUT, nullptr, oss1);
-    REQUIRE(oss1.str() == "rx_fifodepth 10\n");
+    auto oss = std::ostringstream{};
+    multiSlsDetectorClient("rx_fifodepth 10", PUT, nullptr, oss);
+    REQUIRE(oss.str() == "rx_fifodepth 10\n");
 
-    std::ostringstream oss2;
-    multiSlsDetectorClient("rx_fifodepth 100", PUT, nullptr, oss2);
-    REQUIRE(oss2.str() == "rx_fifodepth 100\n");
+    oss = std::ostringstream{};
+    multiSlsDetectorClient("rx_fifodepth 100", PUT, nullptr, oss);
+    REQUIRE(oss.str() == "rx_fifodepth 100\n");
 
-    std::ostringstream oss3;
-    multiSlsDetectorClient("rx_fifodepth", GET, nullptr, oss3);
-    REQUIRE(oss3.str() == "rx_fifodepth 100\n");
+    oss = std::ostringstream{};
+    multiSlsDetectorClient("rx_fifodepth", GET, nullptr, oss);
+    REQUIRE(oss.str() == "rx_fifodepth 100\n");
+}
+
+TEST_CASE("frames", "[.cmd]"){
+    auto oss = std::ostringstream{};
+    multiSlsDetectorClient("frames 1000", PUT, nullptr, oss);
+    REQUIRE(oss.str() == "frames 1000\n");
+
+    oss = std::ostringstream{};
+    multiSlsDetectorClient("frames", GET, nullptr, oss);
+    REQUIRE(oss.str() == "frames 1000\n");
+
+    oss = std::ostringstream{};
+    multiSlsDetectorClient("frames 1", PUT, nullptr, oss);
+    REQUIRE(oss.str() == "frames 1\n");
+
 }

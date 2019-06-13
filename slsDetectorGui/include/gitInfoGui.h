5c63c8fc8ff13627363fb647cb19179139c21e49
Dhanya Thattil <dhanya.thattil@psi.ch>
2019-06-13 09:01:39 +0200

    gui bug fix: switching to tab plot while acquiring produced error messages

--- slsDetectorGui/include/gitInfoGui.h
+++ slsDetectorGui/include/gitInfoGui.h
@@ -1,6 +1,6 @@
 #define GITURL "git@github.com:slsdetectorgroup/slsDetectorPackage.git"
-#define GITREPUUID "46533b041195591b1a09b2662895e084821ba886"
+#define GITREPUUID "7b70274d4f57977649e4f4a69c21238f335561fc"
 #define GITAUTH "Dhanya_Thattil"
-#define GITREV 0x4107
-#define GITDATE 0x20190612
+#define GITREV 0x4109
+#define GITDATE 0x20190613
 #define GITBRANCH "4.0.2"

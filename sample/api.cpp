17145a6326af8895ee384c55831d23169a3f3a80
Erik Frojdh <erik.frojdh@gmail.com>
2019-06-28 17:30:12 +0200

    WIP

--- sample/api.cpp
+++ sample/api.cpp
@@ -8,15 +8,36 @@ std::ostream &operator<<(std::ostream &os, const std::chrono::nanoseconds &t) {
     return os;
 }
 
-template<typename T>
-std::ostream &operator<<(std::ostream &os, const std::vector<T> &vec) {
-    os << "[";
-    auto it = vec.cbegin();
-    std::cout << *it++;
-    while(it!=vec.cend())
+template <typename T, typename _ = void>
+struct is_container : std::false_type {};
+
+template <typename... Ts> struct is_container_helper {};
+
+template <typename T>
+struct is_container<
+    T, typename std::conditional<
+           false,
+           is_container_helper<typename T::value_type, typename T::size_type,
+                               typename T::iterator, typename T::const_iterator,
+                               decltype(std::declval<T>().size()),
+                               decltype(std::declval<T>().begin()),
+                               decltype(std::declval<T>().end()),
+                               decltype(std::declval<T>().cbegin()),
+                               decltype(std::declval<T>().cend()),
+                               decltype(std::declval<T>().empty())>,
+           void>::type> : public std::true_type {};
+
+template <typename Container>
+auto operator<<(std::ostream &os, const Container &con) ->
+    typename std::enable_if<is_container<Container>::value,
+                            std::ostream &>::type {
+    if (con.empty())
+        return os << "[]";
+    auto it = con.cbegin();
+    os << '[' << *it++;
+    while (it != con.cend())
         os << ", " << *it++;
-    os << "]";
-    return os;
+    return os << ']';
 }
 
 using sls::Detector;
@@ -24,8 +45,8 @@ using std::chrono::nanoseconds;
 using std::chrono::seconds;
 
 int main() {
-    // Detector d;
-    // d.setConfig("/home/l_frojdh/virtual.config");
+    Detector d;
+    d.setConfig("/home/l_frojdh/virtual.config");
 
     // d.setExptime(nanoseconds(500)); // set exptime of all modules
     // auto t0 = d.getExptime();
@@ -35,8 +56,8 @@ int main() {
     // auto t1 = d.getExptime({1,3,5}); // get exptime of module 1, 3 and 5
     // std::cout << "exptime: " <<t1 << '\n';
 
-
-    // std::cout << "Period: " <<  d.getPeriod() << '\n';
+    std::cout << "Period: " <<  d.getPeriod() << '\n';
+    std::cout << "Period: " <<  d.getPeriod().squash() << '\n';
     // std::cout << "fname: " << d.getFname() << "\n";
 
     // std::cout << "fwrite: " << std::boolalpha << d.getFwrite() << '\n';
--- slsDetectorSoftware/include/Result.h
+++ slsDetectorSoftware/include/Result.h
@@ -1,6 +1,18 @@
 #pragma once
 #include <vector>
+#include <algorithm>
 namespace sls {
+
+template <typename Container>
+typename Container::value_type
+Squash(const Container &c, typename Container::value_type default_value = {}) {
+    if (std::all_of(begin(c), end(c),
+                    [c](const typename Container::value_type &element) {
+                        return element == c.front();
+                    }))
+        return c.front();
+    return default_value;
+}
 
 template <class T, class Allocator = std::allocator<T>> class Result {
     std::vector<T, Allocator> vec;
@@ -11,9 +23,9 @@ template <class T, class Allocator = std::allocator<T>> class Result {
     template <typename... Args>
     Result(Args &&... args) : vec(std::forward<Args>(args)...) {}
     using value_type = T;
-    using iterator = typename std::vector<T>::iterator;
-    using const_iterator = typename std::vector<T>::const_iterator;
-    using size_type = typename std::vector<T>::size_type;
+    using iterator = decltype(vec.begin());
+    using const_iterator = decltype(vec.cbegin());
+    using size_type = decltype(vec.size());
 
     auto begin() -> decltype(vec.begin()) { return vec.begin(); }
     auto end() -> decltype(vec.end()) { return vec.end(); }
@@ -21,6 +33,10 @@ template <class T, class Allocator = std::allocator<T>> class Result {
     auto cend() const -> decltype(vec.cend()) { return vec.cend(); }
     auto size() const -> decltype(vec.size()) { return vec.size(); }
     auto empty() const -> decltype(vec.empty()) { return vec.empty(); }
+
+    T squash(){
+        return Squash(vec);
+    }
 };
 
 } // namespace sls

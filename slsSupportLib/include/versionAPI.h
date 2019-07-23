61b40d89c7a2cb4abbfd8b2a77babeb585400a96
Dhanya Thattil <dhanya.thattil@psi.ch>
2019-07-23 11:56:02 +0200

    updated client api versioning

--- slsSupportLib/include/versionAPI.h
+++ slsSupportLib/include/versionAPI.h
@@ -1,10 +1,10 @@
 /** API versions */
-#define GITBRANCH "refgui"
+#define GITBRANCH "developer"
 #define APIMOENCH 0x181108
-#define APILIB 0x190604
-#define APIRECEIVER 0x190604
-#define APIGUI 0x190405
 #define APICTB 0x190604
 #define APIGOTTHARD 0x190715
 #define APIJUNGFRAU 0x190718
 #define APIEIGER 0x190722
+#define APILIB 0x190723
+#define APIRECEIVER 0x190722
+#define APIGUI 0x190723

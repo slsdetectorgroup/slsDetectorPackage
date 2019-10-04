98b45b32fd6653fb2758afdc3f804e2a785c504d
Dhanya Thattil <dhanya.thattil@psi.ch>
2019-10-04 10:29:57 +0200

    added jungfrau virtual binaries

--- slsDetectorSoftware/jungfrauDetectorServer/gitInfoJungfrau.h
+++ slsDetectorSoftware/jungfrauDetectorServer/gitInfoJungfrau.h
@@ -1,6 +1,6 @@
 #define GITURL "git@github.com:slsdetectorgroup/slsDetectorPackage.git"
-#define GITREPUUID "4be0b5d4366440449ad86fc78fa04e618d56df5f"
+#define GITREPUUID "74c08cd74240c08b395c9acf8897d5254839d9c3"
 #define GITAUTH "Dhanya_Thattil"
-#define GITREV 0x4097
-#define GITDATE 0x20190408
-#define GITBRANCH "4.0.2-rc"
+#define GITREV 0x4140
+#define GITDATE 0x20191004
+#define GITBRANCH "4.1.0-rc"

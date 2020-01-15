30a4a1740f878189b1d2e691c21e0bb5f4acd9e2
Erik Frojdh <erik.frojdh@gmail.com>
2020-01-15 17:56:08 +0100

    added Mythen3

--- python/scripts/basic.py
+++ python/scripts/basic.py
@@ -2,30 +2,14 @@ import os
 import sys
 import numpy as np
 sys.path.append(os.path.join(os.getcwd(), 'bin'))
-# from sls_detector import Eiger, Jungfrau, Detector, defs
 
-from sls_detector import Detector, Eiger, Jungfrau, DetectorDacs, Dac, Ctb
+from sls_detector import Detector, Mythen3, Eiger, Jungfrau, DetectorDacs, Dac, Ctb
 from sls_detector import dacIndex
 
 
 d = Detector()
 # e = Eiger()
 # c = Ctb()
-j = Jungfrau()
+# j = Jungfrau()
+m = Mythen3()
 
-# def tracefunc(frame, event, arg, indent=[0]):
-#       if event == "call":
-#           indent[0] += 2
-#           print("-" * indent[0] + "> call function", frame.f_code.co_name)
-#       elif event == "return":
-#           print("<" + "-" * indent[0], "exit function", frame.f_code.co_name)
-#           indent[0] -= 2
-#       return tracefunc
-
-# import sys
-# sys.setprofile(tracefunc)
-# print('------------------------------------------------------')
-# j.dacs.vb_comp[:] = 1500
-# print('------------------------------------------------------')
-
-# # sys.setprofile(None)

d3d8d7ba8ecaef494bb0ef329219c080121b0b3d
Erik Frojdh <erik.frojdh@gmail.com>
2020-02-13 17:13:56 +0100

    WIP

--- conda-recepie/build_pylib.sh
+++ conda-recepie/build_pylib.sh
@@ -1,18 +1,7 @@
 
-# mkdir $PREFIX/lib
-# mkdir $PREFIX/include
 
-
-# #Shared and static libraries
-# cp build/bin/_sls_detector* $PREFIX/lib/.
-
-
-# #Binaries
-# cp -r build/bin/sls_detector $PREFIX/lib/.
-
-#copy shared lib 
 echo "|<-------- starting python build"
-# cp build/bin/_sls_detector* python/.
+
 
 cd python
 ${PYTHON} setup.py install

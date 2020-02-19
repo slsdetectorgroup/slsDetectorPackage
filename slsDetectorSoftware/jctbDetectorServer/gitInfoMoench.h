63f5b52a6effdac9d750a7b6cb7ddcc00cc0240a
Anna Bergamaschi <anna.bergamaschi@psi.ch>
2020-02-19 12:26:35 +0100

    jctbDetectorServer compilation flags match only for moench

--- slsDetectorSoftware/jctbDetectorServer/gitInfoMoench.h
+++ slsDetectorSoftware/jctbDetectorServer/gitInfoMoench.h
@@ -1,6 +1,6 @@
 #define GITURL "git@github.com:slsdetectorgroup/slsDetectorPackage.git"
-#define GITREPUUID "9e5ec6a57bf206fe9384260fc4040867a9bbd71c"
-#define GITAUTH "Erik_Frojdh"
-#define GITREV 0x4065
-#define GITDATE 0x20190130
-#define GITBRANCH "developer"
+#define GITREPUUID "53ba22dbc40d17c8ca77647d4ca2edad230f9f8a"
+#define GITAUTH "Anna_Bergamaschi"
+#define GITREV 0x4088
+#define GITDATE 0x20200219
+#define GITBRANCH "moench"

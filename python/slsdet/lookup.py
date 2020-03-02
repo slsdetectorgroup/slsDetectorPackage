95d56ad5455a1a62a0b99d50492e7273eb723f9c
Erik Frojdh <erik.frojdh@gmail.com>
2020-03-02 11:57:35 +0100

    lookup

--- python/slsdet/lookup.py
+++ python/slsdet/lookup.py
@@ -1,9 +1,9 @@
 from .detector import Detector
 
-def view(name):
-    names =  find(name)
+def view(name, det = Detector):
+    names =  find(name, det)
     for n in names:
         print(n)
 
-def find(name):
-    return [n for n in dir(Detector) if name in n]
+def find(name, det = Detector):
+    return [n for n in dir(det) if name.lower() in n.lower()]

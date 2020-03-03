3431752649c14e1b8a920247ee466cfc793eb664
Erik Frojdh <erik.frojdh@gmail.com>
2020-03-03 17:01:45 +0100

    WIP

---
+++ slsSupportLib/tests/test-logger.cpp
@@ -0,0 +1,16 @@
+#include "catch.hpp"
+#include "logger.h"
+
+#include <iostream>
+#include <fstream>
+TEST_CASE("fail"){
+
+    FILE_LOG(logINFO) << "A message";
+
+
+
+
+    // CHECK(false);
+
+
+}

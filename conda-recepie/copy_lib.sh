ce2c62000dfe50ce88c8c685c18e363c63c89d97
Erik Frojdh <erik.frojdh@gmail.com>
2020-03-17 18:57:19 +0100

    include cmake in build

--- conda-recepie/copy_lib.sh
+++ conda-recepie/copy_lib.sh
@@ -5,18 +5,20 @@ mkdir $PREFIX/include
 mkdir $PREFIX/include/slsDetectorPackage
 
 #Shared and static libraries
-cp build/bin/libSlsDetector.so $PREFIX/lib/.
-cp build/bin/libSlsReceiver.so $PREFIX/lib/.
-cp build/bin/libSlsSupport.so $PREFIX/lib/.
+# cp build/bin/libSlsDetector.so $PREFIX/lib/.
+# cp build/bin/libSlsReceiver.so $PREFIX/lib/.
+# cp build/bin/libSlsSupport.so $PREFIX/lib/.
+
+cp build/install/lib/* $PREFIX/lib/
 
 #Binaries
-cp build/bin/sls_detector_acquire $PREFIX/bin/.
-cp build/bin/sls_detector_get $PREFIX/bin/.
-cp build/bin/sls_detector_put $PREFIX/bin/.
-cp build/bin/sls_detector_help $PREFIX/bin/.
-cp build/bin/slsReceiver $PREFIX/bin/.
-cp build/bin/slsMultiReceiver $PREFIX/bin/.
+cp build/install/bin/sls_detector_acquire $PREFIX/bin/.
+cp build/install/bin/sls_detector_get $PREFIX/bin/.
+cp build/install/bin/sls_detector_put $PREFIX/bin/.
+cp build/install/bin/sls_detector_help $PREFIX/bin/.
+cp build/install/bin/slsReceiver $PREFIX/bin/.
+cp build/install/bin/slsMultiReceiver $PREFIX/bin/.
 
-#Which headers do we need for development??
-cp build/install/include/* $PREFIX/include/slsDetectorPackage/
-# cp include/some_lib.h $PREFIX/include/.
+
+cp build/install/include/* $PREFIX/include/
+cp -r build/install/share/ $PREFIX/share

2f33a1a47938644ef1130036db26933b879621bb
Dhanya Thattil <dhanya.thattil@psi.ch>
2020-04-09 09:35:46 +0200

    updated binaries

--- slsSupportLib/include/versionAPI.h
+++ slsSupportLib/include/versionAPI.h
@@ -1,5 +1,8 @@
 /** API versions */
 #define GITBRANCH "setrxhostname"
+#define APILIB 0x200409
+#define APIRECEIVER 0x200409
+#define APIGUI 0x200409
 #define APIEIGER 0x200409
 #define APICTB 0x200409
 #define APIGOTTHARD 0x200409
@@ -7,6 +10,3 @@
 #define APIJUNGFRAU 0x200409
 #define APIMYTHEN3 0x200409
 #define APIMOENCH 0x200409
-#define APILIB 0x200409
-#define APIRECEIVER 0x200409
-#define APIGUI 0x200409

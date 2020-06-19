88d4e2617d954e4a3545bd73516742d6af09c76d
Erik Frojdh <erik.frojdh@gmail.com>
2020-06-19 12:06:13 +0200

    test for sending data

--- slsSupportLib/tests/test-ZmqSocket.cpp
+++ slsSupportLib/tests/test-ZmqSocket.cpp
@@ -1,23 +1,23 @@
 #include "ZmqSocket.h"
 #include "catch.hpp"
 
-TEST_CASE("Throws when cannot create socket"){
+TEST_CASE("Throws when cannot create socket") {
     REQUIRE_THROWS(ZmqSocket("sdiasodjajpvv", 5076001));
 }
 
-TEST_CASE("Get port number for sub"){
+TEST_CASE("Get port number for sub") {
     constexpr int port = 50001;
     ZmqSocket sub("localhost", port);
     REQUIRE(sub.GetPortNumber() == port);
 }
 
-TEST_CASE("Get port number for pub"){
+TEST_CASE("Get port number for pub") {
     constexpr int port = 50001;
     ZmqSocket pub(port, "*");
     REQUIRE(pub.GetPortNumber() == port);
 }
 
-TEST_CASE("Server address"){
+TEST_CASE("Server address") {
     constexpr int port = 50001;
     ZmqSocket pub(port, "*");
     REQUIRE(pub.GetZmqServerAddress() == std::string("tcp://*:50001"));
@@ -29,7 +29,7 @@ TEST_CASE("Send header on localhost") {
     sub.Connect();
 
     ZmqSocket pub(port, "*");
-    
+
     // Header to send
     zmqHeader header;
     header.data = false; // if true we wait for the data
@@ -56,10 +56,9 @@ TEST_CASE("Send header on localhost") {
     REQUIRE(received_header.npixelsx == 724);
     REQUIRE(received_header.npixelsy == 324);
     REQUIRE(received_header.imageSize == 200);
-    
 }
 
-TEST_CASE("Send serveral headers of different length"){
+TEST_CASE("Send serveral headers of different length") {
     constexpr int port = 50001;
     ZmqSocket sub("localhost", port);
     sub.Connect();
@@ -85,4 +84,31 @@ TEST_CASE("Send serveral headers of different length"){
     pub.SendHeader(0, header);
     sub.ReceiveHeader(0, received_header, 0);
     REQUIRE(received_header.fname == "short");
+}
+
+TEST_CASE("Send header and data") {
+    constexpr int port = 50001;
+    ZmqSocket sub("localhost", port);
+    sub.Connect();
+
+    ZmqSocket pub(port, "*");
+
+    std::vector<int> data{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    const int nbytes = data.size() * sizeof(decltype(data)::value_type);
+    zmqHeader header;
+    header.data = true;
+    header.imageSize = nbytes;
+
+    pub.SendHeader(0, header);
+    pub.SendData((char *)data.data(), nbytes);
+
+    zmqHeader received_header;
+    sub.ReceiveHeader(0, received_header, 0);
+    std::vector<int> received_data(received_header.imageSize / sizeof(int));
+    sub.ReceiveData(0, (char *)received_data.data(), received_header.imageSize);
+
+    REQUIRE(data.size() == received_data.size());
+    for (size_t i = 0; i != data.size(); ++i) {
+        REQUIRE(data[i] == received_data[i]);
+    }
 }

5c4d355d57b7557a927c057ec2d472b9cd97f618
Dhanya Thattil <dhanya.thattil@psi.ch>
2020-07-31 17:18:15 +0200

    minor

--- slsReceiverSoftware/src/receiver_defs.h
+++ slsReceiverSoftware/src/receiver_defs.h
@@ -52,38 +52,3 @@
 #define PROCESSOR_PRIORITY (70)
 #define STREAMER_PRIORITY  (10)
 #define TCP_PRIORITY       (10)
-
-/*
-#define HDF5_WRITER_VERSION   (6.1) // 1 decimal places
-#define BINARY_WRITER_VERSION (6.1) // 1 decimal places
-struct masterAttributes {
-    double version;
-    uint32_t detectorType;
-    uint32_t dynamicRange;
-    uint32_t tenGiga;
-    uint32_t imageSize;
-    uint32_t nPixelsX;
-    uint32_t nPixelsY;
-    uint32_t maxFramesPerFile;
-    uint64_t totalFrames;
-    uint64_t exptimeNs;
-    uint64_t subExptimeNs;
-    uint64_t subPeriodNs;
-    uint64_t periodNs;
-    uint32_t quadEnable;
-    uint32_t analogFlag;
-    uint32_t digitalFlag;
-    uint32_t adcmask;
-    uint32_t dbitoffset;
-    uint64_t dbitlist;
-    uint32_t roiXmin;
-    uint32_t roiXmax;
-    uint64_t exptime1Ns;
-    uint64_t exptime2Ns;
-    uint64_t exptime3Ns;
-    uint64_t gateDelay1Ns;
-    uint64_t gateDelay2Ns;
-    uint64_t gateDelay3Ns;
-    uint32_t gates;
-};
-*/

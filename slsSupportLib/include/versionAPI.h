d56b2134ef0c29524ad08e70bfa95df234f468d5
Dhanya Thattil <dhanya.thattil@psi.ch>
2020-08-10 09:58:57 +0200

    updated client versions

--- slsSupportLib/include/versionAPI.h
+++ slsSupportLib/include/versionAPI.h
@@ -1,8 +1,5 @@
 /** API versions */
 #define GITBRANCH    "developer"
-#define APILIB       0x200409
-#define APIRECEIVER  0x200409
-#define APIGUI       0x200409
 #define APICTB 0x200805
 #define APIGOTTHARD 0x200805
 #define APIGOTTHARD2 0x200805
@@ -10,3 +7,6 @@
 #define APIMYTHEN3 0x200805
 #define APIMOENCH 0x200804
 #define APIEIGER 0x200805
+#define APILIB 0x200810
+#define APIRECEIVER 0x200810
+#define APIGUI 0x200803

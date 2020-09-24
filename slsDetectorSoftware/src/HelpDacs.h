92201c8d90f7087be45a58791ce8b4a7e589df13
Dhanya Thattil <dhanya.thattil@psi.ch>
2020-09-24 14:42:50 +0200

    dac argument included

--- slsDetectorSoftware/src/HelpDacs.h
+++ slsDetectorSoftware/src/HelpDacs.h
@@ -7,13 +7,11 @@ std::string GetDacHelp(std::string dac) {
     }
     if (dac == "vthreshold") {
         return std::string(
-            "vthreshold [dac or mV value][(optional unit) mV] "
-            "\n\t[Eiger][Mythen3] Detector threshold voltage for single "
-            "photon counters.\n\t[Eiger] Sets vcmp_ll, vcmp_lr, vcmp_rl, "
-            "vcmp_rr and vcp to the same value. \n\t[Mythen3] Sets vth1, "
-            "vth2 and vth3 to the same value.");
+            "[dac or mV value][(optional unit) mV] \n\t[Eiger][Mythen3] "
+            "Detector threshold voltage for single photon counters.\n\t[Eiger] "
+            "Sets vcmp_ll, vcmp_lr, vcmp_rl, vcmp_rr and vcp to the same "
+            "value. \n\t[Mythen3] Sets vth1, vth2 and vth3 to the same value.");
     }
-    if (dac == "vthreshold") {
-    }
+
     throw sls::RuntimeError("Unknown dac command");
 }

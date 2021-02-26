1ae8c5e464dfbe99b887910693eb325a470c1409
Erik Frojdh <erik.frojdh@gmail.com>
2021-02-26 15:11:58 +0100

    version and jf server added

--- python/setup.py
+++ python/setup.py
@@ -2,11 +2,12 @@
 Setup file for slsdet
 Build upon the pybind11 example found here: https://github.com/pybind/python_example
 """
-from setuptools import setup, Extension, find_packages
-from setuptools.command.build_ext import build_ext
-import sys
-import setuptools
+
 import os
+import sys
+sys.path.append('../libs/pybind11')
+from setuptools import setup, find_packages
+from pybind11.setup_helpers import Pybind11Extension, build_ext
 
 __version__ = os.environ.get('GIT_DESCRIBE_TAG', 'developer')
 
@@ -19,108 +20,28 @@ def get_conda_path():
     return os.environ['CONDA_PREFIX']
 
 
-# class get_pybind_include(object):
-#     """Helper class to determine the pybind11 include path
-#     The purpose of this class is to postpone importing pybind11
-#     until it is actually installed, so that the ``get_include()``
-#     method can be invoked. """
-
-#     def __init__(self, user=False):
-#         self.user = user
-
-#     def __str__(self):
-#         import pybind11
-#         return pybind11.get_include(self.user)
-
-
+#TODO migrate to CMake build? 
 ext_modules = [
-    Extension(
+    Pybind11Extension(
         '_slsdet',
         ['src/main.cpp',
         'src/enums.cpp',
         'src/detector.cpp',
-        'src/network.cpp'],
+        'src/network.cpp',
+        'src/pattern.cpp',
+        'src/scan.cpp',],
         include_dirs=[
-            # Path to pybind11 headers
-            # get_pybind_include(),
-            # get_pybind_include(user=True),
             os.path.join('../libs/pybind11/include'),
             os.path.join(get_conda_path(), 'include'),
 
         ],
-        libraries=['SlsDetector', 'SlsReceiver', 'zmq'],
+        libraries=['SlsDetector', 'SlsSupport', 'SlsReceiver', 'zmq'],
         library_dirs=[
             os.path.join(get_conda_path(), 'lib'),
-            os.path.join(get_conda_path(), 'bin'),
         ],
-
         language='c++'
     ),
 ]
-
-
-# As of Python 3.6, CCompiler has a `has_flag` method.
-# cf http://bugs.python.org/issue26689
-def has_flag(compiler, flagname):
-    """Return a boolean indicating whether a flag name is supported on
-    the specified compiler.
-    """
-    import tempfile
-    with tempfile.NamedTemporaryFile('w', suffix='.cpp') as f:
-        f.write('int main (int argc, char **argv) { return 0; }')
-        try:
-            compiler.compile([f.name], extra_postargs=[flagname])
-        except setuptools.distutils.errors.CompileError:
-            return False
-    return True
-
-
-def cpp_flag(compiler):
-    """Return the -std=c++[11/14] compiler flag.
-    The c++14 is prefered over c++11 (when it is available).
-    """
-    if has_flag(compiler, '-std=c++14'):
-        return '-std=c++14'
-    elif has_flag(compiler, '-std=c++11'):
-        return '-std=c++11'
-    else:
-        raise RuntimeError('Unsupported compiler -- at least C++11 support '
-                           'is needed!')
-
-
-class BuildExt(build_ext):
-    """A custom build extension for adding compiler-specific options."""
-    c_opts = {
-        'msvc': ['/EHsc'],
-        'unix': [],
-    }
-
-    if sys.platform == 'darwin':
-        c_opts['unix'] += ['-stdlib=libc++', '-mmacosx-version-min=10.7']
-
-    def build_extensions(self):
-        ct = self.compiler.compiler_type
-        opts = self.c_opts.get(ct, [])
-        if ct == 'unix':
-            opts.append('-DVERSION_INFO="%s"' % self.distribution.get_version())
-            opts.append(cpp_flag(self.compiler))
-            if has_flag(self.compiler, '-fvisibility=hidden'):
-                opts.append('-fvisibility=hidden')
-        elif ct == 'msvc':
-            opts.append('/DVERSION_INFO=\\"%s\\"' % self.distribution.get_version())
-        for ext in self.extensions:
-            ext.extra_compile_args = opts
-
-        print('**************************************************')
-        print(ct)
-        print(opts)
-        print('**************************************************')
-        build_ext.build_extensions(self)
-        
-
-
-def get_shared_lib():
-    return [f for f in os.listdir('.') if '_slsdet' in f]
 
 setup(
     name='slsdet',
@@ -132,6 +53,6 @@ setup(
     long_description='',
     packages=find_packages(exclude=['contrib', 'docs', 'tests']),
     ext_modules=ext_modules,
-    cmdclass={'build_ext': BuildExt},
+    cmdclass={"build_ext": build_ext},
     zip_safe=False,
 )

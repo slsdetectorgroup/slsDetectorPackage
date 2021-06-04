d33cf25a95afcc734006e8a2b687b370640c8d6a
Dhanya Thattil <dhanya.thattil@psi.ch>
2021-06-04 16:51:36 +0200

    wip

--- slsDetectorServers/eigerDetectorServer/FebControl.h
+++ slsDetectorServers/eigerDetectorServer/FebControl.h
@@ -55,8 +55,7 @@ int Feb_Control_PrepareForAcquisition();
 void Feb_Control_PrintAcquisitionSetup();
 int Feb_Control_StartAcquisition();
 int Feb_Control_StopAcquisition();
-int Feb_Control_GetExposureToggle(int *toggle, int *exposure);
-int Feb_Control_GetExposureStatus(int *status);
+int Feb_Control_GetExposureStatus(int *toggle, int *exposure);
 int Feb_Control_SendSoftwareTrigger();
 int Feb_Control_SoftwareTrigger(int block);
 

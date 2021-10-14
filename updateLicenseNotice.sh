0801957203d35b81a174a4d4c0b035aeff66b37e
Dhanya Thattil <dhanya.thattil@psi.ch>
2021-10-14 18:03:19 +0200

    updating license script

--- updateLicenseNotice.sh
+++ updateLicenseNotice.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
-for file in $(find . -name "*.h"); do
+WD=$PWD
+LICENCE_NOTICE_FILE=$WD/notice_to_add_for_every_file
+
+
+if [ $# -lt 1 ]; then
+    echo "Wrong usage of updateLicenseNotice.sh. Requires atleast 1 argument [RELATIVE PATH]"
+    return -1
+fi
+
+CURRENT=$WD/$1
+
+if [ ! -d "$CURRENT" ]; then
+  echo "This directory ${CURRENT} does not exist"
+  return -1
+fi
+
+cd CURRENT
+
+for file in $(find $CURRENT -name "*.h"); do
   echo Processing $file
 
   cat notice_to_add_for_every_file $file > $file.modified
@@ -7,3 +25,5 @@ for file in $(find . -name "*.h"); do
   mv $file.modified $file
 
 done
+
+cd $WD

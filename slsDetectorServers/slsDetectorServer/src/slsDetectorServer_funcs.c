7f581a0dc0609824a12d4d721255b850fa406ee8
Dhanya Thattil <dhanya.thattil@psi.ch>
2022-05-10 15:22:25 +0200

    debug print out

--- slsDetectorServers/slsDetectorServer/src/slsDetectorServer_funcs.c
+++ slsDetectorServers/slsDetectorServer/src/slsDetectorServer_funcs.c
@@ -155,7 +155,7 @@ int decode_function(int file_des) {
             return FAIL;
         }
 
-        LOG(logINFORED, (" calling function fnum=%d, (%s)\n", fnum,
+        LOG(logDEBUG1, (" calling function fnum=%d, (%s)\n", fnum,
                         getFunctionNameFromEnum((enum detFuncs)fnum)));
         ret = (*flist[fnum])(file_des);
 

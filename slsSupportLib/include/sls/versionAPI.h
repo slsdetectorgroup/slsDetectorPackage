2b5470ef59a04c27c3f76f5629cbbb0e24b8835c
Dhanya Thattil <dhanya.thattil@psi.ch>
2022-06-08 16:40:26 +0200

    update client api

--- slsSupportLib/include/sls/versionAPI.h
+++ slsSupportLib/include/sls/versionAPI.h
@@ -1,10 +1,7 @@
 // SPDX-License-Identifier: LGPL-3.0-or-other
 // Copyright (C) 2021 Contributors to the SLS Detector Package
 /** API versions */
-#define GITBRANCH    "developer"
-#define APILIB       0x220408
-#define APIRECEIVER  0x220408
-#define APIGUI       0x220328
+#define GITBRANCH    "g2rxr"
 #define APICTB 0x220524
 #define APIGOTTHARD 0x220524
 #define APIJUNGFRAU 0x220524
@@ -12,3 +9,6 @@
 #define APIEIGER 0x220524
 #define APIGOTTHARD2 0x220602
 #define APIMYTHEN3 0x220607
+#define APILIB 0x220608
+#define APIRECEIVER 0x220608
+#define APIGUI 0x220608

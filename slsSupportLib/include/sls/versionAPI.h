aa93aed4ed6d557e7389f356ddc08a2c32a6e1cb
Dhanya Thattil <dhanya.thattil@psi.ch>
2022-06-09 13:50:41 +0200

    updated client versioning

--- slsSupportLib/include/sls/versionAPI.h
+++ slsSupportLib/include/sls/versionAPI.h
@@ -2,9 +2,6 @@
 // Copyright (C) 2021 Contributors to the SLS Detector Package
 /** API versions */
 #define GITBRANCH    "developer"
-#define APILIB       0x220408
-#define APIRECEIVER  0x220408
-#define APIGUI       0x220328
 #define APICTB 0x220524
 #define APIGOTTHARD 0x220524
 #define APIJUNGFRAU 0x220524
@@ -12,3 +9,6 @@
 #define APIEIGER 0x220524
 #define APIGOTTHARD2 0x220602
 #define APIMYTHEN3 0x220607
+#define APILIB 0x220609
+#define APIRECEIVER 0x220609
+#define APIGUI 0x220609

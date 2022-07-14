ed36d4d628081abe19df6171d034b2c5dceedd04
Erik Frojdh <erik.frojdh@gmail.com>
2022-07-14 11:24:15 +0200

    import fix

--- python/slsdet/proxy.py
+++ python/slsdet/proxy.py
@@ -1,6 +1,5 @@
 # SPDX-License-Identifier: LGPL-3.0-or-other
 # Copyright (C) 2021 Contributors to the SLS Detector Package
-from asyncore import loop
 from .utils import element_if_equal
 from .enums import dacIndex
 from .defines import M3_MAX_PATTERN_LEVELS, MAX_PATTERN_LEVELS

0883c73e88b4098c3743c6bf20705dc165733e38
Dhanya Thattil <dhanya.thattil@psi.ch>
2022-10-24 10:51:50 +0200

    not updating the client versions for 'server only hot fix'

--- slsSupportLib/include/sls/versionAPI.h
+++ slsSupportLib/include/sls/versionAPI.h
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: LGPL-3.0-or-other
 // Copyright (C) 2021 Contributors to the SLS Detector Package
 /** API versions */
-#define GITBRANCH "6.1.2.rc"
+#define GITBRANCH "6.1.2"
 
 #define APICTB 0x211125
 #define APIGOTTHARD 0x211125
@@ -9,7 +9,7 @@
 #define APIMYTHEN3 0x211125
 #define APIMOENCH 0x211124
 #define APIJUNGFRAU 0x220104
-#define APILIB 0x221021
-#define APIRECEIVER 0x221021
-#define APIGUI 0x221021
-#define APIEIGER 0x221021
+#define APILIB 0x211125
+#define APIRECEIVER 0x211125
+#define APIGUI 0x211125
+#define APIEIGER 0x221024

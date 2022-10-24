fddbbe91cf8798ca10f9a817c8292639d46ecd04
Dhanya Thattil <dhanya.thattil@psi.ch>
2022-10-24 18:04:57 +0200

    wip

--- updateAPIVersion.sh
+++ updateAPIVersion.sh
@@ -51,7 +51,7 @@ if [ -z "$API_BRANCH" ]; then
 else
     #API_BRANCH is defined (3rd argument)
     echo "branch="$API_BRANCH
-    API_VAL+=$API_BRANCH
+    API_VAL+=`"$API_BRANCH"`
 fi
 
 #copy it to versionAPI.h

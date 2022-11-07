615d66d493df153ad7a4ad2e1eebeecbf12b5d80
Dhanya Thattil <33750417+thattil@users.noreply.github.com>
2022-11-07 12:42:54 +0100

    eiger server workaround fix for fw stop done signal (#571)
    - eiger server: fix for fw workaround where stop acquisition processing done signal does not come up, by removing reset in stop acquisition and waiting for2 seconds for feb done processing signal to go down, if it doesnt, throw if status is not idle.
    - error messages not setup for some eiger server errors
    - quad fix (chip signals to trim quad, both left and right registers can be different)
    - minor logical error of no consequence (stop acquisition returns a different enum than expected)

--- slsDetectorServers/eigerDetectorServer/slsDetectorFunctionList.c
+++ slsDetectorServers/eigerDetectorServer/slsDetectorFunctionList.c
@@ -2185,6 +2185,8 @@ int setTrimbits(int *chanregs, char *mess) {
 
     // if quad, set M8 and PROGRAM manually
     if (!Feb_Control_SetChipSignalsToTrimQuad(1)) {
+        sprintf(mess, "Could not set module. Could not enable chip signals to set trimbits\n");
+        LOG(logERROR, (mess)); 
         sharedMemory_unlockLocalLink();
         return FAIL;
     }
@@ -2198,6 +2200,8 @@ int setTrimbits(int *chanregs, char *mess) {
 
         // if quad, reset M8 and PROGRAM manually
         if (!Feb_Control_SetChipSignalsToTrimQuad(0)) {
+            sprintf(mess, "Could not set module. Could not disable chip signals to set trimbits\n");
+            LOG(logERROR, (mess)); 
             sharedMemory_unlockLocalLink();
             return FAIL;
         }
@@ -2208,6 +2212,8 @@ int setTrimbits(int *chanregs, char *mess) {
 
     // if quad, reset M8 and PROGRAM manually
     if (!Feb_Control_SetChipSignalsToTrimQuad(0)) {
+        sprintf(mess, "Could not set module. Could not disable chip signals to set trimbits\n");
+        LOG(logERROR, (mess));   
         sharedMemory_unlockLocalLink();
         return FAIL;
     }
@@ -2787,7 +2793,7 @@ int stopStateMachine() {
 #else
     sharedMemory_lockLocalLink();
     // sends last frames from fifo and wait for feb processing done
-    if ((Feb_Control_StopAcquisition() != STATUS_IDLE)) {
+    if (!Feb_Control_StopAcquisition()) {
         LOG(logERROR, ("failed to stop acquisition\n"));
         sharedMemory_unlockLocalLink();
         return FAIL;
@@ -2810,7 +2816,8 @@ int stopStateMachine() {
 
     // reset feb and beb
     sharedMemory_lockLocalLink();
-    Feb_Control_Reset();
+    // uncommenting this out as it randomly does not set the processing bit to high
+    //Feb_Control_Reset();
     sharedMemory_unlockLocalLink();
     if (!Beb_StopAcquisition()) {
         LOG(logERROR, ("failed to stop acquisition\n"));
@@ -2903,7 +2910,8 @@ void waitForAcquisitionEnd(int *ret, char *mess) {
     sharedMemory_lockLocalLink();
     if (Feb_Control_WaitForFinishedFlag(5000, 1) == STATUS_ERROR) {
         sharedMemory_unlockLocalLink();
-        LOG(logERROR, ("Waiting for finished flag\n"));
+        strcpy(mess, "Could not wait for finished flag\n");
+        LOG(logERROR, (mess));
         *ret = FAIL;
         return;
     }
@@ -2919,6 +2927,7 @@ void waitForAcquisitionEnd(int *ret, char *mess) {
         sharedMemory_unlockLocalLink();
         if (i == STATUS_ERROR) {
             strcpy(mess, "Could not read feb processing done register\n");
+            LOG(logERROR, (mess));
             *ret = (int)FAIL;
             return;
         }
@@ -2930,6 +2939,7 @@ void waitForAcquisitionEnd(int *ret, char *mess) {
         // wait for beb to send out all packets
         if (Beb_IsTransmitting(&isTransmitting, send_to_ten_gig, 1) == FAIL) {
             strcpy(mess, "Could not read delay counters\n");
+            LOG(logERROR, (mess));
             *ret = (int)FAIL;
             return;
         }
--- slsDetectorServers/slsDetectorServer/src/slsDetectorServer_funcs.c
+++ slsDetectorServers/slsDetectorServer/src/slsDetectorServer_funcs.c
@@ -1527,7 +1527,7 @@ int write_register(int file_des) {
         } else {
             if (readRegister(addr, &retval) == FAIL) {
                 ret = FAIL;
-                sprintf(mess, "Could not read register 0x%x.\n", addr);
+                sprintf(mess, "Could not read register 0x%x or inconsistent values. Try to read +0x100 for only left and +0x200 for only right.\n", addr);
                 LOG(logERROR, (mess));
             }
         }
@@ -1565,7 +1565,7 @@ int read_register(int file_des) {
 #elif EIGERD
     if (readRegister(addr, &retval) == FAIL) {
         ret = FAIL;
-        sprintf(mess, "Could not read register 0x%x.\n", addr);
+        sprintf(mess, "Could not read register 0x%x or inconsistent values. Try +0x100 for only left and +0x200 for only right..\n", addr);
         LOG(logERROR, (mess));
     }
 #else

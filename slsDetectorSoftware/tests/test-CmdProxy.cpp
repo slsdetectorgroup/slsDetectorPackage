9874f596f66e54e8f7fb0dd7aa5c303d7d785b59
Dhanya Thattil <dhanya.thattil@psi.ch>
2022-12-06 10:34:17 +0100

    fix to tests m3

--- slsDetectorSoftware/tests/test-CmdProxy.cpp
+++ slsDetectorSoftware/tests/test-CmdProxy.cpp
@@ -1222,7 +1222,7 @@ TEST_CASE("clkphase", "[.cmd]") {
         }
         std::string s_deg_val = "15";
         if (det_type == defs::MYTHEN3) {
-            s_deg_val = "14";
+            s_deg_val = "15";
         } else if (det_type == defs::GOTTHARD2) {
             s_deg_val = "23";
         }

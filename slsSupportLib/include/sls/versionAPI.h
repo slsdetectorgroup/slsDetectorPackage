e8e20277cea20a4d02df009df91e182fe3ccab77
Dhanya Thattil <dhanya.thattil@psi.ch>
2022-12-13 10:14:29 +0100

    formatting

--- slsSupportLib/include/sls/versionAPI.h
+++ slsSupportLib/include/sls/versionAPI.h
@@ -1,14 +1,14 @@
 // SPDX-License-Identifier: LGPL-3.0-or-other
 // Copyright (C) 2021 Contributors to the SLS Detector Package
 /** API versions */
-#define RELEASE      "developer"
-#define APILIB       "developer 0x221213"
-#define APIRECEIVER  "developer 0x221213"
+#define RELEASE     "developer"
+#define APILIB      "developer 0x221213"
+#define APIRECEIVER "developer 0x221213"
 
-#define APICTB "developer 0x221213"
-#define APIGOTTHARD "developer 0x221213"
+#define APICTB       "developer 0x221213"
+#define APIGOTTHARD  "developer 0x221213"
 #define APIGOTTHARD2 "developer 0x221213"
-#define APIJUNGFRAU "developer 0x221213"
-#define APIMYTHEN3 "developer 0x221213"
-#define APIMOENCH "developer 0x221213"
-#define APIEIGER "developer 0x221213"
+#define APIJUNGFRAU  "developer 0x221213"
+#define APIMYTHEN3   "developer 0x221213"
+#define APIMOENCH    "developer 0x221213"
+#define APIEIGER     "developer 0x221213"

c32c60517db4b8a1ea51678a847eae7e628f417e
Dhanya Thattil <dhanya.thattil@psi.ch>
2022-12-15 13:22:11 +0100

    binaries in

--- slsSupportLib/include/sls/versionAPI.h
+++ slsSupportLib/include/sls/versionAPI.h
@@ -10,5 +10,5 @@
 #define APIGOTTHARD2 "developer 0x221213"
 #define APIJUNGFRAU  "developer 0x221213"
 #define APIMYTHEN3   "developer 0x221213"
-#define APIMOENCH    "developer 0x221213"
 #define APIEIGER     "developer 0x221213"
+#define APIMOENCH "developer 0x221215"

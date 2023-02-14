7616801b3b3bcd7288fc90b70e9c08f10f4e51a7
Dhanya Thattil <dhanya.thattil@psi.ch>
2023-02-14 16:45:52 +0100

    user friendly  message added from ' dangerous to continue' to 'check firmware'

--- slsDetectorServers/mythen3DetectorServer/slsDetectorFunctionList.c
+++ slsDetectorServers/mythen3DetectorServer/slsDetectorFunctionList.c
@@ -89,7 +89,7 @@ void basictests() {
 #endif
     if (mapCSP0() == FAIL) {
         strcpy(initErrorMessage,
-               "Could not map to memory. Dangerous to continue.\n");
+               "Could not map to memory. Cannot proceed. Check Firmware.\n");
         LOG(logERROR, (initErrorMessage));
         initError = FAIL;
     }
@@ -99,8 +99,10 @@ void basictests() {
         ((validateKernelVersion(KERNEL_DATE_VRSN) == FAIL) ||
          (checkType() == FAIL) || (testFpga() == FAIL) ||
          (testBus() == FAIL))) {
-        strcpy(initErrorMessage, "Could not pass basic tests of FPGA and bus. "
-                                 "Dangerous to continue.\n");
+        sprintf(initErrorMessage,
+                "Could not pass basic tests of FPGA and bus. Cannot proceed. "
+                "Check Firmware. (Firmware version:0x%llx) \n",
+                getFirmwareVersion());
         LOG(logERROR, ("%s\n\n", initErrorMessage));
         initError = FAIL;
         return;
@@ -388,7 +390,7 @@ void initStopServer() {
         if (mapCSP0() == FAIL) {
             initError = FAIL;
             strcpy(initErrorMessage,
-                   "Stop Server: Map Fail. Dangerous to continue. Goodbye!\n");
+                   "Stop Server: Map Fail. Cannot proceed. Check Firmware. \n");
             LOG(logERROR, (initErrorMessage));
             initCheckDone = 1;
             return;
--- slsSupportLib/include/sls/versionAPI.h
+++ slsSupportLib/include/sls/versionAPI.h
@@ -2,12 +2,12 @@
 // Copyright (C) 2021 Contributors to the SLS Detector Package
 /** API versions */
 #define RELEASE      "7.0.0.rc4"
-#define APICTB       "7.0.0.rc3 0x230130"
-#define APIGOTTHARD  "7.0.0.rc3 0x230130"
-#define APIGOTTHARD2 "7.0.0.rc3 0x230130"
-#define APIJUNGFRAU  "7.0.0.rc3 0x230130"
-#define APIMYTHEN3   "7.0.0.rc3 0x230130"
-#define APIMOENCH    "7.0.0.rc3 0x230130"
 #define APIEIGER     "7.0.0.rc3 0x230130"
 #define APILIB       "7.0.0.rc4 0x230214"
 #define APIRECEIVER  "7.0.0.rc4 0x230214"
+#define APICTB "7.0.0.rc5 0x230214"
+#define APIGOTTHARD "7.0.0.rc5 0x230214"
+#define APIGOTTHARD2 "7.0.0.rc5 0x230214"
+#define APIJUNGFRAU "7.0.0.rc5 0x230214"
+#define APIMYTHEN3 "7.0.0.rc5 0x230214"
+#define APIMOENCH "7.0.0.rc5 0x230214"

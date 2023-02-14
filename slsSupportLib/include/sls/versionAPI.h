7c047cab4a78c3af3e0b0a83d6c2d3348170f8a8
Dhanya Thattil <dhanya.thattil@psi.ch>
2023-02-14 10:30:45 +0100

    udpated version to rc4

--- slsSupportLib/include/sls/versionAPI.h
+++ slsSupportLib/include/sls/versionAPI.h
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: LGPL-3.0-or-other
 // Copyright (C) 2021 Contributors to the SLS Detector Package
 /** API versions */
-#define RELEASE      "7.0.0.rc3"
+#define RELEASE      "7.0.0.rc4"
 #define APICTB       "7.0.0.rc3 0x230130"
 #define APIGOTTHARD  "7.0.0.rc3 0x230130"
 #define APIGOTTHARD2 "7.0.0.rc3 0x230130"
@@ -9,5 +9,5 @@
 #define APIMYTHEN3   "7.0.0.rc3 0x230130"
 #define APIMOENCH    "7.0.0.rc3 0x230130"
 #define APIEIGER     "7.0.0.rc3 0x230130"
-#define APILIB       "7.0.0.rc3 0x230210"
-#define APIRECEIVER  "7.0.0.rc3 0x230210"
+#define APILIB "7.0.0.rc4 0x230214"
+#define APIRECEIVER "7.0.0.rc4 0x230214"

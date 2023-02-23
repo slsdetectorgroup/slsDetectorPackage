90496bcee840d099f1f7f6cdef9d942dd9dcff84
Dhanya Thattil <dhanya.thattil@psi.ch>
2023-02-23 16:38:10 +0100

    updated release date and lib api date

--- slsSupportLib/include/sls/versionAPI.h
+++ slsSupportLib/include/sls/versionAPI.h
@@ -2,8 +2,6 @@
 // Copyright (C) 2021 Contributors to the SLS Detector Package
 /** API versions */
 #define RELEASE      "7.0.0"
-#define APILIB       "7.0.0 0x230222"
-#define APIRECEIVER  "7.0.0 0x230222"
 #define APICTB       "7.0.0 0x230222"
 #define APIGOTTHARD  "7.0.0 0x230222"
 #define APIGOTTHARD2 "7.0.0 0x230222"
@@ -11,3 +9,5 @@
 #define APIMYTHEN3   "7.0.0 0x230222"
 #define APIMOENCH    "7.0.0 0x230222"
 #define APIEIGER     "7.0.0 0x230222"
+#define APILIB       "7.0.0 0x230223"
+#define APIRECEIVER  "7.0.0 0x230222"

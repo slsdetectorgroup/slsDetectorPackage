73b9d908efd67ab63de7705e09208f35cd994bae
Dhanya Thattil <dhanya.thattil@psi.ch>
2023-07-03 17:13:22 +0200

    need to remove start readout in ctb ?

--- slsDetectorServers/slsDetectorServer/src/slsDetectorServer_funcs.c
+++ slsDetectorServers/slsDetectorServer/src/slsDetectorServer_funcs.c
@@ -8228,7 +8228,7 @@ int get_bursts_left(int file_des) {
 int start_readout(int file_des) {
     ret = OK;
     memset(mess, 0, sizeof(mess));
-#if !defined(MYTHEN3D)
+#if !defined(MYTHEN3D) && !defined(CHIPTESTBOARDD)
     functionNotImplemented();
 #else
     if (Server_VerifyLock() == OK) {

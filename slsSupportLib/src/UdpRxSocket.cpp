eb6943e36d820b6d46c71f24be8b792a826393e3
Dhanya Thattil <dhanya.thattil@psi.ch>
2023-07-26 18:07:38 +0200

    minor prints

--- slsSupportLib/src/UdpRxSocket.cpp
+++ slsSupportLib/src/UdpRxSocket.cpp
@@ -71,7 +71,7 @@ bool UdpRxSocket::ReceivePacket(char *dst) noexcept {
     LOG(logINFORED) << portnum << ":Going to receive packet";
     auto bytes_received =
         recvfrom(sockfd_, dst, packet_size_, 0, nullptr, nullptr);
-    LOG(logINFORED) << portnum << ":received " << (bytes_received == packet_size_) ? "all" : "missing";
+    LOG(logINFORED) << portnum << ":received " << ((bytes_received == packet_size_) ? "all" : "missing");
 
     return bytes_received == packet_size_;
 }

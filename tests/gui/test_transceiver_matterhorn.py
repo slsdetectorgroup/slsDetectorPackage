bb63215d9f5878705e0a0bf50cb60f9143f74f66
Bechir <bechir.braham@psi.ch>
2023-09-07 12:05:07 +0200

    add pedestal test

--- tests/gui/test_transceiver_matterhorn.py
+++ tests/gui/test_transceiver_matterhorn.py
@@ -2,6 +2,7 @@ from pathlib import Path
 
 import numpy as np
 from pytestqt.qt_compat import qt_api
+
 from .utils import setup_gui, defaultParams
 from . import main
 
@@ -37,12 +38,12 @@ def test_waveform_acq(qtbot, tmp_path):
     """
     tests Transceiver waveform acquisition and numpy saving
     """
-    params = defaultParams
-    params['image'] = False
-    params['enabled'] = [0, 1]
-    params['plotted'] = [0, 1]
+    params = defaultParams()
+    params.image = False
+    params.enabled = [0, 1]
+    params.plotted = [0, 1]
 
-    setup_gui(qtbot, main, tmp_path=tmp_path)
+    setup_gui(qtbot, main, params, tmp_path=tmp_path)
 
     qtbot.mouseClick(main.pushButtonStart, qt_api.QtCore.Qt.MouseButton.LeftButton)
 
@@ -55,3 +56,33 @@ def test_waveform_acq(qtbot, tmp_path):
     assert testArray.files == ['Transceiver 0', 'Transceiver 1']
     assert np.array_equal(dataArray['Transceiver 0'], testArray['Transceiver 0'])
     assert np.array_equal(dataArray['Transceiver 1'], testArray['Transceiver 1'])
+
+
+def test_pedestal_substraction(qtbot, tmp_path):
+    """
+    tests Transceiver image acquisition, numpy saving and pedestal substraction
+    """
+
+    # record 10 frames
+    params = defaultParams()
+    params.pedestalRecord = True
+    params.nFrames = 10
+    params.numpy = False
+    setup_gui(qtbot, main, params, tmp_path=tmp_path)
+    qtbot.mouseClick(main.pushButtonStart, qt_api.QtCore.Qt.MouseButton.LeftButton)
+    qtbot.wait_until(lambda: main.plotTab.view.labelPedestalFrames.text() == 'recorded frames: 10')
+
+    # apply pedestal and save
+    params.pedestalRecord = False
+    params.numpy = True
+    params.nFrames = 2
+    setup_gui(qtbot, main, params, tmp_path=tmp_path)
+    qtbot.mouseClick(main.pushButtonStart, qt_api.QtCore.Qt.MouseButton.LeftButton)
+    acqIndex = main.acquisitionTab.view.spinBoxAcquisitionIndex.value() - 1
+    newPath = main.acquisitionTab.outputDir / f'{main.acquisitionTab.outputFileNamePrefix}_{acqIndex}.npy'
+    qtbot.wait_until(lambda: newPath.is_file())
+
+    testArray = np.load(newPath)
+
+    assert testArray.shape == (2, 48, 48)
+    assert np.array_equal(testArray, np.zeros((2, 48, 48)))
--- tests/gui/utils.py
+++ tests/gui/utils.py
@@ -2,53 +2,57 @@ from pathlib import Path
 
 from pyctbgui.utils.defines import Defines
 
-defaultParams = {
-    'image': True,
-    "chip": "Matterhorn",
-    "numpy": True,
-    "mode": "Transceiver",
-    "outputDir": "/tmp",
-    "filename": "run",
-    "nFrames": 1,
-    "enabled": [],
-    'plotted': []
-}
+
+class defaultParams:
+    image = True
+    chip = "Matterhorn"
+    numpy = True
+    mode = "Transceiver"
+    outputDir = "/tmp"
+    filename = "run"
+    nFrames = 1
+    enabled = []
+    plotted = []
+    pedestalRecord = True
 
 
-def setup_gui(qtbot, widget, params=defaultParams, tmp_path=Path('/tmp')):
-    if params['image']:
+def setup_gui(qtbot, widget, params=defaultParams(), tmp_path=Path('/tmp')):
+    if params.image:
         widget.plotTab.view.radioButtonImage.setChecked(True)
         widget.plotTab.view.radioButtonWaveform.setChecked(False)
     else:
         widget.plotTab.view.radioButtonWaveform.setChecked(True)
         widget.plotTab.view.radioButtonImage.setChecked(False)
-        if params['mode'] == "Transceiver":
+        if params.mode == "Transceiver":
             widget.tabWidget.setCurrentIndex(Defines.transceiver.tabIndex)
 
             for i in range(Defines.transceiver.count):
                 # check or uncheck enable/plot checkboxes
                 enable = getattr(widget.transceiverTab.view, f"checkBoxTransceiver{i}")
-                enable.setChecked(i in params['enabled'])
+                enable.setChecked(i in params.enabled)
                 enable = getattr(widget.transceiverTab.view, f"checkBoxTransceiver{i}Plot")
-                enable.setChecked(i in params['plotted'])
+                enable.setChecked(i in params.plotted)
 
-    qtbot.keyClicks(widget.plotTab.view.comboBoxPlot, params['chip'])
-    widget.acquisitionTab.view.checkBoxFileWriteNumpy.setChecked(params['numpy'])
-    qtbot.keyClicks(widget.acquisitionTab.view.comboBoxROMode, params['mode'])
+    qtbot.keyClicks(widget.plotTab.view.comboBoxPlot, params.chip)
+    widget.acquisitionTab.view.checkBoxFileWriteNumpy.setChecked(params.numpy)
+    qtbot.keyClicks(widget.acquisitionTab.view.comboBoxROMode, params.mode)
 
     widget.acquisitionTab.view.lineEditFilePath.setText(str(tmp_path))
     widget.acquisitionTab.setFilePath()
 
-    widget.acquisitionTab.view.lineEditFileName.setText(params['filename'])
+    widget.acquisitionTab.view.lineEditFileName.setText(params.filename)
     widget.acquisitionTab.setFileName()
 
-    widget.acquisitionTab.view.spinBoxFrames.setValue(params['nFrames'])
+    widget.acquisitionTab.view.spinBoxFrames.setValue(params.nFrames)
     widget.acquisitionTab.setFrames()
 
-    qtbot.wait_until(lambda: widget.acquisitionTab.view.spinBoxFrames.value() == 1)
+    widget.plotTab.view.radioButtonPedestalRecord.setChecked(params.pedestalRecord)
+    widget.plotTab.view.radioButtonPedestalApply.setChecked(not params.pedestalRecord)
 
-    assert widget.acquisitionTab.view.comboBoxROMode.currentText() == params['mode']
-    assert widget.plotTab.view.comboBoxPlot.currentText() == params['chip']
-    assert widget.acquisitionTab.writeNumpy == params['numpy']
-    assert widget.acquisitionTab.view.spinBoxFrames.value() == params['nFrames']
+    qtbot.wait_until(lambda: widget.acquisitionTab.view.spinBoxFrames.value() == params.nFrames)
+
+    assert widget.acquisitionTab.view.comboBoxROMode.currentText() == params.mode
+    assert widget.plotTab.view.comboBoxPlot.currentText() == params.chip
+    assert widget.acquisitionTab.writeNumpy == params.numpy
+    assert widget.acquisitionTab.view.spinBoxFrames.value() == params.nFrames
     assert widget.acquisitionTab.outputDir == Path(str(tmp_path))

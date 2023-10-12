8d97b677075ea387374d8053aa52d333b3fc0e4d
froejdh_e <erik.frojdh@psi.ch>
2023-10-12 15:35:16 +0200

    added variadic template for checking if a result contains only specified values

--- slsSupportLib/include/sls/TypeTraits.h
+++ slsSupportLib/include/sls/TypeTraits.h
@@ -103,4 +103,12 @@ template <typename T> struct is_vector : public std::false_type {};
 template <typename T>
 struct is_vector<std::vector<T>> : public std::true_type {};
 
+template<bool...> struct bool_pack{};
+
+//credit to fluentcpp.com
+template<bool... Bs>
+using conjunction = std::is_same<bool_pack<true,Bs...>, bool_pack<Bs..., true>>;
+template<typename T, typename... Ts>
+using AllSame = std::enable_if_t<std::conjunction_v<std::is_same<T, Ts>...>>;
+
 } // namespace sls

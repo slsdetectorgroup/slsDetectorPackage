0e43072db8e41bbe8be235582b7f3da4c3cc2c6c
Dhanya Thattil <dhanya.thattil@psi.ch>
2023-10-18 17:04:26 +0200

    eiger required fw version to 32: fix for blocking trigger in quad (#833)

--- slsSupportLib/include/sls/versionAPI.h
+++ slsSupportLib/include/sls/versionAPI.h
@@ -9,5 +9,5 @@
 #define APIGOTTHARD2 "developer 0x230922"
 #define APIMYTHEN3   "developer 0x230922"
 #define APIMOENCH    "developer 0x230922"
-#define APIEIGER     "developer 0x230922"
 #define APIJUNGFRAU "developer 0x231018"
+#define APIEIGER "developer 0x231018"

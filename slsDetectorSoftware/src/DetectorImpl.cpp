9fb151dac944cd1d03da6282c0f9a7b1a823249c
Dhanya Thattil <dhanya.thattil@psi.ch>
2023-11-02 16:18:26 +0100

    minor

--- slsDetectorSoftware/src/DetectorImpl.cpp
+++ slsDetectorSoftware/src/DetectorImpl.cpp
@@ -1239,7 +1239,7 @@ int DetectorImpl::acquire() {
             auto statusList = Parallel(&Module::getRunStatus, {});
             if (statusList.any(ERROR)) 
                 status = ERROR;
-            if (statusList.contains_only(IDLE, STOPPED)) 
+            else if (statusList.contains_only(IDLE, STOPPED)) 
                 status = STOPPED;
             else 
                 status = statusList.squash(RUNNING);

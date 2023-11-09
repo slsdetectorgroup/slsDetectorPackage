c64e87a2b6efa46d8d35e3acd3f7945febfc544f
Dhanya Thattil <dhanya.thattil@psi.ch>
2023-11-09 14:21:11 +0100

    7.0.3.rc: fix server logic in checking detector idle (#859)
    * fix buggy logic in checking detector idle and an argument check

--- slsDetectorServers/slsDetectorServer/src/slsDetectorServer_funcs.c
+++ slsDetectorServers/slsDetectorServer/src/slsDetectorServer_funcs.c
@@ -4667,7 +4667,8 @@ int set_read_n_rows(int file_des) {
     functionNotImplemented();
 #else
     // only set
-    if (Server_VerifyLock() == OK) {
+    if ((Server_VerifyLock() == OK) &&
+        (check_detector_idle("set number of rows") == OK)) {
         if (arg <= 0 || arg > MAX_ROWS_PER_READOUT) {
             ret = FAIL;
             sprintf(mess,
@@ -4699,8 +4700,7 @@ int set_read_n_rows(int file_des) {
                 LOG(logERROR, (mess));
             } else
 #elif JUNGFRAUD
-            if ((check_detector_idle("set number of rows") == OK) &&
-                (arg % READ_N_ROWS_MULTIPLE != 0)) {
+            if (arg % READ_N_ROWS_MULTIPLE != 0) {
                 ret = FAIL;
                 sprintf(mess,
                         "Could not set number of rows. %d must be a multiple "
@@ -8283,9 +8283,9 @@ int set_master(int file_des) {
     functionNotImplemented();
 #else
     // only set
-    if (Server_VerifyLock() == OK) {
-        if ((check_detector_idle("set master") == OK) &&
-            (arg != 0 && arg != 1)) {
+    if ((Server_VerifyLock() == OK) &&
+        (check_detector_idle("set master") == OK)) {
+        if (arg != 0 && arg != 1) {
             ret = FAIL;
             sprintf(mess, "Could not set master. Invalid argument %d.\n", arg);
             LOG(logERROR, (mess));
@@ -8839,9 +8839,9 @@ int set_flip_rows(int file_des) {
     functionNotImplemented();
 #else
     // only set
-    if (Server_VerifyLock() == OK) {
-        if ((check_detector_idle("set flip rows") == OK) &&
-            (arg != 0 && arg != 1)) {
+    if ((Server_VerifyLock() == OK) &&
+        (check_detector_idle("set flip rows") == OK)) {
+        if (arg != 0 && arg != 1) {
             ret = FAIL;
             sprintf(mess, "Could not set flip rows. Invalid argument %d.\n",
                     arg);
@@ -10163,9 +10163,9 @@ int set_synchronization(int file_des) {
     functionNotImplemented();
 #else
     // only set
-    if (Server_VerifyLock() == OK) {
-        if ((check_detector_idle("set synchronization") == OK) &&
-            (arg != 0 && arg != 1)) {
+    if ((Server_VerifyLock() == OK) &&
+        (check_detector_idle("set synchronization") == OK)) {
+        if (arg != 0 && arg != 1) {
             ret = FAIL;
             sprintf(mess,
                     "Could not set synchronization. Invalid argument %d.\n",

953dfe8c99f3b392be43e3efa4a0586292d5de72
Dhanya Thattil <dhanya.thattil@psi.ch>
2023-11-13 17:16:02 +0100

    changing the min exptime and acq time cycles to 0 for testing

--- slsDetectorServers/moenchDetectorServer/slsDetectorFunctionList.c
+++ slsDetectorServers/moenchDetectorServer/slsDetectorFunctionList.c
@@ -694,7 +694,8 @@ int64_t getNumTriggers() {
 
 int setExpTime(int64_t val) {
     if (val < MIN_EXPTIME_NS_VAL) {
-        LOG(logERROR, ("Invalid exptime: %lld ns (min: %d ns)\n", (long long int)val, MIN_EXPTIME_NS_VAL));
+        LOG(logERROR, ("Invalid exptime: %lld ns (min: %d ns)\n",
+                       (long long int)val, MIN_EXPTIME_NS_VAL));
         return FAIL;
     }
     LOG(logINFO, ("Setting exptime %lld ns\n", (long long int)val));
--- slsDetectorServers/moenchDetectorServer/slsDetectorServer_defs.h
+++ slsDetectorServers/moenchDetectorServer/slsDetectorServer_defs.h
@@ -55,12 +55,11 @@
 #define MAX_ROWS_PER_READOUT (400)
 #define ROWS_PER_PACKET      (8)
 
-
 /* Defines in the Firmware */
-#define MIN_EXPTIME_NS_VAL        (100) // 100 ns
+#define MIN_EXPTIME_NS_VAL        (0) // 0 ns
 #define MAX_TIMESLOT_VAL          (0x1F)
 #define MAX_THRESHOLD_TEMP_VAL    (127999) // millidegrees
-#define ACQ_TIME_MIN_CLOCK        (2)
+#define ACQ_TIME_MIN_CLOCK        (0)
 #define ASIC_FILTER_MAX_RES_VALUE (1)
 #define MAX_SELECT_CHIP10_VAL     (63)
 

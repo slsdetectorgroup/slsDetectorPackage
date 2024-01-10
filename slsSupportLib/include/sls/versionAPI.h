f7976400ed9d1c27b4e31248dad75494b8a66d1f
Dhanya Thattil <dhanya.thattil@psi.ch>
2024-01-10 16:59:28 +0100

    updated binaries

--- slsSupportLib/include/sls/versionAPI.h
+++ slsSupportLib/include/sls/versionAPI.h
@@ -4,7 +4,6 @@
 #define RELEASE      "developer"
 #define APILIB       "developer 0x230224"
 #define APIRECEIVER  "developer 0x230224"
-#define APIEIGER "developer 0x240109"
 #define APICTB "developer 0x240110"
 #define APIGOTTHARD "developer 0x240110"
 #define APIGOTTHARD2 "developer 0x240110"
@@ -12,3 +11,4 @@
 #define APIMYTHEN3 "developer 0x240110"
 #define APIMOENCH "developer 0x240110"
 #define APIXILINXCTB "developer 0x240110"
+#define APIEIGER "developer 0x240110"

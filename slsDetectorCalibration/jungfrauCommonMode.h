457cc0f70cd7e28d7c0d250cc875ad9a7b1a1d36
Kirsty Paton <kirsty.paton@psi.ch>
2024-01-22 14:12:23 +0100

    Created jungfrauCommonMode.h to apply common mode correction for full Jungfrau module.

---
+++ slsDetectorCalibration/jungfrauCommonMode.h
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: LGPL-3.0-or-other
+// Copyright (C) 2021 Contributors to the SLS Detector Package
+#ifndef COMMONMODEJF_H
+#define COMMONMODEJF_H
+
+#include "commonModeSubtractionNew.h"
+
+class commonModeSubtractionSuperColumnJF : public commonModeSubtraction {
+  public:
+    commonModeSubtractionSuperColumnJF()
+        : commonModeSubtraction(32){};
+    virtual int getROI(int ix, int iy) { int top=iy/256; int sc=ix/64; return sc+top*16; };
+/*
+    virtual void addToCommonMode(double val, int ix = 0, int iy = 0) {
+            int iroi = getROI(ix, iy);
+            // cout << iy << " " << ix << " " << iroi ;
+            if (iroi >= 0 && iroi < nROI) {
+                mean[iroi] += val;
+                mean2[iroi] += val * val;
+                nCm[iroi]++;
+                if (nCm[iroi] > 64*256)
+                    std::cout << "Too many pixels added " << nCm[iroi] << std::endl
+           
+        }
+    };
+*/
+    virtual commonModeSubtractionSuperColumnJF *Clone() {
+        return new commonModeSubtractionSuperColumnJF();
+    };
+
+  
+};
+
+
+#endif

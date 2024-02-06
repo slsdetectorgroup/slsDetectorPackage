62c428320f3da47759aeafa2def32d1f4bbf1be5
Dhanya Thattil <dhanya.thattil@psi.ch>
2024-02-06 11:45:55 +0100

    moench server name and version renaming

--- slsSupportLib/include/sls/versionAPI.h
+++ slsSupportLib/include/sls/versionAPI.h
@@ -8,6 +8,6 @@
 #define APIGOTTHARD2 "8.0.0 0x231109"
 #define APIJUNGFRAU  "8.0.0 0x231109"
 #define APIMYTHEN3   "8.0.0 0x231109"
-#define APIMOENCH    "8.0.0 0x231109"
 #define APIEIGER     "8.0.0 0x231109"
 #define APILIB       "8.0.1 0x240112"
+#define APIMOENCH "8.0.2 0x240206"

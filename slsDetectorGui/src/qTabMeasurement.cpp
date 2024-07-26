b95da2e8ed349976d02de0583827d44be0efebe2
Zhengheng Li <zhenghenge@gmail.com>
2024-07-26 11:22:05 +0800

    fix the file path resetting issue of GUI
    in the case where different modules have different fpath setting.

--- slsDetectorGui/src/qTabMeasurement.cpp
+++ slsDetectorGui/src/qTabMeasurement.cpp
@@ -839,8 +839,8 @@ void qTabMeasurement::UpdateProgress() {
 int qTabMeasurement::VerifyOutputDirectoryError() {
     try {
         auto retval = det->getFilePath();
-        for (auto &it : retval) {
-            det->setFilePath(it);
+        for (size_t i = 0; i < retval.size(); i++) {
+            det->setFilePath(retval[i], {i});
         }
         return slsDetectorDefs::OK;
     }

58c3f3f7a9de17c188ee655b04616c1fcd60d787
Dhanya Thattil <dhanya.thattil@psi.ch>
2024-09-03 17:37:46 +0200

    802/release notes (#954)
    * updated release notes
    
    * versioning

--- slsSupportLib/include/sls/versionAPI.h
+++ slsSupportLib/include/sls/versionAPI.h
@@ -1,13 +1,13 @@
 // SPDX-License-Identifier: LGPL-3.0-or-other
 // Copyright (C) 2021 Contributors to the SLS Detector Package
 /** API versions */
-#define RELEASE      "8.0.1"
-#define APIRECEIVER  "8.0.0 0x231108"
+#define RELEASE      "8.0.2"
 #define APICTB       "8.0.0 0x231109"
 #define APIGOTTHARD  "8.0.0 0x231109"
 #define APIGOTTHARD2 "8.0.0 0x231109"
 #define APIEIGER     "8.0.0 0x231109"
-#define APILIB       "8.0.1 0x240112"
-#define APIMOENCH    "8.0.2 0x240703"
-#define APIMYTHEN3   "8.0.2 0x240715"
-#define APIJUNGFRAU  "8.0.2 0x240822"
+#define APILIB       "8.0.2 0x240827"
+#define APIRECEIVER  "8.0.2 0x240827"
+#define APIJUNGFRAU  "8.0.2 0x240827"
+#define APIMOENCH    "8.0.2 0x240827"
+#define APIMYTHEN3   "8.0.2 0x240827"

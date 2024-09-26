007330caa7f58079d0ad751d496440ff7832c1fe
Dhanya Thattil <dhanya.thattil@psi.ch>
2024-09-26 15:39:49 +0200

    format

--- pyctbgui/src/thread_utils.h
+++ pyctbgui/src/thread_utils.h
@@ -1,10 +1,10 @@
 #pragma once
-#include <stdint.h>
 #include <stddef.h>
-typedef struct{
-    uint16_t* src;
-    uint16_t* dst; 
-    uint32_t* pm;
+#include <stdint.h>
+typedef struct {
+    uint16_t *src;
+    uint16_t *dst;
+    uint32_t *pm;
     size_t n_frames;
     size_t n_pixels;
-}thread_args;
+} thread_args;

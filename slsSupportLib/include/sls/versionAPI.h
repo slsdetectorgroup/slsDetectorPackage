a6191f9fea85c9adb8f427250726843d54abac37
Dhanya Thattil <dhanya.thattil@psi.ch>
2024-10-24 14:11:39 +0200

    formatting, merge fix

--- slsSupportLib/include/sls/versionAPI.h
+++ slsSupportLib/include/sls/versionAPI.h
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: LGPL-3.0-or-other
 // Copyright (C) 2021 Contributors to the SLS Detector Package
 /** API versions */
-<<<<<<< HEAD
 #define RELEASE      "developer"
 #define APICTB       "developer 0x240918"
 #define APIGOTTHARD  "developer 0x240918"
@@ -13,16 +12,3 @@
 #define APILIB       "developer 0x241021"
 #define APIGOTTHARD2 "developer 0x241022"
 #define APIJUNGFRAU  "developer 0x241024"
-=======
-#define RELEASE "9.0.0"
-#define APICTB       "9.0.0 0x241014"
-#define APIGOTTHARD  "9.0.0 0x241014"
-#define APIMYTHEN3   "9.0.0 0x241014"
-#define APIMOENCH    "9.0.0 0x241014"
-#define APIXILINXCTB "9.0.0 0x241014"
-#define APIEIGER     "9.0.0 0x241014"
-#define APIRECEIVER  "9.0.0 0x241014"
-#define APILIB       "9.0.0 0x241018"
-#define APIGOTTHARD2 "9.0.0 0x241022"
-#define APIJUNGFRAU  "9.0.0 0x241024"
->>>>>>> 48a5ec2e3 (formatting)

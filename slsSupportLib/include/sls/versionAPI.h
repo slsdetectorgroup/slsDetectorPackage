09e82cf4dde61821ee90b6c9429070d79524209e
Dhanya Thattil <dhanya.thattil@psi.ch>
2024-10-24 11:03:36 +0200

    formatting

--- slsSupportLib/include/sls/versionAPI.h
+++ slsSupportLib/include/sls/versionAPI.h
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: LGPL-3.0-or-other
 // Copyright (C) 2021 Contributors to the SLS Detector Package
 /** API versions */
+<<<<<<< HEAD
 #define RELEASE      "developer"
 #define APICTB       "developer 0x240918"
 #define APIGOTTHARD  "developer 0x240918"
@@ -9,6 +10,19 @@
 #define APIEIGER     "developer 0x240918"
 #define APIMYTHEN3   "developer 0x241001"
 #define APIRECEIVER  "developer 0x241014"
-#define APILIB "developer 0x241021"
+#define APILIB       "developer 0x241021"
 #define APIGOTTHARD2 "developer 0x241022"
-#define APIJUNGFRAU "developer 0x241024"
+#define APIJUNGFRAU  "developer 0x241024"
+=======
+#define RELEASE "9.0.0"
+#define APICTB       "9.0.0 0x241014"
+#define APIGOTTHARD  "9.0.0 0x241014"
+#define APIMYTHEN3   "9.0.0 0x241014"
+#define APIMOENCH    "9.0.0 0x241014"
+#define APIXILINXCTB "9.0.0 0x241014"
+#define APIEIGER     "9.0.0 0x241014"
+#define APIRECEIVER  "9.0.0 0x241014"
+#define APILIB       "9.0.0 0x241018"
+#define APIGOTTHARD2 "9.0.0 0x241022"
+#define APIJUNGFRAU  "9.0.0 0x241024"
+>>>>>>> 48a5ec2e3 (formatting)

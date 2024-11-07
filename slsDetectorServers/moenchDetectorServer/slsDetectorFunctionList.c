5088e5a2053e7ccd90a739143c2d5bafecec9965
Dhanya Thattil <dhanya.thattil@psi.ch>
2024-11-07 11:24:46 +0100

    Dev/detach pthreads not joining (#1019)
    * detach the pthreads that are not joining else memory leak
    
    * removed the clean up comment, also refactored the pthread join/detach for ctb 1g

--- slsDetectorServers/moenchDetectorServer/slsDetectorFunctionList.c
+++ slsDetectorServers/moenchDetectorServer/slsDetectorFunctionList.c
@@ -1858,7 +1858,8 @@ int startStateMachine() {
         LOG(logERROR, ("Could not start Virtual acquisition thread\n"));
         sharedMemory_setStatus(IDLE);
         return FAIL;
-    }
+    } else
+        pthread_detach(pthread_virtual_tid);
     LOG(logINFOGREEN, ("Virtual Acquisition started\n"));
     return OK;
 #endif

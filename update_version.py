1a278d44aecc36aa5cd45d0d782f5ee831c140a2
Dhanya Thattil <dhanya.thattil@psi.ch>
2024-11-12 11:45:44 +0100

    fix in comment

--- update_version.py
+++ update_version.py
@@ -1,7 +1,7 @@
 # SPDX-License-Identifier: LGPL-3.0-or-other
 # Copyright (C) 2021 Contributors to the SLS Detector Package
 """
-Script to update VERSION file with semantic versioning if provided as an argument, or with today's date in '0xyymmdd' hex format if no argument is provided.
+Script to update VERSION file with semantic versioning if provided as an argument, or with 0.0.0 if no argument is provided.
 """
 
 import sys

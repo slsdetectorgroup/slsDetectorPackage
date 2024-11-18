3fcca5540fa4d03f21d66c73bddfbfcf8435257e
Dhanya Thattil <dhanya.thattil@psi.ch>
2024-11-18 20:32:18 +0100

    renamed conda-recipe folder, install version file from cmakelists.txt into the root folder, modify build script for conda to check if build or install folder exists, commented out python variants temporarily, changed meta.yaml to get version from hard coded value, and source include VERSION file in conda, configure file in python cmakelists to copy to build folder, changed python setup.py and init.py to read the VERSIOn from root dir

--- python/setup.py
+++ python/setup.py
@@ -12,7 +12,7 @@ from pybind11.setup_helpers import Pybind11Extension, build_ext
 
 
 def read_version():
-    version_file = os.path.join(os.path.dirname(__file__), "slsdet", "VERSION")
+    version_file = os.path.join(os.path.dirname(__file__), "..", "VERSION")
     with open(version_file, "r") as f:
         return f.read().strip()
 

1ffb469d92ce314ffd6f1ada9a3ddb67aac7a3ca
Dhanya Thattil <dhanya.thattil@psi.ch>
2024-11-19 16:38:21 +0100

    update release in versionaPI.h also using the update_version.py that updates the version in the VERSION file, in conda build, read this VERSION file to set environment variable SLS_DET_VERSION and use it in the meta.yaml so that nowhere we explicitly use version (except to create servers)

--- conda-recipe/build_pylib.sh
+++ conda-recipe/build_pylib.sh
@@ -2,6 +2,9 @@
 # Copyright (C) 2021 Contributors to the SLS Detector Package
 
 echo "|<-------- starting python build"
+
 cd python
 cp ../VERSION slsdet/VERSION
+export SLS_DET_VERSION=$(cat python/slsdet/VERSION)
+
 ${PYTHON} setup.py install 

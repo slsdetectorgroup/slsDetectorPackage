2965264bd76d53b8fa35f2a1852d2f82beb09bc3
Dhanya Thattil <dhanya.thattil@psi.ch>
2024-11-19 10:29:11 +0100

    using package_data

--- python/slsdet/__init__.py
+++ python/slsdet/__init__.py
@@ -13,7 +13,7 @@ from .gotthard import Gotthard
 from .moench import Moench
 from .pattern import Pattern, patternParameters
 from .gaincaps import Mythen3GainCapsWrapper
-#from pkg_resources import resource_filename
+from pkg_resources import resource_filename, DistributionNotFound
 
 import _slsdet
 xy = _slsdet.xy
@@ -33,12 +33,18 @@ pedestalParameters = _slsdet.pedestalParameters
 
 import os
 def read_version():
-    root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '../..'))
-    version_file = os.path.join(root_dir, 'VERSION')
-    #version_file = resource_filename('slsdet', '../../VERSION')
-    #version_file = os.path.join(os.path.dirname(__file__), "..", "..", "VERSION")
-    with open(version_file, "r") as f:
-        return f.read().strip()
-
+    try:
+        version_file = resource_filename(__name__, 'VERSION')
+        with open(version_file, "r") as f:
+            return f.read().strip()
+    except (FileNotFoundError, DistributionNotFound):
+        root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '../..'))
+        version_file = os.path.join(root_dir, 'VERSION')
+        #version_file = os.path.join(os.path.dirname(__file__), "..", "VERSION")
+        with open(version_file, "r") as f:
+            return f.read().strip()
+    else:
+        raise RuntimeError("VERSION file not found in package or in expected directory.")
+    
 __version__ = read_version()
 

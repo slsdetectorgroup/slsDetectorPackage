3a42a5da23ceffa6d9b52dc16f12d6aea4960a56
Dhanya Thattil <dhanya.thattil@psi.ch>
2024-11-20 12:06:21 +0100

    using compile definition SLS_DET_VERSION instead of script ot update versionAPI.h for RELEASE value for packageVersion command, simplified project version as it is already check for major minor etc elsewhere in project

--- slsSupportLib/include/sls/versionAPI.h
+++ slsSupportLib/include/sls/versionAPI.h
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: LGPL-3.0-or-other
 // Copyright (C) 2021 Contributors to the SLS Detector Package
 /** API versions */
-#define RELEASE      "9.0.0"
 #define APIRECEIVER  "9.0.0 0x241014"
 #define APILIB       "9.0.0 0x241018"
 #define APICTB       "9.0.0 0x241107"

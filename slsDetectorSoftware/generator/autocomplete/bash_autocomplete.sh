b2633b07306321d4d87497fbbfbc558b475bec28
Dhanya Thattil <dhanya.thattil@psi.ch>
2024-11-28 17:33:27 +0100

    ctb: make patternX path autocomplete

--- slsDetectorSoftware/generator/autocomplete/bash_autocomplete.sh
+++ slsDetectorSoftware/generator/autocomplete/bash_autocomplete.sh
@@ -1505,6 +1505,7 @@ fi
 if [[ ${IS_GET} -eq 0 ]]; then
 if [[ "${cword}" == "2" ]]; then
 FCN_RETURN=""
+IS_PATH=1
 fi
 fi
 return 0
@@ -1514,6 +1515,7 @@ FCN_RETURN=""
 if [[ ${IS_GET} -eq 1 ]]; then
 if [[ "${cword}" == "2" ]]; then
 FCN_RETURN=""
+IS_PATH=1
 fi
 fi
 return 0

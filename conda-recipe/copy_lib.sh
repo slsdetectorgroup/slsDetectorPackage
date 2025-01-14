c12475e6f8f3bc207956d512ea22ac3e019d1342
Dhanya Thattil <dhanya.thattil@psi.ch>
2025-01-14 11:58:28 +0100

    added sls_detector_acquire_zmq binary in conda

--- conda-recipe/copy_lib.sh
+++ conda-recipe/copy_lib.sh
@@ -11,6 +11,7 @@ cp build/install/lib/* $PREFIX/lib/
 
 #Binaries
 cp build/install/bin/sls_detector_acquire $PREFIX/bin/.
+cp build/install/bin/sls_detector_acquire_zmq $PREFIX/bin/.
 cp build/install/bin/sls_detector_get $PREFIX/bin/.
 cp build/install/bin/sls_detector_put $PREFIX/bin/.
 cp build/install/bin/sls_detector_help $PREFIX/bin/.

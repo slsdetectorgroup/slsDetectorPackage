e23c8dfe5ce3ac9516766b9a461bccc1557640d1
Dhanya Thattil <dhanya.thattil@psi.ch>
2025-02-07 13:49:07 +0100

    commnet

--- python/scripts/frameSynchronizerPullSocket.py
+++ python/scripts/frameSynchronizerPullSocket.py
@@ -1,3 +1,6 @@
+# SPDX-License-Identifier: LGPL-3.0-or-other
+# Copyright (C) 2021 Contributors to the SLS Detector Package
+# Script to get combined zmq packets from frame synchronizer using pull zmq sockets
 import json
 import zmq
 

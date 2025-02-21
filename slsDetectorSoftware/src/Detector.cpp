4be47d6a304a9d9fd64529aac79a092904473422
Dhanya Thattil <dhanya.thattil@psi.ch>
2025-02-21 14:27:27 +0100

    warn that the multi id is ignored in config file? or throw?

--- slsDetectorSoftware/src/Detector.cpp
+++ slsDetectorSoftware/src/Detector.cpp
@@ -97,8 +97,15 @@ void Detector::loadParameters(const std::string &fname) {
 void Detector::loadParameters(const std::vector<std::string> &parameters) {
     Caller caller(this);
     CmdParser parser;
+    const int detShmId = getShmId();
     for (const auto &current_line : parameters) {
         parser.Parse(current_line);
+        int m_id = parser.multi_id();
+        if (m_id != detShmId) {
+            LOG(logWARNING)
+                << "Ignoring multi-detector index [" + std::to_string(m_id) +
+                       "] in config file. Maybe for 'config' command?";
+        }
         caller.call(parser.command(), parser.arguments(), parser.detector_id(),
                     defs::PUT_ACTION, std::cout, parser.receiver_id());
     }

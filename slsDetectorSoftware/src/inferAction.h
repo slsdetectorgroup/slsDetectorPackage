b4dc1dde6c5cf747bad265a3f496c1c9a5fd4930
Dhanya Thattil <dhanya.thattil@psi.ch>
2025-02-27 10:42:21 +0100

    patternX to pattern (#1120)
    * pattern is not an issue for yaml. changing patternX to pattern everywhere
    
    * added patternX to deprecated

--- slsDetectorSoftware/src/inferAction.h
+++ slsDetectorSoftware/src/inferAction.h
@@ -488,7 +488,7 @@ class InferAction {
         {"patnloop1", &InferAction::patnloop1},
         {"patnloop2", &InferAction::patnloop2},
         {"patsetbit", &InferAction::patsetbit},
-        {"patternX", &InferAction::pattern},
+        {"pattern", &InferAction::pattern},
         {"patternstart", &InferAction::patternstart},
         {"patwait", &InferAction::patwait},
         {"patwait0", &InferAction::patwait0},

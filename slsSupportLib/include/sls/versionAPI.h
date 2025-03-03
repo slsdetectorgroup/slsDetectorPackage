87d40d418e3bf39266c359271aa57b27e6a4f8a9
Dhanya Thattil <dhanya.thattil@psi.ch>
2025-03-03 15:30:35 +0100

    reverted servers of eiger, gotthard, gotthard2, moench and mythen3 as nothing has really changed there other than formatting

--- slsSupportLib/include/sls/versionAPI.h
+++ slsSupportLib/include/sls/versionAPI.h
@@ -1,13 +1,13 @@
 // SPDX-License-Identifier: LGPL-3.0-or-other
 // Copyright (C) 2021 Contributors to the SLS Detector Package
 /** API versions */
+#define APIGOTTHARD  "9.0.0 0x241121"
+#define APIGOTTHARD2 "9.0.0 0x241121"
+#define APIMYTHEN3   "9.0.0 0x241121"
+#define APIMOENCH    "9.0.0 0x241121"
+#define APIEIGER     "9.0.0 0x241121"
 #define APILIB       "9.1.0 0x250204"
 #define APIRECEIVER  "9.1.0 0x250204"
 #define APICTB       "9.1.0 0x250204"
-#define APIGOTTHARD  "9.1.0 0x250204"
-#define APIGOTTHARD2 "9.1.0 0x250204"
-#define APIMYTHEN3   "9.1.0 0x250204"
-#define APIMOENCH    "9.1.0 0x250204"
 #define APIXILINXCTB "9.1.0 0x250204"
-#define APIEIGER     "9.1.0 0x250204"
 #define APIJUNGFRAU  "9.1.0 0x250224"

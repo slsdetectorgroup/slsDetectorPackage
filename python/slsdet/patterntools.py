d7b98fd77e6548b2f9e6237398a75b899cb24106
froejdh_e <erik.frojdh@psi.ch>
2025-03-11 18:15:29 +0100

    added tests

--- python/slsdet/patterntools.py
+++ python/slsdet/patterntools.py
@@ -5,54 +5,58 @@ from .format import hexFormat, hexFormat_nox, binFormat, binFormat_nob, decForma
 
 
 class pat:
+    """
+    Class to generate a pattern for the SLS detector. Intents to as closely as possible 
+    mimic the old pattern generation in the C code.
+    """
     def __init__(self):
-        self.pattern=Pattern()
-        self.iaddr=0
+        self.pattern = Pattern()
+        self.iaddr = 0
 
-    def SB(self,*args):
+    def SB(self, *args):
         for i in args:
-            self.pattern.word[self.iaddr]=setbit(i,self.pattern.word[self.iaddr])
+            self.pattern.word[self.iaddr] = setbit(i, self.pattern.word[self.iaddr])
         return self.pattern.word[self.iaddr]
  
-    def CB(self,*args):
+    def CB(self, *args):
         for i in args:
-            self.pattern.word[self.iaddr]=clearbit(i,self.pattern.word[self.iaddr])
+            self.pattern.word[self.iaddr] = clearbit(i, self.pattern.word[self.iaddr])
         return self.pattern.word[self.iaddr]
     
 
-    def pw(self,verbose=0):
-        if verbose==1:
+    def _pw(self, verbose = False):
+        if verbose:
             print(f'{self.iaddr:#06x} {self.pattern.word[self.iaddr]:#018x}') 
-        self.pattern.limits[1]=self.iaddr
-        #print("pw",self.iaddr,self.pattern.word[self.iaddr])
-        self.iaddr+=1
-        self.pattern.word[self.iaddr]=self.pattern.word[self.iaddr-1]
+        
+        self.iaddr += 1
+        self.pattern.limits[1] = self.iaddr
+        self.pattern.word[self.iaddr] = self.pattern.word[self.iaddr-1]
 
-    def PW(self,x=1,verbose=0):
+    def PW(self, x = 1, verbose = False):
         for i in range(x):
-            self.pw(verbose)
+            self._pw(verbose)
             
-    def REPEAT(self,x,verbose=0):
+    def REPEAT(self, x, verbose = False):
         for i in range(x):
-            self.pw(verbose)
+            self._pw(verbose)
 
-    def PW2(self,verbose=0):
-        self.REPEAT(2,verbose)
+    def PW2(self, verbose = 0):
+        self.REPEAT(2, verbose)
 
 
-    def CLOCKS(self,bit,times=1,length=1,verbose=0):
+    def CLOCKS(self, bit, times = 1, length = 1, verbose = False):
         """
         clocks "bit" n "times", every half clock is long "length"
-        lenght is optional, default value is 1
+        length is optional, default value is 1
         """
-        for i in range(0,times):
-            self.SB(bit); self.PW(length,verbose)
-            self.CB(bit); self.pw(length,verbose)
+        for i in range(0, times):
+            self.SB(bit); self.PW(length, verbose)
+            self.CB(bit); self.PW(length, verbose)
 
-    def CLOCK(self,bit,length=1,verbose=0):
-        self.CLOCKS(bit,1,length,verbose)
+    def CLOCK(self, bit, length = 1, verbose = 0):
+        self.CLOCKS(bit, 1, length ,verbose)
 
-    def serializer(self,value,serInBit,clkBit,nbits,msbfirst=1,length=1):
+    def serializer(self, value, serInBit, clkBit, nbits, msbfirst = True, length = 1):
         """serializer(value,serInBit,clkBit,nbits,msbfirst=1,length=1)
         Produces the .pat file needed to serialize a word into a shift register.
         value: value to be serialized
@@ -63,131 +67,167 @@ class pat:
         if 0 pushes in the LSB first
         length: length of all the PWs in the pattern
         It produces no output because it modifies directly the members of the class pat via SB and CB"""
-        c=value
-        self.CB(serInBit,clkBit)
-        self.PW(length) #generate intial line with clk and serIn to 0
-        start=0;stop=nbits;step=1
+        
+        c = value
+        self.CB(serInBit, clkBit)
+        self.PW(length) #generate initial line with clk and serIn to 0
+        
+        start = 0
+        stop = nbits
+        step = 1
+
         if msbfirst:
-            start=nbits-1;stop=-1;step=-1 #reverts loop if msb has to be pushed in first
-            for i in range(start,stop,step):
-                if c & (1<<i): 
-                    self.SB(serInBit)
-                    self.PW(length)
-                else:
-                    self.CB(serInBit)
-                    self.PW(length)
-                self.SB(clkBit)
+            start = nbits - 1
+            stop = -1
+            step =- 1 #reverts loop if msb has to be pushed in first
+
+        for i in range(start, stop, step):
+            if c & (1<<i): 
+                self.SB(serInBit)
                 self.PW(length)
-                self.CB(clkBit)
+            else:
+                self.CB(serInBit)
                 self.PW(length)
-            self.CB(serInBit,clkBit)
-            self.PW(length) #generate final line with clk and serIn to 0   
+            self.SB(clkBit)
+            self.PW(length)
+            self.CB(clkBit)
+            self.PW(length)
+
+        self.CB(serInBit, clkBit)
+        self.PW(length) #generate final line with clk and serIn to 0   
 
 
     #NOT IMPLEMENTED YET
+    #TODO! What should setstop do? Or can we remove it? 
     #def setstop():    
     #
-    #def setoutput(bit):
-    #    self.ioctrl=self.setbit(bit,self.ioctrl)
-    #
-    #def setinput(bit):
-    #    self.ioctrl=self.clearbit(bit,self.ioctrl)
-    #
-    #def setclk(bit):
+
+    def setoutput(self, bit):
+       self.pattern.ioctrl = setbit(bit, self.pattern.ioctrl)
+    
+    def setinput(self, bit):
+       self.pattern.ioctrl= clearbit(bit, self.pattern.ioctrl)
+    
+    #TODO! What should setclk do? Or can we remove it?
+    # def setclk(bit):
     #    self.clkctrl=self.setbit(bit,self.clkctrl)
 
-    # def setinputs(self, *args):
-    #    for i in args:
-    #       self.setinput(i)
+    def setinputs(self, *args):
+       for i in args:
+          self.setinput(i)
 
-    #def setoutputs(self, *args):
-    #   for i in args:
-    #      self.setoutput(i)
+    def setoutputs(self, *args):
+      for i in args:
+         self.setoutput(i)
         
     #def setclks(self, *args):
     #    for i in args:
     #        self.setclk(i)
 
-    def setnloop(self,l,reps):
-        self.pattern.nloop[l]=reps
-        #print("patnloop",l,reps,self.pattern.nloop[l])
+    def setnloop(self, i, reps):
+        self.pattern.nloop[i] = reps
 
-    def setstartloop(self,l):
-        self.pattern.loop[l*2]=self.iaddr
-        #print("patstart",l,self.iaddr,self.pattern.loop[l*2])
+    def setstartloop(self, i):
+        """
+        Set startloop[i] to the current address.
+        """
+        self.pattern.startloop[i] = self.iaddr
 
-    def setstoploop(self,l):
-        self.pattern.loop[l*2+1]=self.iaddr
-        #print("patstop",l,self.iaddr,self.pattern.loop[l*2+1])
+
+    def setstoploop(self, i):
+        """
+        Set stoploop[i] to the current address.
+        """
+        self.pattern.stoploop[i] = self.iaddr
         
 
-    def setstart(self,l):
+    def setstart(self):
+        """
+        Set start of pattern to the current address.
+        """
         self.pattern.limits[0]=self.iaddr
-        #print("start",self.iaddr,self.pattern.limits[0])
+
 
     def setstop(self,l):
-        self.pattern.limits[1]=self.iaddr
-        #print("stop",self.iaddr,self.pattern.limits[1])
+        """
+        Set stop of pattern to the current address.
+        """
+        self.pattern.limits[1] = self.iaddr
+
         
-    def setwaitpoint(self,l):
-        self.pattern.wait[l]=self.iaddr
-        #print("wait",l,self.iaddr,self.pattern.wait[l])
+    def setwaitpoint(self, i):
+        """
+        Set wait[i] to the current address.
+        """
+        self.pattern.wait[i] = self.iaddr
 
-    def setwaittime(self,l,t):
-        self.pattern.waittime[l]=t
-        #print("waittime",l,t,self.pattern.waittime[l])
 
-    def setwait(self,l,t):
-        self.setwait(l)
-        self.setwaittime(l,t)
+    def setwaittime(self, i, t):
+        """
+        Set waittime[i] to t.
+        """
+        self.pattern.waittime[i] = t
+
+
+    def setwait(self, i, t):
+        """
+        Set wait[i] to the current address and waittime[i] to t.
+        """
+        self.setwait(i)
+        self.setwaittime(i, t)
 
     
     def patInfo(self):
         print("### SUMMARY OF PATTERN PARAMETERS ###")
         print("Pattern limits (patlimits):",self.pattern.limits) 
-        print("Loop:",self.pattern.loop)
+        print("startloop:",self.pattern.startloop)
+        print("stoploop:",self.pattern.stoploop)
         print("Nloop:",self.pattern.nloop)
         print("Wait:",self.pattern.wait)
         print("Waittime",self.pattern.waittime)
-        print("Words",self.pattern.word[self.pattern.word>0])
+        print("Words", self.pattern.word[self.pattern.word>0])
         print("########################################")
 
     def to_lines(self):
-        pwords=''
-        for i in range(self.pattern.limits[1]):
-            l='patword '+hexFormat(i,4)+' '+hexFormat(self.pattern.word[i],16)+'\n'
-            pwords+=l
-        for i in range(6):
-            l='patloop'+str(i)+' '+hexFormat(self.pattern.loop[i*2],4)+' '+hexFormat(self.pattern.loop[i*2+1],4)+'\n'+'patnloop'+str(i)+' '+str(self.pattern.nloop[i])+'\n'
-            pwords+=l
-        for i in range(6):
-            l='patwait'+str(i)+' '+hexFormat(self.pattern.wait[i],4)+'\n'+'patwaittime'+str(i)+' '+str(self.pattern.waittime[i])+'\n'
-            pwords+=l
-        
-        l='patlimits '+hexFormat(self.pattern.limits[0],4)+' '+hexFormat(self.pattern.limits[1],4)+'\n'
-        pwords+=l
-        return pwords
+        """
+        Convert pattern to text representation.
+        """
+        lines = []
 
-    def saveToFile(self,fname):
-        pwords=''
-        for i in range(self.pattern.limits[1]):
-            l='patword '+hexFormat(i,4)+' '+hexFormat(self.pattern.word[i],16)+'\n'
-            pwords+=l
-        for i in range(6):
-            l='patloop'+str(i)+' '+hexFormat(self.pattern.loop[i*2],4)+' '+hexFormat(self.pattern.loop[i*2+1],4)+'\n'+'patnloop'+str(i)+' '+str(self.pattern.nloop[i])+'\n'
-            pwords+=l
-        for i in range(6):
-            l='patwait'+str(i)+' '+hexFormat(self.pattern.wait[i],4)+'\n'+'patwaittime'+str(i)+' '+str(self.pattern.waittime[i])+'\n'
-            pwords+=l
-        
-        l='patlimits '+hexFormat(self.pattern.limits[0],4)+' '+hexFormat(self.pattern.limits[1],4)+'\n'
-        pwords+=l
+        # write pattern words
+        for i in range(self.pattern.limits[0], self.pattern.limits[1], 1):
+            lines.append(f'patword {hexFormat(i,4)} {hexFormat(self.pattern.word[i],16)}')
 
-        f=open(fname,'w')
-        f.write(pwords)
-        f.close()
+        # write ioctrl and limits (TODO! Should this always be here?)
+        lines.append(f'patioctrl {hexFormat(self.pattern.ioctrl,16)}')
+        lines.append(f'patlimits {hexFormat(self.pattern.limits[0],4)} {hexFormat(self.pattern.limits[1],4)}')
+
+        # write loop limits
+        for i in range(6):
+            lines.append(f'patloop {i} {hexFormat(self.pattern.startloop[i],4)} {hexFormat(self.pattern.stoploop[i],4)}')                    
+            lines.append(f'patnloop {i} {self.pattern.nloop[i]}')
+
+
+        # write wait times
+        for i in range(6):
+            lines.append(f'patwait {i} {hexFormat(self.pattern.wait[i],4)}')
+            lines.append(f'patwaittime {i} {self.pattern.waittime[i]}')
+        
         
 
-    def load(self,det):
-        #print("Loading pattern onto detector")
+        return lines
+    
+    def print(self):
+        for l in self.to_lines():
+            print(l)
+
+    def saveToFile(self, fname):
+        with open(fname,'w') as f:
+            f.writelines(l + '\n' for l in self.to_lines())
+        
+
+    def load(self, det):
+        """
+        Load the pattern into the detector.
+        """
         det.setPattern(self.pattern)
---
+++ python/tests/test_patterntools.py
@@ -0,0 +1,124 @@
+import pytest 
+
+from slsdet.patterntools import pat
+
+
+def apply_detconf(p):
+    """
+    Hacky workaround to apply detConf_mh02 to a pattern
+    """
+    DACMON = 0
+    cnt_en_3 = 1
+    pulse_counter_en = 2
+    cnt_en_1 = 3
+    par_load = 4
+    pulse_mux_ctr = 5
+    reset_cnt = 6
+    reset_periphery = 7
+    config_load = 8
+    cnt_en_0 = 9
+    tbl = 10
+    clk_ext = 11
+    trimbit_load_reg = 12
+    store = 13
+    data_in = 14
+    en_pll_clk = 15
+    cnt_en_2 = 16
+    DACINT = 17
+    data_out_slow = 18 #IN
+    COMP2_MON = 19 #IN
+    start_read = 20
+    dac_store = 21
+    CNT3_MON = 22 #IN
+    EN_PIX_DIG_MON = 23
+    clk_sel = 24
+    BUSY = 25 #IN
+    COMP3_MON = 26 #IN
+    CNT2_MON = 27 #IN
+
+    dbit_ena=62 #FIFO LATCH
+    adc_ena=63  #ADC ENABLE
+
+    #FPGA input/ouutputs
+    p.setoutput(DACMON)
+    p.setoutput(cnt_en_3)
+    p.setoutput(pulse_counter_en)
+    p.setoutput(cnt_en_1)
+    p.setoutput(par_load)
+    p.setoutput(pulse_mux_ctr)
+    p.setoutput(reset_cnt)
+    p.setoutput(reset_periphery)
+    p.setoutput(cnt_en_0)
+    p.setoutput(tbl)
+    p.setoutput(clk_ext)
+    p.setoutput(config_load)
+    p.setoutput(trimbit_load_reg)
+    p.setoutput(store)
+    p.setoutput(data_in)
+    p.setoutput(en_pll_clk)
+    p.setoutput(cnt_en_2)
+    p.setoutput(DACINT)
+    p.setinput(data_out_slow)
+    p.setinput(COMP2_MON)
+    p.setoutput(start_read)
+    p.setoutput(dac_store)
+    p.setinput(CNT3_MON)
+    p.setoutput(EN_PIX_DIG_MON)
+    p.setoutput(clk_sel)
+    p.setinput(BUSY)
+    p.setinput(COMP3_MON)
+    p.setinput(CNT2_MON)
+
+    #system signals
+    p.setoutput(adc_ena)
+    # FIFO LATCH
+    p.setoutput(dbit_ena)
+    return p    
+
+
+def test_simple_pattern():
+    """
+    Using enable pll pattern for MH02
+    """
+    en_pll_clk = 15
+    p = pat()
+    p = apply_detconf(p)
+    p.SB(en_pll_clk)
+    p.PW()
+    p.PW() 
+
+    lines = p.to_lines()
+
+    enable_pll_pattern = [
+        "patword 0x0000 0x0000000000008000",
+        "patword 0x0001 0x0000000000008000",
+        "patioctrl 0xc000000001b3ffff",
+        "patlimits 0x0000 0x0001",
+        "patloop 0 0x1fff 0x1fff",
+        "patnloop 0 0",
+        "patloop 1 0x1fff 0x1fff",
+        "patnloop 1 0",
+        "patloop 2 0x1fff 0x1fff",
+        "patnloop 2 0",
+        "patloop 3 0x1fff 0x1fff",
+        "patnloop 3 0",
+        "patloop 4 0x1fff 0x1fff",
+        "patnloop 4 0",
+        "patloop 5 0x1fff 0x1fff",
+        "patnloop 5 0",
+        "patwait 0 0x1fff",
+        "patwaittime 0 0",
+        "patwait 1 0x1fff",
+        "patwaittime 1 0",
+        "patwait 2 0x1fff",
+        "patwaittime 2 0",
+        "patwait 3 0x1fff",
+        "patwaittime 3 0",
+        "patwait 4 0x1fff",
+        "patwaittime 4 0",
+        "patwait 5 0x1fff",
+        "patwaittime 5 0",
+    ]
+
+    for i, line in enumerate(lines):
+        assert line == enable_pll_pattern[i]

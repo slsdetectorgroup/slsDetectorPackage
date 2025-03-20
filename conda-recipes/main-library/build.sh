6dd0a5b0dd821a1971fb8363339e7b02df888c1f
Fröjd Lars Erik <froejdh_e@pcmoench03.psi.ch>
2025-03-20 13:39:51 +0100

    added zlib

--- conda-recipes/main-library/build.sh
+++ conda-recipes/main-library/build.sh
@@ -18,7 +18,7 @@ cmake .. -G Ninja \
       -DSLS_USE_TESTS=ON \
       -DSLS_USE_PYTHON=OFF \
       -DCMAKE_BUILD_TYPE=Release \
-      -DSLS_USE_HDF5=OFF\
+      -DSLS_USE_HDF5=OFF \
      
 NCORES=$(getconf _NPROCESSORS_ONLN)
 echo "Building using: ${NCORES} cores"

3bfa3bfba3572adafef43844d20b2fd6b9b32f1b
Dhanya Thattil <dhanya.thattil@psi.ch>
2025-05-13 23:30:35 +0200

    update receiver version

--- slsSupportLib/include/sls/versionAPI.h
+++ slsSupportLib/include/sls/versionAPI.h
@@ -9,5 +9,5 @@
 #define APIXILINXCTB "9.1.0 0x250204"
 #define APIJUNGFRAU  "9.1.0 0x250318"
 #define APILIB       "9.1.0 0x250325"
-#define APIRECEIVER  "9.1.0 0x250325"
 #define APIMYTHEN3   "9.1.1 0x250409"
+#define APIRECEIVER  "9.1.1 0x250513"

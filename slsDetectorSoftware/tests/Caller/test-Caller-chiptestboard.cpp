071b142b1083f65d3185a438138b661af937b27f
Dhanya Thattil <dhanya.thattil@psi.ch>
2025-08-04 14:11:51 +0200

    fixed ctb dbit clock changing period in tests as it was setting run clock instead

--- slsDetectorSoftware/tests/Caller/test-Caller-chiptestboard.cpp
+++ slsDetectorSoftware/tests/Caller/test-Caller-chiptestboard.cpp
@@ -1000,7 +1000,7 @@ TEST_CASE("dbitclk", "[.cmdcall]") {
     auto det_type = det.getDetectorType().squash();
 
     if (det_type == defs::CHIPTESTBOARD) {
-        auto prev_val = det.getRUNClock();
+        auto prev_val = det.getDBITClock();
         {
             std::ostringstream oss;
             caller.call("dbitclk", {"20"}, -1, PUT, oss);
@@ -1017,7 +1017,7 @@ TEST_CASE("dbitclk", "[.cmdcall]") {
             REQUIRE(oss.str() == "dbitclk 10\n");
         }
         for (int i = 0; i != det.size(); ++i) {
-            det.setRUNClock(prev_val[i], {i});
+            det.setDBITClock(prev_val[i], {i});
         }
     } else {
         // clock index might work

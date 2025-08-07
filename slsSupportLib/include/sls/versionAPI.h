2bf7b4068ce1bd063a6bb12c1fe11a9074496dee
Dhanya Thattil <dhanya.thattil@psi.ch>
2025-08-07 12:41:22 +0200

    updating package version, client version, server versions. Renaming server versions, using hardlinks in serverBin. Removing ctb servers in serverBin.

--- slsSupportLib/include/sls/versionAPI.h
+++ slsSupportLib/include/sls/versionAPI.h
@@ -1,12 +1,12 @@
 // SPDX-License-Identifier: LGPL-3.0-or-other
 // Copyright (C) 2021 Contributors to the SLS Detector Package
 /** API versions */
-#define APILIB       "0.0.0 0x250805"
-#define APIRECEIVER  "0.0.0 0x250807"
-#define APICTB       "0.0.0 0x250725"
-#define APIGOTTHARD2 "0.0.0 0x250725"
-#define APIMOENCH    "0.0.0 0x250725"
-#define APIEIGER     "0.0.0 0x250725"
-#define APIXILINXCTB "0.0.0 0x250725"
-#define APIJUNGFRAU  "0.0.0 0x250725"
-#define APIMYTHEN3   "0.0.0 0x250725"
+#define APILIB       "10.0.0 0x250805"
+#define APIRECEIVER  "10.0.0 0x250807"
+#define APICTB       "10.0.0 0x250807"
+#define APIGOTTHARD2 "10.0.0 0x250807"
+#define APIMOENCH    "10.0.0 0x250807"
+#define APIEIGER     "10.0.0 0x250807"
+#define APIXILINXCTB "10.0.0 0x250807"
+#define APIJUNGFRAU  "10.0.0 0x250807"
+#define APIMYTHEN3   "10.0.0 0x250807"

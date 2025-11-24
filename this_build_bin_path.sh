ea6a1dd5fefcfafc1a3b40ca2b5ffa00862ff5d6
Martin Brückner <martin.brueckner@psi.ch>
2025-11-24 16:10:53 +0100

    better this_build_bin_path script

--- this_build_bin_path.sh
+++ this_build_bin_path.sh
@@ -1,39 +1,15 @@
-# SPDX-License-Identifier: LGPL-3.0-or-other
-# Copyright (C) 2021 Contributors to the SLS Detector Package
-#echo $#
-#if [ $# = 0 ]; then 
-#    f=$0
-#else
-#    f=$1
-#fi
-#echo $f
-if [ "x${BASH_ARGV[0]}" = "x" ]; then
-#if [ "x$f" = "x" ]; then
-    if [ ! -f this_build_bin_path.sh ]; then
-	f=$0
-	echo "aaaa"
-    #thispath=$(dirname ${BASH_ARGV[0]})  
-    thispath=$(dirname $f) 
-    p=$(cd ${thispath};pwd); 
-    THIS_PATH="$p/build/bin/"
-   #     echo "ERROR: must cd where/this/package/is before calling this_path.sh"
-#	echo "Try sourcing it"                                 
-    else
-	echo "bbb"
-	THIS_PATH="$PWD/build/bin/"; 
-    fi                        
-else        
-    thispath=$(dirname ${BASH_ARGV[0]})
-    p=$(cd ${thispath};pwd); 
-    THIS_PATH="$p/build/bin/"    
-    echo "ccc"
-fi   
+#!/bin/bash
 
-    echo "this_path="$THIS_PATH  
-    export PATH=$THIS_PATH:$PATH
-    export LD_LIBRARY_PATH=$THIS_PATH:$LD_LIBRARY_PATH
-    export PYTHONPATH=$THIS_PATH:$PYTHONPATH
-    
-    echo "path="$PATH
-    echo "ld_library_path="$LD_LIBRARY_PATH
-    echo "pythonpath="$PYTHON_PATH
+# Since this script could be sourced, $0 is not sufficent, BASH_SOURCE[0] is necessary
+SCRIPT_LOCATION="$(realpath ${BASH_SOURCE[0]})"
+SCRIPT_LOCATION="$(dirname ${SCRIPT_LOCATION})"
+BUILDBIN_LOCATION="${SCRIPT_LOCATION}/build/bin"
+
+if [ ! -d "${BUILDBIN_LOCATION}" ]; then
+	echo Cannot find path ${BUILDBIN_LOCATION}
+else
+	echo Adding ${BUILDBIN_LOCATION} to PATH, PYTHONPATH and LD_LIBRARY_PATH
+	export PATH=${PATH}:${BUILDBIN_LOCATION}
+	export PYTHONPATH=${PYTHONPATH}:${BUILDBIN_LOCATION}
+	export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${BUILDBIN_LOCATION}
+fi

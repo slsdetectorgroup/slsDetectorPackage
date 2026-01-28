c3c3970f1952d93352fc16d1aa7eedc8f91e586c
froejdh_e <erik.frojdh@psi.ch>
2026-01-28 15:13:15 +0100

    added 16bit 1 counter and 16bit 4 counters

--- pyctbgui/pyctbgui/services/Transceiver.py
+++ pyctbgui/pyctbgui/services/Transceiver.py
@@ -133,7 +133,10 @@ class TransceiverTab(QtWidgets.QWidget):
                 nbitsPerDBit += (8 - (dSamples % 8))
             transceiverOffset += nDBitEnabled * (nbitsPerDBit // 8)
         trans_array = np.array(np.frombuffer(data, offset=transceiverOffset, dtype=np.uint16))
-        return decoder.decode(trans_array, pm.matterhorn_transceiver())
+        print(f'{trans_array.shape=}')
+        tmp =  decoder.decode(trans_array, self.mainWindow.pixel_map)
+        print(f'{tmp.shape=}')
+        return tmp
 
     def processImageData(self, data, dSamples):
         """

dfb46ad91173ff53ac543da763610997fc187d78
Dhanya Thattil <dhanya.thattil@psi.ch>
2026-04-16 17:15:54 +0200

    fixed tests

--- python/tests/test_freq.py
+++ python/tests/test_freq.py
@@ -1,14 +1,20 @@
-from slsdet import Hz
-'''
+from slsdet import Hz, MHz, kHz
+
 def test_Hz():
     f = Hz(1)
-    assert f.value() == 1e6
-    f = Hz('1MHz')
-    assert f.value() == 1e6
-    f = Hz('5000kHz')
-    assert f.value() == 5e6
-    assert Hz(1) == 1
-    assert Hz(1e6) == 1e6
-    assert Hz(1, 'MHz') == 1e6
-    assert Hz(0.5, 'GHz') == 0.5e9
-'''
+    assert f.value == 1
+    f = Hz(1 * 1000)
+    assert f.value == 1000
+    f = MHz(5)
+    assert f.value == 5_000_000
+    f = MHz(0.5)
+    assert f.value == 500_000
+    f = kHz(2.5)
+    assert f.value == 2500
+    f = kHz(5000)
+    assert f.value == 5_000_000
+
+def test_rounding_exact():
+    f = MHz(1.234)
+    assert f.value == round(1.234 * 1_000_000)
+

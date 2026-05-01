b1a6f2ba3269fce1a1dbc4a58832fe573b1eb401
Dhanya Thattil <dhanya.thattil@psi.ch>
2026-05-01 11:25:51 +0200

    missed a test marker

--- python/tests/test_det_api.py
+++ python/tests/test_det_api.py
@@ -637,7 +637,7 @@ def test_v_limit(session_simulator, request):
     Log(LogLevel.INFOGREEN, f"✅ {request.node.name} passed")
 
 
-
+@pytest.mark.detectorintegration
 def test_v_abcd(session_simulator, request):
     """Test v_a, v_b, v_c, v_d, v_io are deprecated comands."""
     det_type, num_interfaces, num_mods, d = session_simulator

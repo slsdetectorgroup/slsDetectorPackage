111d10cfa7e133d9f95593ed10f3d1dcc5150176
Dhanya Thattil <dhanya.thattil@psi.ch>
2026-05-06 15:57:26 +0200

    formatting

--- slsDetectorServers/ctbDetectorServer/slsDetectorFunctionList.c
+++ slsDetectorServers/ctbDetectorServer/slsDetectorFunctionList.c
@@ -1882,8 +1882,10 @@ int getPowerADC(enum powerIndex index, int *retval, char *mess) {
 #ifdef VIRTUAL
     return 0;
 #endif
-    // read register containing the measurement (16-bit signed), sign extend to int
-    *retval = (int)(int16_t)bus_r(POWER_MONITOR_BASE_REG + ((int)adcIndex << MEM_MAP_SHIFT));
+    // read register containing the measurement (16-bit signed), sign extend to
+    // int
+    *retval = (int)(int16_t)bus_r(POWER_MONITOR_BASE_REG +
+                                  ((int)adcIndex << MEM_MAP_SHIFT));
     return OK;
 }
 

f1b5a5b78a284187fe682a400215f71227a81466
Martin Mueller <martin.mueller@psi.ch>
2026-08-13 10:35:39 +0200

    run clang-format

--- slsReceiverSoftware/src/HDF5DataFile.cpp
+++ slsReceiverSoftware/src/HDF5DataFile.cpp
@@ -196,15 +196,18 @@ void HDF5DataFile::CreateFile() {
         plist.setChunk(DATA_RANK, dimsChunk);
 
         // option to add generic filter to property list
-            /* TODO:
-                - as command instead of compile time ?
-                - allow nested filters ?
-            */
+        /* TODO:
+            - as command instead of compile time ?
+            - allow nested filters ?
+        */
         if (SLS_HDF5_FILTER_ID > 0) {
-            // HDF5 filters are dynamically registered --> check if the filter is available
+            // HDF5 filters are dynamically registered --> check if the filter
+            // is available
             const auto filterId = static_cast<H5Z_filter_t>(SLS_HDF5_FILTER_ID);
             if (H5Zfilter_avail(filterId) <= 0) {
-                throw RuntimeError("HDF5 filter ID " + std::to_string(SLS_HDF5_FILTER_ID) + " is not available, check HDF5_PLUGIN_PATH");
+                throw RuntimeError("HDF5 filter ID " +
+                                   std::to_string(SLS_HDF5_FILTER_ID) +
+                                   " is not available, check HDF5_PLUGIN_PATH");
             }
 
             // parse generic filter parameters
@@ -215,14 +218,16 @@ void HDF5DataFile::CreateFile() {
                 if (token.empty()) {
                     continue;
                 }
-                filterParams.push_back(static_cast<unsigned int>(std::stoul(token, nullptr, 10)));
+                filterParams.push_back(
+                    static_cast<unsigned int>(std::stoul(token, nullptr, 10)));
             }
 
             // apply filter to property list
             if (filterParams.empty()) {
                 plist.setFilter(filterId, 0, 0, nullptr);
             } else {
-                plist.setFilter(filterId, 0, filterParams.size(), filterParams.data());
+                plist.setFilter(filterId, 0, filterParams.size(),
+                                filterParams.data());
             }
         }
         plistPara.setChunk(PARA_RANK, dimsChunkPara);

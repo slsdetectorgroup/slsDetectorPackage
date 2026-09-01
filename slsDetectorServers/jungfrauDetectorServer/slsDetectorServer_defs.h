e04b30e38f57b14414b63db3fe57b1801a96ae34
Dhanya Thattil <dhanya.thattil@psi.ch>
2026-09-01 17:24:18 +0200

    jf firmware already fixed max loops for pedestalmode. allow the max now. should have been in 9.1 firmware

--- slsDetectorServers/jungfrauDetectorServer/slsDetectorServer_defs.h
+++ slsDetectorServers/jungfrauDetectorServer/slsDetectorServer_defs.h
@@ -63,7 +63,10 @@
 #define DEFAULT_TIMING_INFO_DECODER      (SWISSFEL)
 #define DEFAULT_ELECTRON_COLLECTION_MODE (0)
 
-#define MAX_PEDESTAL_LOOPS   (0xFF) // until fixed in firmware
+#define MAX_PEDESTAL_LOOPS                                                     \
+    (PEDESTAL_MODE_ITRTNS_MSK >> PEDESTAL_MODE_ITRTNS_OFST)
+#define MAX_PEDESTAL_FRAMES                                                    \
+    (PEDESTAL_MODE_LNGTH_MSK >> PEDESTAL_MODE_LNGTH_OFST)
 #define HIGHVOLTAGE_MIN      (60)
 #define HIGHVOLTAGE_MAX      (200)
 #define DAC_MIN_MV           (0)
--- slsDetectorServers/slsDetectorServer/src/slsDetectorServer_funcs.c
+++ slsDetectorServers/slsDetectorServer/src/slsDetectorServer_funcs.c
@@ -10745,6 +10745,13 @@ int set_pedestal_mode(int file_des) {
                         "be 0. [%hhu, %hu].\n",
                         frames, loops);
                 LOG(logERROR, (mess));
+            } else if (frames > MAX_PEDESTAL_FRAMES) {
+                ret = FAIL;
+                sprintf(mess,
+                        "Could not set pedestal mode. Frames [%hhu] cannot be "
+                        "greater than %d.\n",
+                        frames, MAX_PEDESTAL_FRAMES);
+                LOG(logERROR, (mess));
             } else if (loops > MAX_PEDESTAL_LOOPS) {
                 ret = FAIL;
                 sprintf(mess,
